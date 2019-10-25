02b86520e466dbab5dd81dce13fdb3ccd7c7580d
Dario Rekowski on RockPI <dario.rekowski@gmx.de>
2019-10-25 12:20:05 +0000

    update compile protos, condition for mkdir

--- compile_proto.sh
+++ compile_proto.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-mkdir ./src/cpp/proto
+if [ ! -d "./src/cpp/proto" ] ; then
+	mkdir ./src/cpp/proto
+fi
 protoc --cpp_out=./src/cpp/proto --proto_path=./src/proto/gradido ./src/proto/gradido/*.proto
