55fd78f1c7efa3b4f787d174a7bdd067d2919e52
Dario <dario.rekowski@gmx.de>
2019-11-27 13:26:07 +0100

    update verification code in email

--- src/cpp/HTTPInterface/CheckEmailPage.cpp
+++ src/cpp/HTTPInterface/CheckEmailPage.cpp
@@ -209,7 +209,10 @@ void CheckEmailPage::handleRequest(Poco::Net::HTTPServerRequest& request, Poco::
 #line 104 "F:\\Gradido\\gradido_login_server\\src\\cpsp\\checkEmail.cpsp"
 	responseStream << ( langCatalog->gettext("Schicke mir eine E-Mail und ich k&uuml;mmere mich darum: "));
 	responseStream << "<br>\n";
-	responseStream << "                <a href=\"mailto:coin@gradido.net?subject=Invalid E-Mail Verification Code&amp;body=Hallo Dario,%0D%0A%0D%0Amein E-Mail Verification-Code: 121121354 funktioniert nicht,%0D%0Akannst du das prüfen?%0D%0A%0D%0AMit freundlichen Grüßen%0D%0A\">E-Mail</a>\n";
+	responseStream << "                <a href=\"mailto:coin@gradido.net?subject=Invalid E-Mail Verification Code&amp;body=Hallo Dario,%0D%0A%0D%0Amein E-Mail Verification-Code: ";
+#line 105 "F:\\Gradido\\gradido_login_server\\src\\cpsp\\checkEmail.cpsp"
+	responseStream << ( verificationCode );
+	responseStream << " funktioniert nicht,%0D%0Akannst du das prüfen?%0D%0A%0D%0AMit freundlichen Grüßen%0D%0A\">E-Mail</a>\n";
 	responseStream << "\t\t\t  </p>\n";
 	responseStream << "            </div>\n";
 	responseStream << "          </div>\n";
