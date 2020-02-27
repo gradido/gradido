7e4759b33e47c4b64b370ac4ce7abbfc323eeae4
Dario <dario.rekowski@gmx.de>
2020-02-27 12:34:44 +0100

    change to utf8 with bom

--- src/cpp/ImportantTests.cpp
+++ src/cpp/ImportantTests.cpp
@@ -1,4 +1,4 @@
-#include "ImportantTests.h"
+Ôªø#include "ImportantTests.h"
 
 #include <string>
 #include "ServerConfig.h"
@@ -11,11 +11,11 @@ namespace ImportantTests {
 		auto de_words = &ServerConfig::g_Mnemonic_WordLists[ServerConfig::MNEMONIC_GRADIDO_BOOK_GERMAN_RANDOM_ORDER];
 		auto en_words = &ServerConfig::g_Mnemonic_WordLists[ServerConfig::MNEMONIC_BIP0039_SORTED_ORDER];
 
-		std::string passphrase_1_de = u8"beziffern Anbeginn h‰keln Sozialabgaben Rasen flieﬂen Frau weltweit Urlaub Urwissen Lohn plˆtzlich Gefrierpunkt Derartig Biedermeier getragen denken Realisierung Boden maximal voneinander Fˆrdern Braten Entlastung";
+		std::string passphrase_1_de = u8"beziffern Anbeginn h√§keln Sozialabgaben Rasen flie√üen Frau weltweit Urlaub Urwissen Lohn pl√∂tzlich Gefrierpunkt Derartig Biedermeier getragen denken Realisierung Boden maximal voneinander F√∂rdern Braten Entlastung";
 		std::string passphrase_1_en = "boil banner regret since goat awful crane imitate myth clump rally offer train airport purpose machine helmet ahead alley gesture load scrub river glory";
 		std::string passphrase_1_pubkey_hex = "cfce9cfad355ceb8c099a97f55a2bd7aa8d2bd0b86970f7d1e135be9e1da5eb7";
 
-		std::string passphrase_2_de = u8"dazu Zyklus Danach Auge losfliegen besprechen stoﬂen ohne heutige Begr¸ndung Dogma Erkenntnis genieﬂen Medaille ƒste Google woher Sprache P‰dagoge Schweigen rasant Sekunde nahm Nordstern";
+		std::string passphrase_2_de = u8"dazu Zyklus Danach Auge losfliegen besprechen sto√üen ohne heutige Begr√ºndung Dogma Erkenntnis genie√üen Medaille √Ñste Google woher Sprache P√§dagoge Schweigen rasant Sekunde nahm Nordstern";
 		std::string passphrase_2_en = "place oblige gain jar neither note cry riot empty inform egg skate suffer garlic lake ladder liquid focus gorilla subject strong much oyster reduce";
 		std::string passphrase_2_pubkey_hex = "3d547825bb53465579b95560981f444105495f2b6a68134fbec28ce518ac7b38";
 
