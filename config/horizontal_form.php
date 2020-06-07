0d9d1b595ee4b68117c568ad5c25e1b746978683
Christine Slotty <christine.slotty@software-labor.de>
2020-06-07 18:50:16 +0200

    form WIP

--- config/horizontal_form.php
+++ config/horizontal_form.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,35 +8,22 @@
 
 return [
     // Container element used by control().
-    'inputContainer' => '<div class="form-group row showcase_row_area">{{content}}</div>',
+    'inputContainer' => '{{content}}',
     // Container element used by control() when a field has an error.
-    //'inputContainerError' => '<div class="input {{type}}{{required}} error">{{content}}{{error}}</div>',
-    'inputContainerError' => '<div class="form-group row showcase_row_area {{type}}{{required}} is-invalid">{{content}}{{error}}</div>',
+    'inputContainerError' => '<div class="{{type}}{{required}} is-invalid">{{content}}{{error}}</div>',
     // Label element when inputs are not nested inside the label.
-    'label' => '<div class="col-md-3 showcase_text_area">'
-              . '<label{{attrs}}>{{text}}</label>'
-             . '</div>',
+    'label' => '<label{{attrs}} class="form-label">{{text}}</label>',
     // Generic input element.
-    'input' => '<div class="col-md-9 showcase_content_area">'
-              . '<input type="{{type}}" class="form-control" name="{{name}}"{{attrs}}/>'
-             . '</div>',
+    'input' => '<input type="{{type}}" class="form-control" name="{{name}}"{{attrs}}/>',
     // Textarea input element,
-    'textarea' => '<div class="col-md-9 showcase_content_area">'
-                  . '<textarea class="form-control" name="{{name}}"{{attrs}}>{{value}}</textarea>'
-                . '</div>',
+    'textarea' => '<textarea class="form-control" name="{{name}}"{{attrs}}>{{value}}</textarea>',
     // Error message wrapper elements.
-    //'error' => '<div class="error-message">{{content}}</div>',
-    //'error' => '{{content}}',
-    'error' => '<div class="col-md-9 offset-md-3 showcase_content_area invalid-feedback">'
+    'error' => '<div class="invalid-feedback">'
               . '{{content}}'
              . '</div>',
-    
     // Container for error items.
-    //'errorList' => '<ul>{{content}}</ul>',
     'errorList' => '{{content}}',
-    
     // Error item wrapper.
-    //'errorItem' => '<li>{{text}}</li>',
     'errorItem' => '<div>{{text}}</div>'
 ];
 /*
@@ -49,11 +36,11 @@ return [
       <input type="number" step="0.01" class="form-control" id="inputAmount" name="inputAmount" >
     </div>
   </div>
- 
+
 
 <div class="input number required">
   <label for="amount">Amount</label>
   <input type="number" name="amount" required="required" step="0.01" id="amount">
 </div>
- 
+
  */
--- src/Template/TransactionSendCoins/create.ctp
+++ src/Template/TransactionSendCoins/create.ctp
@@ -19,8 +19,8 @@ $this->loadHelper('Form', [
       <?= $this->Form->control('email', ['label' => __('Empfänger'), 'placeholder' => 'E-Mail']) ?>
       <?= $this->Form->control('memo', ['label' => __('Verwendungszweck'), 'rows' => 3]) ?>
       <?= $this->Form->control('amount', ['label' => __('Betrag in GDD')]) ?>
-      <?= $this->Form->button(__('Transaktion abschließen'), ['name' => 'next', 'class' => 'btn btn-sm btn-primary']) ?>
-      <!--<?= $this->Form->button(__('Weitere Transaktion erstellen'), ['name' => 'add', 'class' => 'btn btn-sm']) ?>-->
+      <?= $this->Form->button(__('Transaktion abschließen'), ['name' => 'next', 'class' => 'form-button']) ?>
+      <!--<?= $this->Form->button(__('Weitere Transaktion erstellen'), ['name' => 'add', 'class' => 'form-button']) ?>-->
     <?= $this->Form->end() ?>
   </div>
 </div>
