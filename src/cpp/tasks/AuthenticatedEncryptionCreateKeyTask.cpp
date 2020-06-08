b285768fcef4a8d8b5e483622b11c58c3c85ecdb
Dario <dario.rekowski@gmx.de>
2020-06-08 13:40:21 +0200

    add AuthenticatedEncryptionCreateKeyTask for running password creation in background

---
+++ src/cpp/tasks/AuthenticatedEncryptionCreateKeyTask.cpp
@@ -0,0 +1,33 @@
+#include "AuthenticatedEncryptionCreateKeyTask.h"
+
+#include "../ServerConfig.h"
+#include "../SingletonManager/SingletonTaskObserver.h"
+#include "../SingletonManager/ErrorManager.h"
+
+AuthenticatedEncryptionCreateKeyTask::AuthenticatedEncryptionCreateKeyTask(Poco::AutoPtr<controller::User> user, const std::string& passwd)
+	: UniLib::controller::CPUTask(ServerConfig::g_CryptoCPUScheduler), mUser(user), mPassword(passwd)
+{
+	SingletonTaskObserver::getInstance()->addTask(mUser->getModel()->getEmail(), TASK_OBSERVER_PASSWORD_CREATION);
+}
+
+AuthenticatedEncryptionCreateKeyTask::~AuthenticatedEncryptionCreateKeyTask()
+{
+	SingletonTaskObserver::getInstance()->removeTask(mUser->getModel()->getEmail(), TASK_OBSERVER_PASSWORD_CREATION);
+}
+
+int AuthenticatedEncryptionCreateKeyTask::run()
+{
+	auto em = ErrorManager::getInstance();
+	const static char* function_name = "AuthenticatedEncryptionCreateKeyTask::run";
+	auto authenticated_encryption = new AuthenticatedEncryption;
+	if (AuthenticatedEncryption::AUTH_ENCRYPT_OK != authenticated_encryption->createKey(mUser->getModel()->getEmail(), mPassword)) {
+		em->addError(new Error(function_name, "error creating key"));
+		em->addError(new ParamError(function_name, "for email", mUser->getModel()->getEmail()));
+		em->addError(new ParamError(function_name, "strerror: ", strerror(errno)));
+		em->sendErrorsAsEmail();
+		return -1;
+	}
+	mUser->setPassword(authenticated_encryption);
+
+	return 0;
+}
