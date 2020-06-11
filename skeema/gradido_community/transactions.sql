6cc3910da04cf6b4ae717f0f1160662021b1eb7b
Dario via Pythagoras <dariofrodo@gmx.de>
2020-06-11 15:18:44 +0200

    add default

--- skeema/gradido_community/transactions.sql
+++ skeema/gradido_community/transactions.sql
@@ -2,7 +2,7 @@ CREATE TABLE `transactions` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `state_group_id` int(11) DEFAULT NULL,
   `transaction_type_id` int(11) NOT NULL,
-  `tx_hash` binary(32) NOT NULL,
+  `tx_hash` binary(32) DEFAULT NULL,
   `memo` varchar(255) COLLATE utf8_bin NOT NULL,
   `received` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
