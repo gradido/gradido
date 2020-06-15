0c8b0333a1e07603fa458c793e2b0f467754f3e5
Dario via Pythagoras <dariofrodo@gmx.de>
2020-06-15 15:06:54 +0200

    important! add default value

--- skeema/gradido_login/email_opt_in.sql
+++ skeema/gradido_login/email_opt_in.sql
@@ -4,7 +4,7 @@ CREATE TABLE `email_opt_in` (
   `verification_code` bigint(25) unsigned NOT NULL,
   `email_opt_in_type_id` int(11) NOT NULL,
   `created` datetime NOT NULL DEFAULT current_timestamp(),
-  `resend_count` int(11) NOT NULL,
+  `resend_count` int(11) DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE KEY `verification_code` (`verification_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
