7c186ad1e642fa834121c763653de0a605f65169
Dario via Pythagoras <dariofrodo@gmx.de>
2020-06-15 15:05:35 +0200

    Add field to count how often verification email was send

--- skeema/gradido_login/email_opt_in.sql
+++ skeema/gradido_login/email_opt_in.sql
@@ -4,6 +4,7 @@ CREATE TABLE `email_opt_in` (
   `verification_code` bigint(25) unsigned NOT NULL,
   `email_opt_in_type_id` int(11) NOT NULL,
   `created` datetime NOT NULL DEFAULT current_timestamp(),
+  `resend_count` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `verification_code` (`verification_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
