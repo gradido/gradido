8f62ad61154d46fa8c3eb5faa6ed7e88f6e6a6c8
Dario Rekowski on RockPI <dario.rekowski@gmx.de>
2020-08-21 13:16:58 +0000

    change to blake2

--- src/Controller/JsonRequestHandlerController.php
+++ src/Controller/JsonRequestHandlerController.php
@@ -58,10 +58,7 @@ class JsonRequestHandlerController extends AppController {
             case 'checkUser': return $this->checkUser($jsonData->email, $jsonData->last_name);
             case 'getUsers' : return $this->getUsers($jsonData->page, $jsonData->limit);
             case 'getUserBalance': return $this->getUserBalance($jsonData->email, $jsonData->last_name);
-
             case 'errorInTransaction': return $this->errorInTransaction($jsonData->created, $jsonData->transactionGenericHash, $jsonData->error, $jsonData->errorMessage);
-
-            case 'errorInTransaction': return $this->errorInTransaction($jsonData->created, $jsonData->transactionSha256, $jsonData->error, $jsonData->errorMessage);
 
           }
           return $this->returnJson(['state' => 'error', 'msg' => 'unknown method for post', 'details' => $method]);
@@ -70,7 +67,6 @@ class JsonRequestHandlerController extends AppController {
     }
     
     //! \param $transactionCreated creation of transaction in timestamp in seconds
-
     //!        -1 if transaction couldn't decode
     //! \param $transactionBodyBase64Sha256 generic hash from transaction body serialized and converted to base64 
     //!        using sodium_crypto_generichash to calculate
