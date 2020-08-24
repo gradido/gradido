490fbfe6ca5415e540aa57c4951501a81bbb23ab
einhornimmond - MarkX <dariofrodo@gmx.de>
2020-08-24 18:24:48 +0200

    include grpc manuell in cmake project, debug windows, other configurations are to come

--- compile_proto.sh
+++ compile_proto.sh
@@ -6,12 +6,10 @@ if [ ! -d "./src/cpp/proto/gradido" ] ; then
 	mkdir ./src/cpp/proto/gradido
 fi
 
-protoc --cpp_out=./src/cpp/proto --proto_path=./src/proto ./src/proto/gradido/*.proto
+./protoc --cpp_out=./src/cpp/proto --proto_path=./src/proto ./src/proto/gradido/*.proto
 
 #if [ ! -d "./src/cpp/proto/hedera" ] ; then 
 #	mkdir ./src/cpp/proto/hedera 
 #fi
 
-
-#./protoc --cpp_out=./src/cpp/proto/hedera --proto_path=./src/proto/hedera/hedera-protobuf/src/main/proto ./src/proto/hedera/hedera-protobuf/src/main/proto/*.proto
-
+./protoc --plugin=protoc-gen-grpc=./grpc_cpp_plugin.exe --cpp_out=./src/cpp/proto/hedera --grpc_out=./src/cpp/proto/hedera --proto_path=./src/proto/hedera/hedera-protobuf/src/main/proto ./src/proto/hedera/hedera-protobuf/src/main/proto/*.proto
