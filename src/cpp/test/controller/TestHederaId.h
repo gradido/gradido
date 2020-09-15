4cd082bc944f86254d0333db7104ec3e4cbf29eb
Dario <dario.rekowski@gmx.de>
2020-09-15 14:34:13 +0200

    add tests for hedera account and hedera id for faster fixing mysql error

---
+++ src/cpp/test/controller/TestHederaId.h
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+
+#include "../controller/HederaId.h"
+
+
+namespace controller {
+
+	class TestHederaId : public ::testing::Test {
+	protected:
+		void SetUp() override;
+	};
+
+}
