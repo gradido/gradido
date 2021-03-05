3d68745c879196e76956172b2d185b6a266b6494
ogerly <landmatrosen@googlemail.com>
2021-03-05 10:57:11 +0100

    style animie gradido coin logo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,14 +4,20 @@
     </header>
     <div class="bg-gray-100 min-h-screen pt-40 text-lg">     
       <router-view />
+       <particles-bg type="custom" :config="config" :bg="true" />
     </div>
   </div> 
 </template>
 
 <script>
+import { ParticlesBg } from "particles-bg-vue";
+import icon from "./icon.js";
 
 export default {
-   name: 'app',
+  name: 'app',
+  components: {
+      ParticlesBg,
+    },
     created () { 
       //console.log("this.$cookies.get('gdd_session_id') ", this.$cookies.get('gdd_session_id') )
       //console.log(" $cookies.isKey('gdd_session_id') == true", this.$cookies.isKey("gdd_session_id") == true )
@@ -21,7 +27,25 @@ export default {
       }else {
        this.$router.push("/Login")
       } 
-    }
+    },
+     data: function () {
+    return {
+      config: {
+        num: [1, 7],
+        rps: 15,
+        radius: [5, 50],
+        life: [6.5, 15],
+        v: [1, 1],
+        tha: [-40, 40],
+        body: icon,
+        alpha: [0.6, 0],
+        scale: [0.1, 0.4],
+        position: "all",
+        cross: "dead",
+        random: 2,
+      },
+    };
+  },
 }
 </script>
 <style>
