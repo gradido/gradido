c7bdf8978594b932615e48f9bb1c19d3c3bf3fcf
ogerly <landmatrosen@googlemail.com>
2021-03-05 09:14:56 +0100

    style 404 side :)

--- frontend/src/views/Layout/AuthLayout_gdd.vue
+++ frontend/src/views/Layout/AuthLayout_gdd.vue
@@ -1,48 +1,43 @@
 <template>
-  <div class="main-content bg-default">
-    
-     <template>
-        
- 
-    <div class="main-content">
-        <router-view></router-view>
-    
-    </div>
-
-    <footer class="py-5" id="footer-main">
-      <b-container >
-        <b-row align-v="center" class="justify-content-xl-between">
-          <b-col xl="6">
-            <div class="copyright text-center text-xl-left text-muted">
-               © {{year}} <a href="https://gradido.net/" class="font-weight-bold ml-1" target="_blank">Gradido Wallet</a>
-            </div>
-          </b-col>
-          <b-col xl="6" class="col-xl-6">
-            <b-nav class="nav-footer justify-content-center justify-content-xl-end">
-              <b-nav-item ref="https://www.creative-tim.com" target="_blank">
-                Gradido
-              </b-nav-item>
-              <b-nav-item href="https://www.creative-tim.com/presentation" target="_blank">
-               {{$t('imprint')}}
-              </b-nav-item>
-              <b-nav-item href="http://blog.creative-tim.com" target="_blank">
-                {{$t('privacy_policy')}}
-              </b-nav-item>
-              <b-nav-item href="https://www.creative-tim.com/license" target="_blank">
-                 {{$t('license')}}
-              </b-nav-item>
-            </b-nav>
-          </b-col>
-        </b-row>
-         <br>
-            <br>
-         <b-row >           
+  <div class="main-content bg-default">    
+    <template>
+      <div class="main-content">
+        <router-view></router-view>    
+      </div>
+      <footer class="py-5" id="footer-main">
+        <b-container >
+          <b-row align-v="center" class="justify-content-xl-between">
+            <b-col xl="6">
+              <div class="copyright text-center text-xl-left text-muted">
+                © {{year}} <a href="https://gradido.net/" class="font-weight-bold ml-1" target="_blank">Gradido Wallet</a>
+              </div>
+            </b-col>
+            <b-col xl="6" class="col-xl-6">
+              <b-nav class="nav-footer justify-content-center justify-content-xl-end">
+                <b-nav-item ref="https://www.creative-tim.com" target="_blank">
+                  Gradido
+                </b-nav-item>
+                <b-nav-item href="https://www.creative-tim.com/presentation" target="_blank">
+                {{$t('imprint')}}
+                </b-nav-item>
+                <b-nav-item href="http://blog.creative-tim.com" target="_blank">
+                  {{$t('privacy_policy')}}
+                </b-nav-item>
+                <b-nav-item href="https://www.creative-tim.com/license" target="_blank">
+                  {{$t('license')}}
+                </b-nav-item>
+              </b-nav>
+            </b-col>
+          </b-row>
+          <br>
+          <br>
+          <b-row >           
             <b-col class="nav-link text-center"  @click.prevent="setLocale('en')">{{ $t('languages.en') }}</b-col>
             <b-col class="nav-link text-center" @click.prevent="setLocale('de')">{{ $t('languages.de') }}</b-col>
-        </b-row>
-      </b-container>
-    </footer>
-        </template>
+          </b-row>
+        </b-container>
+      </footer>
+    </template>
   </div>
 </template>
 <script>
