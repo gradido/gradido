da1dd17887f7ca1b0af9a3bb785abff2476785dd
einhornimmond <dario.rekowski@gmx.de>
2021-03-13 14:45:21 +0100

    remove parsing hedera proto because there not used here

--- compile_proto.sh
+++ compile_proto.sh
@@ -8,10 +8,10 @@ fi
 
 protoc --cpp_out=./src/cpp/proto --proto_path=./src/proto ./src/proto/gradido/*.proto
 
-if [ ! -d "./src/cpp/proto/hedera" ] ; then 
-	mkdir ./src/cpp/proto/hedera 
-fi
+#if [ ! -d "./src/cpp/proto/hedera" ] ; then 
+#	mkdir ./src/cpp/proto/hedera 
+#fi
 
 
-protoc --cpp_out=./src/cpp/proto/hedera --proto_path=./src/proto/hedera/hedera-protobuf/src/main/proto ./src/proto/hedera/hedera-protobuf/src/main/proto/*.proto
+#./protoc --cpp_out=./src/cpp/proto/hedera --proto_path=./src/proto/hedera/hedera-protobuf/src/main/proto ./src/proto/hedera/hedera-protobuf/src/main/proto/*.proto
 
