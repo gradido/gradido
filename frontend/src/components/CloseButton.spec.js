a6ee7fe57ee5d5e098e546fb063f783537a8bcfe
Moriz Wahl <moriz.wahl@gmx.de>
2021-03-22 14:17:34 +0100

    setup for unit tests in frontend

---
+++ frontend/src/components/CloseButton.spec.js
@@ -0,0 +1,33 @@
+import { mount } from '@vue/test-utils'
+
+import CloseButton from './CloseButton'
+
+const localVue = global.localVue
+
+describe('CloseButton', () => {
+
+  let wrapper
+  let propsData = {
+    taget: ["Target", 0],
+    expanded: false,
+  }
+
+  const Wrapper = () => {
+    return mount(CloseButton, { localVue, propsData })
+  }
+
+  describe('mount', () => {
+
+    beforeEach(() => {
+      wrapper = Wrapper()
+    })
+
+    it('emmits click event', () => {
+      console.log(wrapper.html())
+      wrapper.find('.navbar-toggler').trigger('click')
+      expect(wrapper.emitted('click')).toBeTruthy()
+    })
+    
+  })
+  
+})
