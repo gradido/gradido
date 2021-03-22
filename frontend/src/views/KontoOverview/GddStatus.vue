c831fdca6a96327ce920afd50d1f9e24bdd02880
ogerly <landmatrosen@googlemail.com>
2021-03-22 16:06:10 +0100

    clear for deploy phase 1 WIP

--- frontend/src/views/KontoOverview/GddStatus.vue
+++ frontend/src/views/KontoOverview/GddStatus.vue
@@ -4,7 +4,8 @@
         <b-col xl="6" md="6">
           <stats-card type="gradient-red"
                       sub-title="balance_gdd"
-                      class="mb-4">
+                      class="mb-4"
+                      style="font-size: -webkit-xxx-large">
               {{ $n($store.state.user.balance) }} GDD
           </stats-card>
         </b-col>
@@ -22,12 +23,6 @@
 
 <script>
 export default {
-  name: 'GddStatus',
-  data(){
-    return {
-      balance_gdd: this.$store.state.user.balance,
-      balance_gdt: this.$store.state.user.balance_gdt,
-    }
-  }
+  name: 'GddStatus'
 };
 </script>
