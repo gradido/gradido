763c22648ae6aa95a7863b174a79ad50e4cfe8ca
Moriz Wahl <moriz.wahl@gmx.de>
2021-03-24 16:28:36 +0100

    locale for validation message is switched when changing locale. Cleaned uo AuthLayout_gdd.vue

--- frontend/src/views/Layout/AuthLayout_gdd.vue
+++ frontend/src/views/Layout/AuthLayout_gdd.vue
@@ -9,38 +9,10 @@
   </div>
 </template>
 <script>
-import ContentFooter from './ContentFooter.vue';
-import { localeChanged } from 'vee-validate'
-  export default { 
-     components: {
-      ContentFooter
-    },
-    data() {
-      return {
-        year: new Date().getFullYear(),
-        pageClass: 'login-page'
-      };
-    },
-    computed: {
-      title() {
-        return `${this.$route.name} Page`;
-      }
-    },
-    methods: {
-      setLocale(locale) {
-        this.$i18n.locale = locale
-        localeChanged()
-        //this.$router.push({
-        //  params: { lang: locale }
-        //})
-        //this.hideDropdown()
-      }
-    }
-  };
-  
+import ContentFooter from './ContentFooter.vue'
+ export default { 
+   components: {
+     ContentFooter
+   },
+ }
 </script>
-<style lang="scss">
- 
-.card { background-color:rgba(136, 130, 131, 0.3); }
- 
-</style>
