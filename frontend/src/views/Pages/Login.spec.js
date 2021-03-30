1431d13d368afe207641119f1b1ca2f48963de08
Moriz Wahl <moriz.wahl@gmx.de>
2021-03-30 19:29:39 +0200

    first tests for login form. to do: get rules of vee validate running

--- frontend/src/views/Pages/Login.spec.js
+++ frontend/src/views/Pages/Login.spec.js
@@ -1,5 +1,6 @@
 import { mount, RouterLinkStub } from '@vue/test-utils'
 import Vuex from 'vuex'
+import flushPromises from 'flush-promises'
 
 import Login from './Login'
 
@@ -39,6 +40,60 @@ describe('Login', () => {
 
     it('renders the Login form', () => {
       expect(wrapper.find('div.login-form').exists()).toBeTruthy()
+    })
+
+    describe('Login header', () => {
+      it('has a welcome message', () => {
+        expect(wrapper.find('div.header').text()).toBe('Gradido site.login.community')
+      })
+    })
+
+    describe('links', () => {
+      it('has a link "Forgot Password?"', () => {
+        expect(wrapper.findAllComponents(RouterLinkStub).at(0).text())
+          .toEqual('site.login.forgot_pwd')
+      })
+
+      it('links to /password when clicking "Forgot Password?"', () => {
+        expect(wrapper.findAllComponents(RouterLinkStub).at(0).props().to)
+          .toBe('/password')
+      })
+
+      it('has a link "Create new account"', () => {
+        expect(wrapper.findAllComponents(RouterLinkStub).at(1).text())
+          .toEqual('site.login.new_wallet')
+      })
+
+      it('links to /register when clicking "Create new account"', () => {
+        expect(wrapper.findAllComponents(RouterLinkStub).at(1).props().to)
+          .toBe('/register')
+      })
+    })
+
+    describe('Login form', () => {
+      it('has a login form', () => {
+        expect(wrapper.find('form').exists()).toBeTruthy()
+      })
+
+      it('has an Email input field', () => {
+        expect(wrapper.find('input[placeholder="Email"]').exists()).toBeTruthy()
+      })
+
+      it('has an Password input field', () => {
+        expect(wrapper.find('input[placeholder="form.password"]').exists()).toBeTruthy()
+      })
+
+      it('has a Submit button', () => {
+        expect(wrapper.find('button[type="submit"]').exists()).toBeTruthy()
+      })
+
+      it('shows a warining when no valid Email is entered', async () => {
+        wrapper.find('input[placeholder="Email"]').setValue('no_valid@Email')
+        //await wrapper.vm.$nextTick()
+        await flushPromises()
+        console.log(wrapper.find('fieldset').html())
+        await expect(false).toBetruthy()
+      })
     })
   })
 })
--- frontend/test/testSetup.js
+++ frontend/test/testSetup.js
@@ -3,8 +3,22 @@ import ElementUI from 'element-ui'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import Vuex from 'vuex'
 import { ValidationProvider, ValidationObserver } from 'vee-validate'
+//import * as rules from 'vee-validate/dist/rules'
+//import { extend } from 'vee-validate'
 import BaseInput from '@/components/Inputs/BaseInput.vue'
 import BaseButton from '@/components/BaseButton.vue'
+import RegeneratorRuntime from 'regenerator-runtime'
+
+/*
+
+Object.keys(rules).forEach((rule) => {
+  extend(rule, {
+    ...rules[rule], // copies rule configuration
+    message: messages[rule], // assign message
+  })
+})
+
+*/
 
 global.localVue = createLocalVue()
 
@@ -12,6 +26,8 @@ global.localVue.use(ElementUI)
 global.localVue.use(BootstrapVue)
 global.localVue.use(Vuex)
 global.localVue.use(IconsPlugin)
+global.localVue.use(RegeneratorRuntime)
+//global.localVue.use()
 global.localVue.component(BaseInput.name, BaseInput)
 global.localVue.component('validation-provider', ValidationProvider)
 global.localVue.component('validation-observer', ValidationObserver)
