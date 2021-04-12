f79767a9a8fc982b654b28d10f86eceeb8e6fa2b
Moriz Wahl <moriz.wahl@gmx.de>
2021-04-12 21:03:39 +0200

    added mode: history to get rid of the # in the routes

--- frontend/src/routes/router.js
+++ frontend/src/routes/router.js
@@ -8,6 +8,7 @@ Vue.use(VueRouter)
 const router = new VueRouter({
   routes, // short for routes: routes
   linkActiveClass: 'active',
+  mode: 'history',
   scrollBehavior: (to, from, savedPosition) => {
     if (savedPosition) {
       return savedPosition
