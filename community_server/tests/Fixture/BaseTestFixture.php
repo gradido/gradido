57144956b5436d4bf2fafd1bd394d0ce2a4ae7e7
Dario Rekowski on RockPI <dario.rekowski@gmx.de>
2021-04-14 10:00:44 +0000

    add function for easy filling test fixtures with datas from db (export)

---
+++ community_server/tests/Fixture/BaseTestFixture.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class BaseTestFixture extends TestFixture
+{
+ 
+    protected function sqlEntrysToRecords($sql_entries, $fields) {
+        $field_array_keys = array_keys($fields);
+        $records = [];
+        foreach($sql_entries as $sql_entry) {
+            $record = [];
+            foreach($sql_entry as $i => $value) {
+                $field = $field_array_keys[$i];
+                $record[$field] = $value;
+            }
+            $records[] = $record;
+        }
+    }
+}
