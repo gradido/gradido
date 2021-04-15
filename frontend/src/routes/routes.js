7373e2930f55f05b380a9e2ec4e993edaf1b02c4
Moriz Wahl <moriz.wahl@gmx.de>
2021-04-15 18:41:28 +0200

    refactor router to avoid redirects and misuse of children

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,66 +1,49 @@
-import DashboardLayout from '@/views/Layout/DashboardLayout_gdd.vue'
-import AuthLayoutGDD from '@/views/Layout/AuthLayout_gdd.vue'
-
 import NotFound from '@/views/NotFoundPage.vue'
 
 const routes = [
   {
-    path: '/login',
-    redirect: 'login',
-    component: AuthLayoutGDD,
-    children: [
-      {
-        path: '/login',
-        component: () => import('../views/Pages/Login.vue'),
-      },
-      {
-        path: '/thx',
-        component: () => import('../views/Pages/thx.vue'),
-      },
-      {
-        path: '/register',
-        component: () => import('../views/Pages/Register.vue'),
-      },
-      {
-        path: '/password',
-        component: () => import('../views/Pages/Password.vue'),
-      },
-      {
-        path: '/explorer',
-        name: 'Explorer',
-        component: () => import('../views/Pages/Explorer.vue'),
-      },
-    ],
+    path: '/overview',
+    component: () => import('../views/KontoOverview.vue'),
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
-    path: '/overview',
-    redirect: 'overview',
-    component: DashboardLayout,
-    children: [
-      {
-        path: '/overview',
-        component: () => import('../views/KontoOverview.vue'),
-        meta: {
-          requiresAuth: true,
-        },
-      },
-      {
-        path: '/profile',
-        component: () => import('../views/Pages/UserProfileCard.vue'),
-      },
-      {
-        path: '/profileedit',
-        component: () => import('../views/Pages/UserProfileEdit.vue'),
-      },
-      {
-        path: '/activity',
-        component: () => import('../views/Pages/UserProfileActivity.vue'),
-      },
-      {
-        path: '/transactions',
-        component: () => import('../views/Pages/UserProfileTransactionList.vue'),
-      },
-    ],
+    path: '/profile',
+    component: () => import('../views/Pages/UserProfileCard.vue'),
+  },
+  {
+    path: '/profileedit',
+    component: () => import('../views/Pages/UserProfileEdit.vue'),
+  },
+  {
+    path: '/activity',
+    component: () => import('../views/Pages/UserProfileActivity.vue'),
+  },
+  {
+    path: '/transactions',
+    component: () => import('../views/Pages/UserProfileTransactionList.vue'),
+  },
+  {
+    path: '/login',
+    component: () => import('../views/Pages/Login.vue'),
+  },
+  {
+    path: '/thx',
+    component: () => import('../views/Pages/thx.vue'),
+  },
+  {
+    path: '/register',
+    component: () => import('../views/Pages/Register.vue'),
+  },
+  {
+    path: '/password',
+    component: () => import('../views/Pages/Password.vue'),
+  },
+  {
+    path: '/explorer',
+    name: 'Explorer',
+    component: () => import('../views/Pages/Explorer.vue'),
   },
   { path: '*', component: NotFound },
 ]
