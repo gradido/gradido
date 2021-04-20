f9e360ec3f0c606c36725bf39e666e9077879f8b
einhorn_b <dariofrodo@gmx.de>
2021-04-20 13:33:11 +0200

    prepare for testing

--- login_server/Dockerfiles/build_and_run.sh
+++ login_server/Dockerfiles/build_and_run.sh
@@ -3,7 +3,8 @@ cp build/conan* build_vol/
 cd build_vol 
 
 cmake -DCMAKE_BUILD_TYPE=Debug ..
+make -j$(nproc) protoc grpc_cpp_plugin
 make -j$(nproc) Gradido_LoginServer
 #echo "building done"
 chmod +x ./bin/Gradido_LoginServer
-#./bin/Gradido_LoginServer
+./bin/Gradido_LoginServer
