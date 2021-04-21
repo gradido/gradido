457aedc54b87ea46baced5789b12fe1fecc28607
Dario Rekowski on RockPI <dario.rekowski@gmx.de>
2021-04-21 13:25:09 +0000

    fix linting

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -11,7 +11,7 @@ const environment = {
 
 const server = {
   LOGIN_API_URL: process.env.LOGIN_API_URL || 'http://localhost/login_api/',
-  COMMUNITY_API_URL:process.env.COMMUNITY_API_URL || 'http://localhost/api/',
+  COMMUNITY_API_URL: process.env.COMMUNITY_API_URL || 'http://localhost/api/',
 }
 
 const CONFIG = {
