7f837fdeaa86849c1783791a14be69cf1eeb9be5
ogerly <landmatrosen@googlemail.com>
2021-04-27 09:55:17 +0200

    reset link remove on login.vue

--- frontend/src/views/Pages/Login.vue
+++ frontend/src/views/Pages/Login.vue
@@ -87,10 +87,7 @@
                 {{ $t('site.login.new_wallet') }}
               </router-link>
             </b-col>
-          </b-row>
-          <b-row>
-            <b-col><router-link to="/reset">reset</router-link></b-col>
-          </b-row>
+          </b-row>         
         </b-col>
       </b-row>
     </b-container>
