d4dc54ab2b97302b71b1ef5aca51eb9862983d90
ogerly <landmatrosen@googlemail.com>
2021-04-29 14:04:47 +0200

    change mobile view with padding + yarn lint

--- frontend/src/views/Layout/AuthLayout_gdd.vue
+++ frontend/src/views/Layout/AuthLayout_gdd.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="main-content">
-    <template>
-      <div class="main-content">
-        <router-view></router-view>
-        <content-footer v-if="!$route.meta.hideFooter"></content-footer>
-      </div>
-    </template>
+  <div>
+    <router-view class="p-3"></router-view>
+    <content-footer v-if="!$route.meta.hideFooter"></content-footer>
   </div>
 </template>
 <script>
