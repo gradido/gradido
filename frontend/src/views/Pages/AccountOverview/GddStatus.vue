e8e828baf193a50231bf02391de2ce036472cee7
ogerly <landmatrosen@googlemail.com>
2021-05-12 15:21:42 +0200

    status add pending, sorry mogge

--- frontend/src/views/Pages/AccountOverview/GddStatus.vue
+++ frontend/src/views/Pages/AccountOverview/GddStatus.vue
@@ -3,13 +3,13 @@
     <b-row>
       <b-col>
         <b-card style="background-color: #ebebeba3 !important">
-          <b>{{ $n(balance) }}</b>
+          <b>{{ pending ? '—' : $n(balance) }}</b>
           GDD
         </b-card>
       </b-col>
       <b-col>
         <b-card class="lg-h2 text-right" style="background-color: #ebebeba3 !important">
-          {{ $n(GdtBalance) }} GDT
+          {{ pending ? '—' : $n(GdtBalance) }} GDT
         </b-card>
       </b-col>
     </b-row>
