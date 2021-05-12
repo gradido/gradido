58faba9709eabd8559498772d878d3ee5756e045
ogerly <landmatrosen@googlemail.com>
2021-05-12 09:37:12 +0200

    language switch component in authlayout_gdd

--- frontend/src/views/Layout/AuthLayout_gdd.vue
+++ frontend/src/views/Layout/AuthLayout_gdd.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="wrapper">
-    <div class="main-content">
+    <div class="main-content mt-4">
       <router-view></router-view>
+      <language-switch class="text-center mb-5 mt-5" />
       <content-footer v-if="!$route.meta.hideFooter"></content-footer>
     </div>
   </div>
 </template>
 <script>
 import ContentFooter from './ContentFooter.vue'
+import LanguageSwitch from '@/components/LanguageSwitch.vue'
 export default {
   components: {
     ContentFooter,
+    LanguageSwitch,
   },
 }
 </script>
