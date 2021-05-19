8b9aa31339b230721d04bce07882683dbc3c9eed
Moriz Wahl <moriz.wahl@gmx.de>
2021-05-19 17:13:34 +0200

    remove changes from other PR

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -49,14 +49,6 @@ const routes = [
   {
     path: '/thx',
     component: () => import('../views/Pages/thx.vue'),
-    beforeEnter: (to, from, next) => {
-      const validFrom = ['/password', '/reset', '/register']
-      if (!validFrom.includes(from.path)) {
-        next({ path: '/login' })
-      } else {
-        next()
-      }
-    },
   },
   {
     path: '/password',
--- frontend/src/views/Pages/thx.vue
+++ frontend/src/views/Pages/thx.vue
@@ -5,54 +5,12 @@
       <b-container>
         <div class="header-body text-center mb-7">
           <p class="h1">{{ $t('site.thx.title') }}</p>
-          <p class="h4">{{ $t(displaySetup.subtitle) }}</p>
+          <p class="h4">{{ $t('site.thx.subtitle') }}</p>
           <hr />
-          <b-button :to="displaySetup.linkTo">{{ $t(displaySetup.button) }}</b-button>
+          <b-button to="/login">{{ $t('login') }}</b-button>
         </div>
       </b-container>
     </div>
     <!-- Page content -->
   </div>
 </template>
-<script>
-const textFields = {
-  '/password': {
-    subtitle: 'site.thx.email',
-    button: 'login',
-    linkTo: '/login',
-  },
-  '/reset': {
-    subtitle: 'site.thx.reset',
-    button: 'site.login.signin',
-    linkTo: '/overview',
-  },
-  '/register': {
-    subtitle: 'site.thx.register',
-    button: 'site.login.signin',
-    linkTo: '/overview',
-  },
-}
-
-export default {
-  name: 'Thx',
-  data() {
-    return {
-      comingFrom: null,
-      displaySetup: {},
-    }
-  },
-  beforeRouteEnter(to, from, next) {
-    next((vm) => {
-      vm.comingFrom = from.path
-    })
-  },
-  methods: {
-    setupDisplay() {
-      this.displaySetup = textFields[this.comingFrom]
-    },
-  },
-  created() {
-    this.setupDisplay()
-  },
-}
-</script>
