9e82998f92e37ee22e91e36144048d90b0970e20
Dario Rekowski on RockPI <dario.rekowski@gmx.de>
2021-06-01 11:54:59 +0000

    remove not longer used field from table

--- community_server/tests/Fixture/TransactionCreationsFixture.php
+++ community_server/tests/Fixture/TransactionCreationsFixture.php
@@ -17,7 +17,6 @@ class TransactionCreationsFixture extends BaseTestFixture
         'transaction_id' => ['type' => 'integer', 'length' => 10, 'unsigned' => true, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
         'state_user_id' => ['type' => 'integer', 'length' => 10, 'unsigned' => true, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
         'amount' => ['type' => 'biginteger', 'length' => 20, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
-        'ident_hash' => ['type' => 'binary', 'length' => 32, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
         'target_date' => ['type' => 'timestamp', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
         '_constraints' => [
             'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
@@ -36,8 +35,8 @@ class TransactionCreationsFixture extends BaseTestFixture
     public function init()
     {
          $sql = [
-            [2, 1, 4, 10000000, '0000000000000000000000000000000000000000000000000000000000000000', '2021-01-01 00:00:00'],
-            [3, 2, 1, 10000000, '0000000000000000000000000000000000000000000000000000000000000000', '2021-01-01 00:00:00']
+            [2, 1, 4, 10000000, '2021-01-01 00:00:00'],
+            [3, 2, 1, 10000000, '2021-01-01 00:00:00']
         ];
         $this->records = $this->sqlEntrysToRecords($sql, $this->fields);
         parent::init();
