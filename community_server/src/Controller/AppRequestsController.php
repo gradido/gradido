fdee25e90127f84aecad8034192a63e8e3a50c6d
Dario Rekowski on RockPI <dario.rekowski@gmx.de>
2021-06-07 09:10:04 +0000

    forward group_alias or group_id for transactions

--- community_server/src/Controller/AppRequestsController.php
+++ community_server/src/Controller/AppRequestsController.php
@@ -81,6 +81,18 @@ class AppRequestsController extends AppController
         }
         return true;
     }
+    
+    private function copyOptionalFields($fields, &$param, $data = null) {
+        if($data == null) {
+            $data = $this->request->input('json_decode');
+        }
+        
+        foreach($fields as $field) {
+            if(isset($data->$field)) {
+                $param[$field] = $data->$field;
+            } 
+        }
+    }
 
     private function rewriteKeys(&$data, $replaceKeys)
     {
@@ -115,6 +127,7 @@ class AppRequestsController extends AppController
         if($required_fields !== true) {
             return $required_fields;
         }
+        $this->copyOptionalFields(['group_id', 'group_alias'], $param, $data);
         
         if(floatval($param['amount']) <= 0.0) {
             return ['state' => 'error', 'msg' => 'amount is invalid', 'details' => $param['amount']];
@@ -155,9 +168,6 @@ class AppRequestsController extends AppController
         $required_fields = $this->checkAndCopyRequiredFields(['target_date'], $params, $data);
         if($required_fields !== true) {
             return $this->returnJson($required_fields);
-        }
-        if(!isset($params['memo']) || strlen($params['memo']) < 5 || strlen($params['memo']) > 150) {
-            return $this->returnJson(['state' => 'error', 'msg' => 'memo is not set or not in expected range [5;150]']);
         }
         $params['transaction_type'] = 'transfer';
         
@@ -276,7 +286,8 @@ class AppRequestsController extends AppController
         $this->viewBuilder()->setLayout('ajax');
         $login_result = $this->requestLogin($session_id, false);
         if($login_result !== true) {
-            return $this->returnJson($login_result);
+            $this->set('body', $login_result);
+            return;
         }
         $session = $this->getRequest()->getSession();
         $user = $session->read('StateUser');
@@ -285,16 +296,24 @@ class AppRequestsController extends AppController
         
         $state_balance = $state_balances_table->find()->where(['state_user_id' => $user['id']])->first();
 
-        if(!$state_balance) {
-            return $this->returnJson(['state' => 'success', 'balance' => 0]);
-        }
+        
         $now = new FrozenTime();
-        $body = [
-            'state' => 'success',
-            'balance' => $state_balance->amount,
-            'decay' => $state_balance->partDecay($now),
-            'decay_date' => $now
-        ];
+        if(!$state_balance) {
+            $body = [
+                'state' => 'success',
+                'balance' => 0,
+                'decay' => 0
+            ];
+        } else {
+        
+            $body = [
+                'state' => 'success',
+                'balance' => $state_balance->amount,
+                'decay' => $state_balance->partDecay($now),
+            ];
+        }
+        
+        $body['decay_date'] = $now;
         $this->set('body', $body);
     }
     
@@ -302,35 +321,31 @@ class AppRequestsController extends AppController
     {
         $this->viewBuilder()->setLayout('ajax');
         $startTime = microtime(true);
+        
         $login_result = $this->requestLogin($session_id, false);
+        
         if($login_result !== true) {
             return $this->returnJson($login_result);
         }
         $session = $this->getRequest()->getSession();
         $user = $session->read('StateUser');
-        
-        
+                
         $stateBalancesTable = TableRegistry::getTableLocator()->get('StateBalances');
         $stateUserTransactionsTable = TableRegistry::getTableLocator()->get('StateUserTransactions');
         $transactionsTable  = TableRegistry::getTableLocator()->get('Transactions');
         
-        
         $stateBalancesTable->updateBalances($user['id']);
-
+        
         $gdtSum = 0;        
+        
         $gdtEntries = $this->JsonRequestClient->sendRequestGDT(['email' => $user['email']], 'GdtEntries' . DS . 'sumPerEmailApi');
-
+        
         if('success' == $gdtEntries['state'] && 'success' == $gdtEntries['data']['state']) {
           $gdtSum = intval($gdtEntries['data']['sum']);
         } else {
           $this->addAdminError('StateBalancesController', 'overview', $gdtEntries, $user['id'] ? $user['id'] : 0);
         }
 
-        $stateUserTransactions_total = $stateUserTransactionsTable
-                                        ->find()
-                                        ->select(['id'])
-                                        ->where(['state_user_id' => $user['id']])
-                                        ->contain([]);
         
         $stateUserTransactionsQuery = $stateUserTransactionsTable
                                         ->find()
@@ -343,17 +358,18 @@ class AppRequestsController extends AppController
         $decay = true;
         $transactions = [];
         $transactions_from_db = $stateUserTransactionsQuery->toArray();
-        if($stateUserTransactionsQuery->count() > 0) {
+        
+        if(count($transactions_from_db)) {
             if($orderDirection == 'DESC') {
                 $transactions_from_db = array_reverse($transactions_from_db);
             }
+            
             $transactions = $transactionsTable->listTransactionsHumanReadable($transactions_from_db, $user, $decay);
             
             if($orderDirection == 'DESC') {
                 $transactions = array_reverse($transactions);
             }
-            
-        }
+        } 
         
         $state_balance = $stateBalancesTable->find()->where(['state_user_id' => $user['id']])->first();
         
@@ -361,7 +377,7 @@ class AppRequestsController extends AppController
             'state' => 'success',
             'transactions' => $transactions,
             'transactionExecutingCount' => $session->read('Transactions.executing'),
-            'count' => $stateUserTransactions_total->count(),
+            'count' => $stateUserTransactionsQuery->count(),
             'gdtSum' => $gdtSum,
             'timeUsed' => microtime(true) - $startTime
         ];
@@ -375,8 +391,8 @@ class AppRequestsController extends AppController
             $body['balance'] = $state_balance->amount;
             $body['decay'] = $stateBalancesTable->calculateDecay($state_balance->amount, $state_balance->record_date, $now);
         }
-        
-        $this->set('body', $body);
+
+        $this->set('body', $body);       
     }
     
     private function acquireAccessToken($session_id)
