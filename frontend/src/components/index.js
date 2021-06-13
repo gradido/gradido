3e3048cf3969f15526814f07910fa421394568bd
ogerly <landmatrosen@googlemail.com>
2021-06-13 05:45:59 +0200

    component tab, tabs removed

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,11 +1,9 @@
 import BaseNav from './Navbar/BaseNav'
 import NavbarToggleButton from './Navbar/NavbarToggleButton'
 
-import TabPane from './Tabs/Tab.vue'
-import Tabs from './Tabs/Tabs.vue'
 import Collapse from './Collapse/Collapse.vue'
 import CollapseItem from './Collapse/CollapseItem.vue'
 
 import SidebarPlugin from './SidebarPlugin'
 
-export { SidebarPlugin, BaseNav, NavbarToggleButton, TabPane, Tabs, Collapse, CollapseItem }
+export { SidebarPlugin, BaseNav, NavbarToggleButton, Collapse, CollapseItem }
