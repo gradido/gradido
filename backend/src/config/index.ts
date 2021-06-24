00639965122b2a0d35ed4fa6b4ad2c6c953325f0
einhornimmond <dario.rekowski@gmx.de>
2021-06-24 17:32:54 +0200

    get groups from login server, first running api call to login server

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,5 +1,8 @@
 // ATTENTION: DO NOT PUT ANY SECRETS IN HERE (or the .env)
 
+import dotenv from 'dotenv'
+dotenv.config()
+
 const server = {
   LOGIN_API_URL: process.env.LOGIN_API_URL || 'http://localhost/login_api/',
   COMMUNITY_API_URL: process.env.COMMUNITY_API_URL || 'http://localhost/api/',
