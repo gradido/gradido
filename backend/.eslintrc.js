94ac32c86ac5b781100228e7908d1483f50853ef
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-06-24 21:35:34 +0200

    removed development environment linting exception

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   // add your custom rules here
   rules: {
     'no-console': ['error'],
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': 'error',
     'prettier/prettier': [
       'error',
       {
