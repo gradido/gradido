b45b7cf03b3273fdd580d922cbd506f0f023dcd4
Moriz Wahl <moriz.wahl@gmx.de>
2021-07-06 14:28:34 +0200

    load all validation rules for unit tests

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import DashboardPlugin from './plugins/dashboard-plugin'
 import App from './App.vue'
 import i18n from './i18n.js'
 import './validation-rules'
+import { loadAllRules } from './validation-rules'
 
 import { store } from './store/store'
 
@@ -11,6 +12,8 @@ import router from './routes/router'
 // plugin setup
 Vue.use(DashboardPlugin)
 Vue.config.productionTip = false
+
+loadAllRules(i18n)
 
 router.beforeEach((to, from, next) => {
   if (to.meta.requiresAuth && !store.state.sessionId) {
