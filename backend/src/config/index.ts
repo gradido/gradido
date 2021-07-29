5c423f6c72f98f0512553bc7b448427670fb6a20
Hannes Heine <hannes.heine@gmail.com>
2021-07-29 07:27:59 +0200

    Changed the default config value so that the programm get the right config on localhost.

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -8,8 +8,8 @@ const server = {
   JWT_SECRET: process.env.JWT_SECRET || 'secret123',
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '10m',
   GRAPHIQL: process.env.GRAPHIQL === 'true' || false,
-  LOGIN_API_URL: process.env.LOGIN_API_URL || 'http://localhost/login_api/',
-  // COMMUNITY_API_URL: process.env.COMMUNITY_API_URL || 'http://localhost/api/',
+  LOGIN_API_URL: process.env.LOGIN_API_URL || 'http://localhost/', // login_api/',
+  COMMUNITY_API_URL: process.env.COMMUNITY_API_URL || 'http://ngninx/api/',
 }
 
 // This is needed by graphql-directive-auth
