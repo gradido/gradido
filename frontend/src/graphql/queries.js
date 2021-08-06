259b7547d49eca43ae1259378e5a093785871725
Hannes Heine <hannes.heine@gmail.com>
2021-08-06 20:01:04 +0200

    Implementing the updateUserInfos for language.

--- frontend/src/graphql/queries.js
+++ frontend/src/graphql/queries.js
@@ -15,8 +15,52 @@ export const login = gql`
     }
   }
 `
+
 export const logout = gql`
   query($sessionId: Float!) {
     logout(sessionId: $sessionId)
+  }
+`
+
+export const resetPassword = gql`
+  query($sessionId: Float!, $email: String!, $password: String!) {
+    resetPassword(sessionId: $sessionId, email: $email, password: $password)
+  }
+`
+
+export const loginViaEmailVerificationCode = gql`
+  query($optin: String!) {
+    loginViaEmailVerificationCode(optin: $optin) {
+      sessionId
+      email
+    }
+  }
+`
+
+export const updateUserInfos = gql`
+  query(
+    $sessionId: Float!
+    $email: String!
+    $firstName: String
+    $lastName: String
+    $description: String
+    $username: String
+    $password: String
+    $passwordNew: String
+    $locale: String
+  ) {
+    updateUserInfos(
+      sessionId: $sessionId
+      email: $email
+      firstName: $firstName
+      lastName: $lastName
+      description: $description
+      username: $username
+      password: $password
+      passwordNew: $passwordNew
+      language: $locale
+    ) {
+      validValues
+    }
   }
 `
