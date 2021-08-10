f04f05778b41d8ec300364b54ca330a0fcd19908
Moriz Wahl <moriz.wahl@gmx.de>
2021-08-10 16:19:44 +0200

    fix tests

--- frontend/src/views/Pages/AccountOverview/GddTransactionList.vue
+++ frontend/src/views/Pages/AccountOverview/GddTransactionList.vue
@@ -24,10 +24,7 @@
                 {{ $t('form.memo') }}
               </small>
             </div>
-            <div v-if="date" class="text-sm">
-              {{ $t('form.date') }}
-            </div>
-            <div v-if="decay" class="pt-2">
+            <div v-if="decay" class="text-sm">
               <b-icon v-if="type != 'decay'" icon="droplet-half" height="15" class="mb-3" />
             </div>
           </div>
