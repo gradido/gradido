d480cadf93d0a50460a63966ab41a7a62d384d7b
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-08-23 00:00:35 +0200

    fixed build js up/down/reset commands started to do some more prepare stuff

--- database/src/prepare.ts
+++ database/src/prepare.ts
@@ -9,7 +9,7 @@
 import { createConnection } from 'mysql'
 import CONFIG from './config'
 
-export default async () => {
+export default async (): Promise<void> => {
   const con = createConnection({
     host: CONFIG.DB_HOST,
     port: CONFIG.DB_PORT,
@@ -24,4 +24,19 @@ export default async () => {
     CREATE DATABASE IF NOT EXISTS ${CONFIG.DB_DATABASE} 
       DEFAULT CHARACTER SET utf8mb4
       DEFAULT COLLATE utf8mb4_unicode_ci;`)
+
+  // Check if old migration table is present, delete if needed
+  await con.query(
+    `SHOW COLUMNS FROM \`${CONFIG.DB_DATABASE}\`.\`migrations\` LIKE 'version';`,
+    (err, result /* , fields */) => {
+      if (err) throw err
+      if (result.length > 0) {
+        con.query(`DROP TABLE \`${CONFIG.DB_DATABASE}\`.\`migrations\``)
+        // eslint-disable-next-line no-console
+        console.log('Found and dropped old migrations table')
+      }
+    },
+  )
+
+  await con.end()
 }
