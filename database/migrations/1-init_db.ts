7ed56ed5ab1f79b9491314a33c6c45b7a12d4bec
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-08-22 13:30:26 +0200

    database migrations - initial commit

---
+++ database/migrations/1-init_db.ts
@@ -0,0 +1,13 @@
+export async function upgrade(queryFn: (query: string, values?: any[]) => Promise<Array<any>>) {
+  // write upgrade logic as parameter of queryFn
+  await queryFn(`
+    CREATE TABLE TEST (ID INTEGER NULL, NAME VARCHAR(20) NULL);
+  `);
+}
+    
+export async function downgrade(queryFn: (query: string, values?: any[]) => Promise<Array<any>>) {
+  // write downgrade logic as parameter of queryFn
+  await queryFn(`
+    DROP TABLE TEST;
+  `);
+}
