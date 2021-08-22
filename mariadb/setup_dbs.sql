8547362609c3c20e3ccd97ef4f424beb944b4d59
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-08-22 22:39:19 +0200

    removed skeema from community database

--- mariadb/setup_dbs.sql
+++ mariadb/setup_dbs.sql
@@ -1,9 +1,6 @@
 create database gradido_login 
 		DEFAULT CHARACTER SET utf8mb4
 		DEFAULT COLLATE utf8mb4_unicode_ci;
-create database gradido_community 
-		DEFAULT CHARACTER SET utf8mb4
-		DEFAULT COLLATE utf8mb4_unicode_ci;
 create database IF NOT EXISTS _skeema_tmp
 		DEFAULT CHARACTER SET utf8mb4
 		DEFAULT COLLATE utf8mb4_unicode_ci;
