73aaada658e49d4d9887404eafb6c34d88252de9
Moriz Wahl <moriz.wahl@gmx.de>
2021-08-30 18:40:28 +0200

    validation of sessionID via API call to login server

--- backend/src/auth/auth.ts
+++ backend/src/auth/auth.ts
@@ -2,14 +2,19 @@
 
 import { AuthChecker } from 'type-graphql'
 import decode from '../jwt/decode'
+import { apiGet } from '../apis/loginAPI'
+import CONFIG from '../config'
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
-export const isAuthorized: AuthChecker<any> = ({ root, args, context, info }, roles) => {
+export const isAuthorized: AuthChecker<any> = async ({ root, args, context, info }, roles) => {
   if (context.token) {
     const decoded = decode(context.token)
     if (decoded.sessionId && decoded.sessionId !== 0) {
+      const result = await apiGet(
+        `${CONFIG.LOGIN_API_URL}checkSessionState?session_id=${decoded.sessionId}`,
+      )
       context.sessionId = decoded.sessionId
-      return true
+      return result.success
     }
   }
   return false
