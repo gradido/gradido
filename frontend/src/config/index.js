9024534eaca7591866eba404b7ee8d94c91328e9
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-09-15 10:24:36 +0200

    restructured config, refactor BUILD_COMMIT

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,13 +1,31 @@
-// ATTENTION: DO NOT PUT ANY SECRETS IN HERE (or the .env)
+// ATTENTION: DO NOT PUT ANY SECRETS IN HERE (or the .env).
+//            The whole contents is exposed to the client
 
 // Load Package Details for some default values
 const pkg = require('../../package')
+
+const version = {
+  APP_VERSION: pkg.version,
+  BUILD_COMMIT:
+    process.env.BUILD_COMMIT ||
+    // the check for undefined is because of a conflict between webpack-dotenv and vue cli env filtering
+    (process.env.VUE_APP_BUILD_COMMIT !== 'undefined' ? process.env.VUE_APP_BUILD_COMMIT : null) ||
+    null,
+  // self reference of `version.BUILD_COMMIT` is not possible at this point, hence the duplicate code
+  BUILD_COMMIT_SHORT: (
+    process.env.BUILD_COMMIT ||
+    (process.env.VUE_APP_BUILD_COMMIT !== 'undefined' ? process.env.VUE_APP_BUILD_COMMIT : null) ||
+    '0000000'
+  ).substr(0, 7),
+  // unused
+  // BUILD_DATE: process.env.BUILD_DATE || process.env.VUE_APP_BUILD_DATE || '1970-01-01T00:00:00.00Z',
+  // BUILD_VERSION: process.env.BUILD_VERSION || process.env.VUE_APP_BUILD_VERSION || '0.0.0.0',
+}
 
 const environment = {
   NODE_ENV: process.env.NODE_ENV,
   DEBUG: process.env.NODE_ENV !== 'production' || false,
   PRODUCTION: process.env.NODE_ENV === 'production' || false,
-  ALLOW_REGISTER: process.env.ALLOW_REGISTER !== 'false',
 }
 
 const server = {
@@ -16,17 +34,15 @@ const server = {
   GRAPHQL_URI: process.env.GRAPHQL_URI || 'http://localhost:4000/graphql',
 }
 
-// eslint-disable-next-line no-console
-console.log('hash: %o', process.env.VUE_APP_BUILD_COMMIT)
+const options = {
+  ALLOW_REGISTER: process.env.ALLOW_REGISTER !== 'false',
+}
 
 const CONFIG = {
+  ...version,
   ...environment,
   ...server,
-  APP_VERSION: pkg.version,
-  COMMIT_HASH:
-    process.env.VUE_APP_BUILD_COMMIT === 'undefined'
-      ? '00000000'
-      : process.env.VUE_APP_BUILD_COMMIT,
+  ...options,
 }
 
 export default CONFIG
