cebf92c451fbc5c525019a9102a7a44beecb6e23
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-09-21 10:34:56 +0200

    fixed migrations directory to default to the production version. This is the downside of this patch - the .env file has to be different in dev and production version

--- database/src/config/index.ts
+++ database/src/config/index.ts
@@ -13,7 +13,7 @@ const database = {
 
 const migrations = {
   MIGRATIONS_TABLE: process.env.MIGRATIONS_TABLE || 'migrations',
-  MIGRATIONS_DIRECTORY: process.env.MIGRATIONS_DIRECTORY || './migrations/',
+  MIGRATIONS_DIRECTORY: process.env.MIGRATIONS_DIRECTORY || './build/migrations/',
 }
 
 const CONFIG = { ...database, ...migrations }
