8c189adf52b87499aa7a7623d7433b289ce32d2e
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-09-28 13:31:28 +0200

    simplify migrations query

--- backend/src/typeorm/getDBVersion.ts
+++ backend/src/typeorm/getDBVersion.ts
@@ -1,11 +1,8 @@
-import { getConnection } from 'typeorm'
 import { Migration } from './entity/Migration'
 
 const getDBVersion = async (): Promise<string | null> => {
-  const connection = getConnection()
-  const migrations = connection.getRepository(Migration)
   try {
-    const dbVersion = await migrations.findOne({ order: { version: 'DESC' } })
+    const dbVersion = await Migration.findOne({ order: { version: 'DESC' } })
     return dbVersion ? dbVersion.fileName : null
   } catch (error) {
     return null
