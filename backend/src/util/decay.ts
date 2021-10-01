f0ddb3e3f91e38380452787fd13d5c2f214a81be
Moriz Wahl <moriz.wahl@gmx.de>
2021-10-01 13:11:58 +0200

    get the tests working again (skipped)

--- backend/src/util/decay.ts
+++ backend/src/util/decay.ts
@@ -54,4 +54,4 @@ async function calculateDecayWithInterval(
   return result
 }
 
-export { calculateDecay, calculateDecayWithInterval }
+export default { calculateDecay, calculateDecayWithInterval }
