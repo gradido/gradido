e62307988069346431501c8f43cf08037874cbed
einhorn_b <dariofrodo@gmx.de>
2021-10-02 16:46:45 +0200

    fix lint

--- backend/src/graphql/resolvers/GdtResolver.ts
+++ backend/src/graphql/resolvers/GdtResolver.ts
@@ -30,7 +30,6 @@ export class GdtResolver {
     if (!resultGDT.success) {
       throw new Error(resultGDT.data)
     }
-    console.log(resultGDT.data)
     return new GdtEntryList(resultGDT.data)
   }
 }
