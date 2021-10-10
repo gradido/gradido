ecb0fe287d880649e573ed07cff1a63780caffd0
Einhornimmond <git@einhornimmond.de>
2021-10-10 16:17:10 +0200

    fix test and more checks

--- backend/src/util/decay.ts
+++ backend/src/util/decay.ts
@@ -10,6 +10,12 @@ async function calculateDecay(amount: number, from: Date, to: Date): Promise<num
   if (amount === undefined || !from || !to) {
     throw new Error('at least one parameter is undefined')
   }
+  if(from === to) { 
+    return amount
+  }
+  if(to < from) {
+    throw new Error('to < from, so the target date is in the past?')
+  }
   // load decay start block
   const transactionRepository = getCustomRepository(TransactionRepository)
   const decayStartBlock = await transactionRepository.findDecayStartBlock()
