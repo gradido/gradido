14e3c33cc5f4c70fccea1ea4a5d81173d07b975f
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-10-12 01:27:20 +0200

    more error logging for debugging purposes

--- backend/src/typeorm/getDBVersion.ts
+++ backend/src/typeorm/getDBVersion.ts
@@ -1,10 +1,13 @@
+import { getRepository } from 'typeorm'
 import { Migration } from '@entity/Migration'
 
 const getDBVersion = async (): Promise<string | null> => {
   try {
-    const dbVersion = await Migration.findOne({ order: { version: 'DESC' } })
+    const dbVersion = await getRepository(Migration).findOne({ order: { version: 'DESC' } })
     return dbVersion ? dbVersion.fileName : null
   } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error)
     return null
   }
 }
