a600eaa4b80a8262e92f9735e7655edd0946b0b0
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-10-12 10:37:43 +0200

    unify database connection database & backend - also log errors

--- database/src/typeorm/connection.ts
+++ database/src/typeorm/connection.ts
@@ -16,7 +16,10 @@ const connection = async (): Promise<Connection | null> => {
       entities,
       synchronize: false,
     })
-  } catch (error) {}
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error)
+  }
 
   return con
 }
