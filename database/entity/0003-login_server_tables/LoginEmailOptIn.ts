d097e5cd76f065eabae8abb0ccdb200545c5cddd
elweyn <heine.hannes@gmail.com>
2021-11-04 12:55:04 +0100

    Created the LoginEmailOptIn table entity.

---
+++ database/entity/0003-login_server_tables/LoginEmailOptIn.ts
@@ -0,0 +1,26 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+
+// Moriz: I do not like the idea of having two user tables
+@Entity('login_email_opt_in')
+export class LoginEmailOptIn extends BaseEntity {
+  @PrimaryGeneratedColumn('increment', { unsigned: true })
+  id: number
+
+  @Column()
+  userId: number
+
+  @Column({ name: 'verification_code', type: 'bigint', unsigned: true })
+  verificationCode: BigInt
+
+  @Column({ name: 'email_opt_in_type_id' })
+  emailOptInTypeId: number
+
+  @Column({ name: 'created', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date
+
+  @Column({ name: 'resend_count', default: 0 })
+  resendCount: number
+
+  @Column({ name: 'updated', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date
+}
