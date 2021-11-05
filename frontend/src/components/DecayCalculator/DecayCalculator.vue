5f585c15f7ce52295d8c95631089349abf93f2b7
ogerly <fridolin@tutanota.com>
2021-11-05 17:34:03 +0100

    add [200~

--- frontend/src/components/DecayCalculator/DecayCalculator.vue
+++ frontend/src/components/DecayCalculator/DecayCalculator.vue
@@ -1,3 +1,42 @@
 <template>
-  <div id="decay_calculator">Decay Calculator</div>
+  <div id="decay_calculator">
+    <div>
+      <b-calendar v-model="value" :min="min" locale="en" v-on:selected="getDays"></b-calendar>
+    </div>
+    <b>{{ gdd }} GDD</b>
+    <div>days: {{ days }}</div>
+    <div>seconds: {{ seconds }}</div>
+    <div>
+      Vergänglichkeit:
+      <b>{{ decays }}</b>
+    </div>
+  </div>
 </template>
+<script>
+const now = new Date()
+
+export default {
+  data() {
+    return {
+      value: '',
+      min: new Date(now.getFullYear(), now.getMonth(), now.getDate()),
+      start: '',
+      end: '',
+      days: 0,
+      seconds: 0,
+      decays: 0,
+      gdd: 1000,
+    }
+  },
+  methods: {
+    getDays() {
+      const start = this.$moment(this.today)
+      const end = this.$moment(this.value)
+
+      this.days = end.diff(start, 'days')
+      this.seconds = end.diff(start, 'seconds')
+      this.decays = this.gdd - this.gdd * Math.pow(0.99999997802044727, this.seconds)
+    },
+  },
+}
+</script>
