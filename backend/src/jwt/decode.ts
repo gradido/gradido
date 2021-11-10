e76646b327fff16c17250e552f6e4b812799b31c
elweyn <heine.hannes@gmail.com>
2021-11-10 06:13:29 +0100

    Withdrew the sessionId from the JWT.

--- backend/src/jwt/decode.ts
+++ backend/src/jwt/decode.ts
@@ -2,27 +2,22 @@ import jwt, { JwtPayload } from 'jsonwebtoken'
 import CONFIG from '../config/'
 
 interface CustomJwtPayload extends JwtPayload {
-  sessionId: number
   pubKey: Buffer
 }
 
 type DecodedJwt = {
   token: string
-  sessionId: number
   pubKey: Buffer
 }
 
 export default (token: string): DecodedJwt => {
   if (!token) throw new Error('401 Unauthorized')
-  let sessionId = null
   let pubKey = null
   try {
     const decoded = <CustomJwtPayload>jwt.verify(token, CONFIG.JWT_SECRET)
-    sessionId = decoded.sessionId
     pubKey = decoded.pubKey
     return {
       token,
-      sessionId,
       pubKey,
     }
   } catch (err) {
