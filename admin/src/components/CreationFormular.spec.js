9c02869ea0901df94836706e3ac4c78b75425648
Moriz Wahl <moriz.wahl@gmx.de>
2021-11-19 15:51:29 +0100

    fix test CreationFormular

--- admin/src/components/CreationFormular.spec.js
+++ admin/src/components/CreationFormular.spec.js
@@ -3,13 +3,31 @@ import CreationFormular from './CreationFormular.vue'
 
 const localVue = global.localVue
 
-const mocks = { $moment: jest.fn() }
+const mocks = {
+  $moment: jest.fn(() => {
+    return {
+      format: jest.fn((m) => m),
+      subtract: jest.fn(() => {
+        return {
+          format: jest.fn((m) => m),
+        }
+      }),
+    }
+  }),
+}
+
+const propsData = {
+  type: '',
+  item: {},
+  creation: {},
+  itemsMassCreation: {},
+}
 
 describe('CreationFormular', () => {
   let wrapper
 
   const Wrapper = () => {
-    return mount(CreationFormular, { localVue, mocks })
+    return mount(CreationFormular, { localVue, mocks, propsData })
   }
 
   describe('mount', () => {
