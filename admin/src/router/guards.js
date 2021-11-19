5baeb0b0ef9ea027604ccb7ff9058a8e40b9ed91
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-11-19 19:56:37 +0100

    refactor routes and route guards

--- admin/src/router/guards.js
+++ admin/src/router/guards.js
@@ -1,7 +1,23 @@
 const addNavigationGuards = (router, store) => {
+  // store token on authenticate
   router.beforeEach((to, from, next) => {
+    console.log('token', to.path, from.path, to.query)
     // handle authentication
-    if (to.meta.requiresAuth && !store.state.token) {
+    if (to.path === '/authenticate' && to.query.token) {
+      console.log('token', to.query.token, to)
+      store.commit('token',to.query.token)
+      // to.query = {}
+      next({path: '/'})
+    } else {
+      next()
+    }
+  })
+
+  // protect all routes but not-found  
+  router.beforeEach((to, from, next) => {
+    console.log('protect', to.path, from.path)
+    // handle authentication
+    if (!store.state.token && to.path !== '/not-found') {
       next({ path: '/not-found' })
     } else {
       next()
