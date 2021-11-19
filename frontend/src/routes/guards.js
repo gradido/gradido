c5ec31343c0164f9c6fec52501c1ef11675824c6
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-11-19 20:54:49 +0100

    separate router.beforeach & implement the authenticate route

--- frontend/src/routes/guards.js
+++ frontend/src/routes/guards.js
@@ -1,12 +1,28 @@
 const addNavigationGuards = (router, store) => {
+  // handle publisherId
   router.beforeEach((to, from, next) => {
-    // handle publisherId
     const publisherId = to.query.pid
     if (publisherId) {
       store.commit('publisherId', publisherId)
       delete to.query.pid
     }
-    // handle authentication
+    next()
+  })
+
+  // store token on authenticate
+  router.beforeEach((to, from, next) => {
+    console.log('token', to.path, from.path, to.query)
+    if (to.path === '/authenticate' && to.query.token) {
+      console.log('token', to.query.token, to)
+      store.commit('token',to.query.token)
+      next({path: '/overview'})
+    } else {
+      next()
+    }
+  })
+  
+  // handle authentication
+  router.beforeEach((to, from, next) => {
     if (to.meta.requiresAuth && !store.state.token) {
       next({ path: '/login' })
     } else {
