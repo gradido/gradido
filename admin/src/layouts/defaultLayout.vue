d45048f3dfd3d63487663c6ebf5bd6146009f93f
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-11-19 19:57:32 +0100

    seperate app and layouts

---
+++ admin/src/layouts/defaultLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nav-bar class="wrapper-nav" />
+    <router-view class="wrapper p-3"></router-view>
+    <foo-ter />
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+import FooTer from '@/components/Footer.vue'
+export default {
+  name: 'defaultLayout',
+  components: {
+    NavBar,
+    FooTer,
+  },
+}
+</script>
