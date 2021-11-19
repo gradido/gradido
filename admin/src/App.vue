8e8dee1e4a4621eda437bd3ebf48a781d01d4643
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-11-19 21:26:12 +0100

    convenience mode to disable the auth mode for development

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
-    <default-layout v-if="$store.state.token" />
+    <default-layout v-if="showLayout" />
     <router-view v-else></router-view>
   </div>
 </template>
 
 <script>
 import defaultLayout from '@/layouts/defaultLayout.vue'
+import CONFIG from './config'
 
 export default {
   name: 'app',
   components: { defaultLayout },
+  data() {
+    return {
+      showLayout: CONFIG.DEBUG_DISABLE_AUTH || this.$store.state.token
+    }
+  }
 }
 </script>
