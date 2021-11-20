f21587ae3abb1ed8125d4de277bfbef990047176
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-11-20 16:37:25 +0100

    frontend lint

--- frontend/src/routes/guards.js
+++ frontend/src/routes/guards.js
@@ -11,16 +11,15 @@ const addNavigationGuards = (router, store) => {
 
   // store token on authenticate
   router.beforeEach((to, from, next) => {
-    console.log('token', to.path, from.path, to.query)
     if (to.path === '/authenticate' && to.query.token) {
-      console.log('token', to.query.token, to)
-      store.commit('token',to.query.token)
-      next({path: '/overview'})
+      // TODO verify user in order to get user data
+      store.commit('token', to.query.token)
+      next({ path: '/overview' })
     } else {
       next()
     }
   })
-  
+
   // handle authentication
   router.beforeEach((to, from, next) => {
     if (to.meta.requiresAuth && !store.state.token) {
