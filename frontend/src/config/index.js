7ba1ec848210d42e462cf5aabb51709ce5c259d9
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-11-20 16:37:25 +0100

    frontend lint

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -20,8 +20,7 @@ const environment = {
 
 const endpoints = {
   GRAPHQL_URI: process.env.GRAPHQL_URI || 'http://localhost:4000/graphql',
-  // TODO port
-  ADMIN_AUTH_URL: process.env.ADMIN_AUTH_URL || 'http://localhost/admin/authenticate?token=$1'
+  ADMIN_AUTH_URL: process.env.ADMIN_AUTH_URL || 'http://localhost/admin/authenticate?token=$1',
 }
 
 const options = {}
