0ae04898f4185af2352f8bccfc5c42ee79b474ef
Moriz Wahl <moriz.wahl@gmx.de>
2021-11-23 17:59:44 +0100

    add one to one relation from login_user to login_user_backup

--- database/entity/0003-login_server_tables/LoginUserBackup.ts
+++ database/entity/0003-login_server_tables/LoginUserBackup.ts
@@ -1,16 +1,18 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, JoinColumn, OneToOne } from 'typeorm'
+import { LoginUser } from '../LoginUser'
 
 @Entity('login_user_backups')
 export class LoginUserBackup extends BaseEntity {
   @PrimaryGeneratedColumn('increment', { unsigned: true })
   id: number
 
-  @Column({ name: 'user_id', nullable: false })
-  userId: number
-
   @Column({ type: 'text', name: 'passphrase', nullable: false })
   passphrase: string
 
   @Column({ name: 'mnemonic_type', default: -1 })
   mnemonicType: number
+
+  @OneToOne(() => LoginUser, { nullable: false })
+  @JoinColumn({ name: 'user_id' })
+  loginUser: LoginUser
 }
