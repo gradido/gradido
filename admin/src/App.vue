2443973891267a3f9cca66e4db5edee103a46335
ogerly <fridolin@tutanota.com>
2021-11-23 10:33:57 +0100

    app.vue reinstate

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,9 +1,19 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <nav-bar class="wrapper-nav" />
+    <router-view class="wrapper p-3"></router-view>
+    <foo-ter />
+  </div>
 </template>
 
 <script>
+import NavBar from '@/components/NavBar.vue'
+import FooTer from '@/components/Footer.vue'
 export default {
   name: 'App',
+  components: {
+    NavBar,
+    FooTer,
+  },
 }
 </script>
