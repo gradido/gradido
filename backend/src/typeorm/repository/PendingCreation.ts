bb5e2361d458dfab791cd29b23ecbac0fe7e3718
Hannes Heine <heine.hannes@gmail.com>
2021-11-24 13:06:38 +0100

    Withdrew test commit.

--- backend/src/typeorm/repository/PendingCreation.ts
+++ backend/src/typeorm/repository/PendingCreation.ts
@@ -2,6 +2,4 @@ import { EntityRepository, Repository } from 'typeorm'
 import { PendingCreation } from '@entity/PendingCreation'
 
 @EntityRepository(LoginUserBackup)
-export class PendingCreationRepository extends Repository<PendingCreation> {
-  // TODO: git commit --author="Hannes Heine <heine.hannes@gmail.com>"
-}
+export class PendingCreationRepository extends Repository<PendingCreation> {}
