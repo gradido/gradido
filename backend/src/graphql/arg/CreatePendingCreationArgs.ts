e011d0a803c8af44dfd9aa06694862cf6e44d478
ogerly <fridolin@tutanota.com>
2021-11-24 11:09:13 +0100

    Implemented the arguments for graphql.

---
+++ backend/src/graphql/arg/CreatePendingCreationArgs.ts
@@ -0,0 +1,16 @@
+import { ArgsType, Field } from 'type-graphql'
+
+@ArgsType()
+export default class CreatePendingCreationArgs {
+  @Field(() => String)
+  email: string
+
+  @Field(() => Number)
+  amount: number
+
+  @Field(() => String)
+  note: string
+
+  @Field(() => Date)
+  creationDate: Date
+}
