92b2dcf6b60ec446f50f60c08666dcd8149a7ace
Hannes Heine <heine.hannes@gmail.com>
2021-11-24 13:10:34 +0100

    Corrected PendingCreationRepository.

--- backend/src/typeorm/repository/PendingCreation.ts
+++ backend/src/typeorm/repository/PendingCreation.ts
@@ -1,5 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm'
 import { PendingCreation } from '@entity/PendingCreation'
 
-@EntityRepository(LoginUserBackup)
+@EntityRepository(PendingCreation)
 export class PendingCreationRepository extends Repository<PendingCreation> {}
