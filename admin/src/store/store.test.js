50e1a16f839907daf0776d1fb58a865eae4c1c35
Moriz Wahl <moriz.wahl@gmx.de>
2021-11-24 10:28:57 +0100

    test store

--- admin/src/store/store.test.js
+++ admin/src/store/store.test.js
@@ -1,6 +1,6 @@
 import { mutations } from './store'
 
-const { token } = mutations
+const { token, openCreationsPlus, openCreationsMinus, resetOpenCreations } = mutations
 
 describe('Vuex store', () => {
   describe('mutations', () => {
@@ -9,6 +9,30 @@ describe('Vuex store', () => {
         const state = { token: null }
         token(state, '1234')
         expect(state.token).toEqual('1234')
+      })
+    })
+
+    describe('openCreationsPlus', () => {
+      it('increases the open creations by a given number', () => {
+        const state = { openCreations: 0 }
+        openCreationsPlus(state, 12)
+        expect(state.openCreations).toEqual(12)
+      })
+    })
+
+    describe('openCreationsMinus', () => {
+      it('decreases the open creations by a given number', () => {
+        const state = { openCreations: 12 }
+        openCreationsMinus(state, 2)
+        expect(state.openCreations).toEqual(10)
+      })
+    })
+
+    describe('resetOpenCreations', () => {
+      it('sets the open creations to 0', () => {
+        const state = { openCreations: 24 }
+        resetOpenCreations(state)
+        expect(state.openCreations).toEqual(0)
       })
     })
   })
