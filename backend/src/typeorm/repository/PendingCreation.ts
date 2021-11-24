725ab91c4901b045226e5479103a4a1f5e89060b
Hannes Heine <heine.hannes@gmail.com>
2021-11-24 12:37:28 +0100

    Test other author.

--- backend/src/typeorm/repository/PendingCreation.ts
+++ backend/src/typeorm/repository/PendingCreation.ts
@@ -2,4 +2,6 @@ import { EntityRepository, Repository } from 'typeorm'
 import { PendingCreation } from '@entity/PendingCreation'
 
 @EntityRepository(LoginUserBackup)
-export class PendingCreationRepository extends Repository<PendingCreation> {}
+export class PendingCreationRepository extends Repository<PendingCreation> {
+  // TODO: git commit --author="Hannes Heine <heine.hannes@gmail.com>"
+}
