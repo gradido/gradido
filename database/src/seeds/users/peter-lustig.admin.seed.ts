3824920c47d34a4f4e4cd0e3dc3fb489aa56e757
Moriz Wahl <moriz.wahl@gmx.de>
2021-11-26 12:31:02 +0100

    get things working

--- database/src/seeds/users/peter-lustig.admin.seed.ts
+++ database/src/seeds/users/peter-lustig.admin.seed.ts
@@ -1,5 +1,6 @@
 import { Factory, Seeder } from 'typeorm-seeding'
 import { peterLustig } from './peter-lustig'
+import { userSeeder } from '../helpers/user-helpers'
 
 export class CreatePeterLustigSeed implements Seeder {
   public async run(factory: Factory): Promise<void> {
