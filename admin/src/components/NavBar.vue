eed923e11f7ea89e12dccf80b2745ba32ed1e478
ogerly <fridolin@tutanota.com>
2021-11-26 09:10:17 +0100

    Add Nav-Item Overview / Übersicht

--- admin/src/components/NavBar.vue
+++ admin/src/components/NavBar.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="component-nabvar">
     <b-navbar toggleable="sm" type="dark" variant="success">
-      <b-navbar-brand to="/">Adminbereich</b-navbar-brand>
+      <b-navbar-brand to="/"></b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
+          <b-nav-item to="/">Übersicht |</b-nav-item>
           <b-nav-item to="/user">Usersuche |</b-nav-item>
           <b-nav-item to="/creation">Mehrfachschöpfung</b-nav-item>
           <b-nav-item
