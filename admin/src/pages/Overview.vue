9f7eecc49d70d80443a961e555dee82480c771f0
ogerly <fridolin@tutanota.com>
2021-11-27 11:18:21 +0100

    Unused points currently deleted

--- admin/src/pages/Overview.vue
+++ admin/src/pages/Overview.vue
@@ -29,26 +29,6 @@
       </b-card-text>
     </b-card>
     <br />
-    <b-row>
-      <b-col>
-        <b-card border-variant="info" header="offene Registrierung" align="center">
-          <b-card-text>Unbestätigte E-mail Registrierung</b-card-text>
-        </b-card>
-      </b-col>
-      <b-col>
-        <b-card border-variant="info" header="geschöpfte Stunden" align="center">
-          <b-card-text>Wievile Stunden können noch von Mitgliedern geschöpft werden?</b-card-text>
-        </b-card>
-      </b-col>
-      <b-col>
-        <b-card border-variant="info" header="Gemeinschafts Konto" align="center">
-          <b-card-text>
-            Für jedes Mitglied kann für das Gemeinschaftskonto geschöpft werden. Pro Monat 1000 x
-            Mitglieder
-          </b-card-text>
-        </b-card>
-      </b-col>
-    </b-row>
     <hr />
     <br />
     <b-list-group>
@@ -57,17 +37,17 @@
       </b-list-group-item>
       <b-list-group-item class="d-flex justify-content-between align-items-center">
         Mitglieder
-        <b-badge class="bg-success" pill>14</b-badge>
+        <b-badge class="bg-success" pill>2400</b-badge>
       </b-list-group-item>
 
       <b-list-group-item class="d-flex justify-content-between align-items-center">
         aktive Mitglieder
-        <b-badge class="bg-primary" pill>12</b-badge>
+        <b-badge class="bg-primary" pill>2201</b-badge>
       </b-list-group-item>
 
       <b-list-group-item class="d-flex justify-content-between align-items-center">
         nicht bestätigte Mitglieder
-        <b-badge class="bg-warning text-dark" pill>2</b-badge>
+        <b-badge class="bg-warning text-dark" pill>120</b-badge>
       </b-list-group-item>
     </b-list-group>
   </div>
