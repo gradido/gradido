66e629daa2081c3a35b07e99245f7e6d30304e4d
Moriz Wahl <moriz.wahl@gmx.de>
2021-12-01 11:02:00 +0100

    Interface for TransactionCreateCoin and TransactionSendCoin

--- database/src/interface/TransactionContext.ts
+++ database/src/interface/TransactionContext.ts
@@ -1,3 +1,4 @@
+import { Transaction } from '../../entity/Transaction'
 import { TransactionSendCoin } from '../../entity/TransactionSendCoin'
 import { TransactionCreation } from '../../entity/TransactionCreation'
 import { User } from '../../entity/User'
@@ -17,4 +18,21 @@ export interface BalanceContext {
   recordDate?: Date
   amount?: number
   user?: User
+}
+
+export interface TransactionSendCoinContext {
+  senderPublic?: Buffer
+  userId?: number
+  recipiantPublic?: Buffer
+  recipiantUserId?: number
+  amount?: number
+  senderFinalBalance?: number
+  transaction?: Transaction
+}
+
+export interface TransactionCreationContext {
+  userId?: number
+  amount?: number
+  targetDate?: Timestamp
+  transaction?: Transaction
 }
