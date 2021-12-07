11069ee5e79fcf0208d165e63ec5699c00275c4e
ogerly <fridolin@tutanota.com>
2021-12-07 11:14:31 +0100

    add class container an main-content in DashboardLayout_gdd.vue

--- frontend/src/views/Layout/DashboardLayout_gdd.vue
+++ frontend/src/views/Layout/DashboardLayout_gdd.vue
@@ -16,19 +16,6 @@
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <!--<b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-          </b-nav-form> 
-
-          <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>
-          -->
-
           <b-nav-item-dropdown right>
             <!-- Using 'button-content' slot -->
             <template #button-content>
@@ -55,7 +42,7 @@
       </b-collapse>
     </b-navbar>
 
-    <div class="main-content" style="max-width: 1000px">
+    <div class="container main-content" style="max-width: 1000px">
       <div @click="$sidebar.displaySidebar(false)">
         <fade-transition :duration="200" origin="center top" mode="out-in">
           <router-view
