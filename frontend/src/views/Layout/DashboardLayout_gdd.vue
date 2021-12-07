73006ede996604a09e7642b3b0c1a9e3756fa5b3
ogerly <fridolin@tutanota.com>
2021-12-07 10:34:24 +0100

    insert b-navbar from bootstrap

--- frontend/src/views/Layout/DashboardLayout_gdd.vue
+++ frontend/src/views/Layout/DashboardLayout_gdd.vue
@@ -1,5 +1,46 @@
 <template>
   <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <div class="navbar-brand">
+        <img :src="logo" class="navbar-brand-img" alt="..." />
+      </div>
+      <b-nav-text>{{ balance }} GDD</b-nav-text>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="/overview">{{ $t('overview') }}</b-nav-item>
+          <b-nav-item href="/send">{{ $t('send') }}</b-nav-item>
+          <b-nav-item href="/transactions">{{ $t('transactions') }}</b-nav-item>
+          <b-nav-item href="/profile">{{ $t('site.navbar.my-profil') }}</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
     <side-bar @logout="logout" :balance="balance" :pending="pending">
       <template slot="links">
         <sidebar-item
@@ -28,6 +69,7 @@
         ></sidebar-item>
       </template>
     </side-bar>
+
     <div class="main-content" style="max-width: 1000px">
       <div class="d-none d-md-block">
         <b-navbar>
@@ -84,6 +126,7 @@ export default {
   },
   data() {
     return {
+      logo: 'img/brand/green.png',
       balance: 0,
       GdtBalance: 0,
       transactions: [],
@@ -143,3 +186,9 @@ export default {
   },
 }
 </script>
+<style>
+.navbar-brand-img {
+  height: 2rem;
+  padding-left: 10px;
+}
+</style>
