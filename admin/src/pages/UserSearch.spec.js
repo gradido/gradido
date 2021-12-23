3fce16b170199eb3d1c03ec1bfb1a3756ad6f460
Moriz Wahl <moriz.wahl@gmx.de>
2021-12-23 14:24:58 +0100

    fix test for UserSearch

--- admin/src/pages/UserSearch.spec.js
+++ admin/src/pages/UserSearch.spec.js
@@ -26,6 +26,16 @@ const mocks = {
   $toasted: {
     error: toastErrorMock,
   },
+  $moment: jest.fn(() => {
+    return {
+      format: jest.fn((m) => m),
+      subtract: jest.fn(() => {
+        return {
+          format: jest.fn((m) => m),
+        }
+      }),
+    }
+  }),
 }
 
 describe('UserSearch', () => {
