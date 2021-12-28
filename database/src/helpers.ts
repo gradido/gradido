b6c66dd2daf2d24b867680773482e52697809bc3
Moriz Wahl <moriz.wahl@gmx.de>
2021-12-28 17:47:43 +0100

    try relative path to migrations

--- database/src/helpers.ts
+++ database/src/helpers.ts
@@ -2,6 +2,8 @@ import CONFIG from './config'
 import { createPool, PoolConfig } from 'mysql'
 import { Migration } from 'ts-mysql-migrate'
 
+console.log(__dirname)
+
 const poolConfig: PoolConfig = {
   host: CONFIG.DB_HOST,
   port: CONFIG.DB_PORT,
