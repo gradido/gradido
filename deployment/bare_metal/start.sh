9c36fc798fc95b498026c8cd084708c3ba0d7a58
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2022-01-05 21:22:26 +0100

    try without trailing slash for backend (nginx) updated start script

--- deployment/bare_metal/start.sh
+++ deployment/bare_metal/start.sh
@@ -5,12 +5,15 @@
 # TODO update git branch
 
 # Node build process can be memory hungry - make sure you have either enoght memory or swap space
-export NODE_OPTIONS="--max-old-space-size=6000"
+#export NODE_OPTIONS="--max-old-space-size=6000"
 
 # Install & build database
 # TODO change directory correcty
 yarn install
 yarn build
+yarn up
+# TODO only in staging!
+yarn seed 
 
 # Install & build backend
 # TODO change directory correcty
@@ -32,15 +35,21 @@ yarn build
 pm2 delete gradido-backend
 # TODO working directory
 pm2 start --name gradido-backend "yarn start"
+pm2 save
 
 # start frontend
 # TODO pm2 stop gradido-frontend
 pm2 delete gradido-frontend
 # TODO working directory
 pm2 start --name gradido-frontend "yarn start"
+pm2 save
 
 # start admin
 # TODO pm2 stop gradido-admin
 pm2 delete gradido-admin
 # TODO working directory
 pm2 start --name gradido-admin "yarn start"
+pm2 save
+
+# restart nginx
+sudo /etc/init.d/nginx restart
