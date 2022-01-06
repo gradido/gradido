4541916472576d07e1e48a1768272ac383a02cc1
elweyn <heine.hannes@gmail.com>
2022-01-06 12:06:12 +0100

    Prepare the different types of UserTable usage in the test.

--- admin/src/components/UserTable.spec.js
+++ admin/src/components/UserTable.spec.js
@@ -6,28 +6,221 @@ const localVue = global.localVue
 describe('UserTable', () => {
   let wrapper
 
-  const propsData = {
-    type: 'Type',
-    itemsUser: [],
-    fieldsTable: [],
-    creation: [],
+  const propsDataPageUserSearch = {
+    type: 'PageUserSearch',
+    itemsUser: [
+      {
+        email: 'bibi@bloxberg.de',
+        firstName: 'Bibi',
+        lastName: 'Bloxberg',
+        creation: [1000, 1000, 1000],
+      },
+      {
+        email: 'bibi@bloxberg.de',
+        firstName: 'Bibi',
+        lastName: 'Bloxberg',
+        creation: [1000, 1000, 1000],
+      },
+      {
+        email: 'bibi@bloxberg.de',
+        firstName: 'Bibi',
+        lastName: 'Bloxberg',
+        creation: [1000, 1000, 1000],
+      },
+    ],
+    fieldsTable: ['email', 'firstName', 'lastName', 'creation'],
+  }
+
+  const propsDataUserListSearch = {
+    type: 'UserListSearch',
+    itemsUser: [
+      {
+        email: 'bibi@bloxberg.de',
+        firstName: 'Bibi',
+        lastName: 'Bloxberg',
+        creation: [1000, 1000, 1000],
+      },
+      {
+        email: 'bibi@bloxberg.de',
+        firstName: 'Bibi',
+        lastName: 'Bloxberg',
+        creation: [1000, 1000, 1000],
+      },
+      {
+        email: 'bibi@bloxberg.de',
+        firstName: 'Bibi',
+        lastName: 'Bloxberg',
+        creation: [1000, 1000, 1000],
+      },
+    ],
+    fieldsTable: ['email', 'firstName', 'lastName', 'creation'],
+    creation: [1000, 1000, 1000],
+  }
+
+  const propsDataUserListMassCreation = {
+    type: 'UserListMassCreation',
+    itemsUser: [
+      {
+        email: 'bibi@bloxberg.de',
+        firstName: 'Bibi',
+        lastName: 'Bloxberg',
+        creation: [1000, 1000, 1000],
+      },
+      {
+        email: 'bibi@bloxberg.de',
+        firstName: 'Bibi',
+        lastName: 'Bloxberg',
+        creation: [1000, 1000, 1000],
+      },
+      {
+        email: 'bibi@bloxberg.de',
+        firstName: 'Bibi',
+        lastName: 'Bloxberg',
+        creation: [1000, 1000, 1000],
+      },
+    ],
+    fieldsTable: ['email', 'firstName', 'lastName', 'creation'],
+    creation: [1000, 1000, 1000],
+  }
+
+  const propsDataPageCreationConfirm = {
+    type: 'PageCreationConfirm',
+    itemsUser: [
+      {
+        email: 'bibi@bloxberg.de',
+        firstName: 'Bibi',
+        lastName: 'Bloxberg',
+        amount: 10,
+        memo: 'Test 1',
+        date: '11-09-2001',
+        moderator: 1,
+      },
+      {
+        email: 'bibi@bloxberg.de',
+        firstName: 'Bibi',
+        lastName: 'Bloxberg',
+        amount: 10,
+        memo: 'Test 2',
+        date: '21-09-2001',
+        moderator: 1,
+      },
+      {
+        email: 'bibi@bloxberg.de',
+        firstName: 'Bibi',
+        lastName: 'Bloxberg',
+        amount: 10,
+        memo: 'Test 3',
+        date: '30-09-2001',
+        moderator: 1,
+      },
+    ],
+    fieldsTable: ['email', 'firstName', 'lastName', 'amount', 'memo', 'date', 'moderator'],
   }
 
   const mocks = {
     $t: jest.fn((t) => t),
   }
 
-  const Wrapper = () => {
+  const Wrapper = (propsData) => {
     return mount(UserTable, { localVue, propsData, mocks })
   }
 
   describe('mount', () => {
-    beforeEach(() => {
-      wrapper = Wrapper()
+    describe('type PageUserSearch', () => {
+      beforeEach(() => {
+        wrapper = Wrapper(propsDataPageUserSearch)
+      })
+
+      it('has a DIV element with the class.component-user-table', () => {
+        expect(wrapper.find('.component-user-table').exists()).toBeTruthy()
+      })
+
+      it('expect(wrapper.html()).', () => {
+        // eslint-disable-next-line no-console
+        console.log(wrapper.html())
+      })
     })
 
-    it('has a DIV element with the class.component-user-table', () => {
-      expect(wrapper.find('.component-user-table').exists()).toBeTruthy()
+    describe('type UserListSearch', () => {
+      beforeEach(() => {
+        wrapper = Wrapper(propsDataUserListSearch)
+      })
+
+      it('has a DIV element with the class.component-user-table', () => {
+        expect(wrapper.find('.component-user-table').exists()).toBeTruthy()
+      })
+
+      it('expect(wrapper.html()).', () => {
+        // eslint-disable-next-line no-console
+        console.log(wrapper.html())
+      })
     })
+
+    describe('type UserListMassCreation', () => {
+      beforeEach(() => {
+        wrapper = Wrapper(propsDataUserListMassCreation)
+      })
+
+      it('has a DIV element with the class.component-user-table', () => {
+        expect(wrapper.find('.component-user-table').exists()).toBeTruthy()
+      })
+
+      it('expect(wrapper.html()).', () => {
+        // eslint-disable-next-line no-console
+        console.log(wrapper.html())
+      })
+    })
+
+    describe('type PageCreationConfirm', () => {
+      beforeEach(() => {
+        wrapper = Wrapper(propsDataPageCreationConfirm)
+      })
+
+      it('has a DIV element with the class.component-user-table', () => {
+        expect(wrapper.find('.component-user-table').exists()).toBeTruthy()
+      })
+
+      it('expect(wrapper.html()).', () => {
+        // eslint-disable-next-line no-console
+        console.log(wrapper.html())
+      })
+    })
+    /** 
+    <user-table
+      v-if="itemsList.length > 0"
+      type="UserListSearch"
+      :itemsUser="itemsList"
+      :fieldsTable="Searchfields"
+      :criteria="criteria"
+      :creation="creation"
+      @update-item="updateItem"
+    /> 
+    
+    <user-table
+      v-show="itemsMassCreation.length > 0"
+      class="shadow p-3 mb-5 bg-white rounded"
+      type="UserListMassCreation"
+      :itemsUser="itemsMassCreation"
+      :fieldsTable="fields"
+      :criteria="null"
+      :creation="creation"
+      @update-item="updateItem"
+    />
+    
+    <user-table
+      class="mt-4"
+      type="PageCreationConfirm"
+      :itemsUser="confirmResult"
+      :fieldsTable="fields"
+      @remove-confirm-result="removeConfirmResult"
+    />
+
+    <user-table
+      type="PageUserSearch"
+      :itemsUser="searchResult"
+      :fieldsTable="fields"
+      :criteria="criteria"
+    />
+    */
   })
 })
