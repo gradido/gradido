ae2fd66eed521f039b5ec10ff68b955a83905105
ogerly <fridolin@tutanota.com>
2022-01-10 10:43:45 +0100

    add href on profil settings change passwort

--- frontend/src/views/Pages/UserProfile/UserCard_FormUserPasswort.vue
+++ frontend/src/views/Pages/UserProfile/UserCard_FormUserPasswort.vue
@@ -3,7 +3,7 @@
     <div>
       <b-row class="mb-4 text-right">
         <b-col class="text-right">
-          <a @click="showPassword ? (showPassword = !showPassword) : cancelEdit()">
+          <a href="#!" @click="showPassword ? (showPassword = !showPassword) : cancelEdit()">
             <span class="pointer mr-3">{{ $t('settings.password.change-password') }}</span>
             <b-icon v-if="showPassword" class="pointer ml-3" icon="pencil"></b-icon>
             <b-icon v-else icon="x-circle" class="pointer ml-3" variant="danger"></b-icon>
