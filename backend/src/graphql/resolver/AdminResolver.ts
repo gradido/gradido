55d85a8ccf6f7f3d6d0fcae05aadb330384d07eb
Moriz Wahl <moriz.wahl@gmx.de>
2022-01-10 10:01:47 +0100

    fix: Change Password in User Profile

--- backend/src/graphql/resolver/AdminResolver.ts
+++ backend/src/graphql/resolver/AdminResolver.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+
 import { Resolver, Query, Arg, Args, Authorized, Mutation, Ctx } from 'type-graphql'
 import { getCustomRepository, Raw } from 'typeorm'
 import { UserAdmin } from '../model/UserAdmin'
