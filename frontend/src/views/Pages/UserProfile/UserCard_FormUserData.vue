34e86a73186053eef1ba732b9febdc9a2bb0246d
ogerly <fridolin@tutanota.com>
2022-01-10 10:40:05 +0100

    add href on profil settings change name

--- frontend/src/views/Pages/UserProfile/UserCard_FormUserData.vue
+++ frontend/src/views/Pages/UserProfile/UserCard_FormUserData.vue
@@ -3,7 +3,7 @@
     <div>
       <b-row class="mb-4 text-right">
         <b-col class="text-right">
-          <a @click="showUserData ? (showUserData = !showUserData) : cancelEdit()">
+          <a href="#!" @click="showUserData ? (showUserData = !showUserData) : cancelEdit()">
             <span class="pointer mr-3">{{ $t('settings.name.change-name') }}</span>
             <b-icon v-if="showUserData" class="pointer ml-3" icon="pencil"></b-icon>
             <b-icon v-else icon="x-circle" class="pointer ml-3" variant="danger"></b-icon>
