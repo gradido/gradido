d41c404288aecc907b35a75fbc04d6a73185f579
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2022-01-12 16:59:53 +0100

    removed comment

--- backend/src/webhook/github.ts
+++ backend/src/webhook/github.ts
@@ -5,8 +5,6 @@ import CONFIG from '../config'
 export const githubWebhook = async (req: any, res: any): Promise<void> => {
   // End call as early as possible
   res.status(200).end()
-  // eslint-disable-next-line no-console
-  console.log('Call ended')
 
   // Handle push events
   if (req.headers['x-github-event'] === 'push') {
