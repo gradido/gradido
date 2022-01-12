d2d22f68b2172522670b3505a2591584b7292e33
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2022-01-12 16:46:24 +0100

    remove console log

--- backend/src/webhook/github.ts
+++ backend/src/webhook/github.ts
@@ -3,8 +3,6 @@ import path from 'path'
 import CONFIG from '../config'
 
 export const githubWebhook = async (req: any, res: any): Promise<void> => {
-  // eslint-disable-next-line no-console
-  console.log('Hook received')
   // End call as early as possible
   res.status(200).end()
   // eslint-disable-next-line no-console
