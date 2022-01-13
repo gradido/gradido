8fcc3b31adf0809272eb8679c8cd4462b6661317
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2022-01-13 22:34:17 +0100

    improved installscript alot - proper order - templates for all configs to fill in automatically

--- deployment/bare_metal/install.sh
+++ deployment/bare_metal/install.sh
@@ -3,14 +3,25 @@
 # This install script requires the minimum requirements already installed.
 # How to do this is described in detail in [setup.md](./setup.md)
 
+# Find current directory & configure paths
+set -o allexport
+SCRIPT_PATH=$(realpath $0)
+SCRIPT_DIR=$(dirname $SCRIPT_PATH)
+PROJECT_ROOT=$SCRIPT_DIR/../..
+set +o allexport
+
 # Load .env or .env.dist if not present
 set -o allexport
-if [ -f ".env" ]; then
-    source .env
+#TODO
+if [ -f "$SCRIPT_DIR/.env" ]; then
+    source $SCRIPT_DIR/.env
 else
-    source .env.dist
+    source $SCRIPT_DIR/.env.dist
 fi
 set +o allexport
+
+# Configure git
+git config pull.ff only
 
 # Install mariadb
 sudo apt-get install -y mariadb-server
@@ -23,57 +34,24 @@ sudo mysql_secure_installation
 # Remove test database and access to it? [Y/n] Y
 # Reload privilege tables now? [Y/n] Y
 
-# create db user
-DB_USER=gradido 
-DB_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo);
-# create table 
-#create database gradido_community 
-#    DEFAULT CHARACTER SET utf8mb4
-#    DEFAULT COLLATE utf8mb4_unicode_ci;
-# GRANT ALL PRIVILEGES ON gradido_community.* TO '$DB_USER'@'localhost';
-sudo mysql <<EOFMYSQL
-    CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWD';
-    GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'localhost';
-    FLUSH PRIVILEGES;
-EOFMYSQL
-# TODO generate .env
-echo $DB_PASSWORD
-
-#TODO go to database
-#TODO generate this
-#TODO database setup
-cp .env.dist .env
-
-#TODO go to backend
-#TODO generate this
-#TODO database setup
-#TODOchange jwt secret
-#TODO change email releated stuff
-cp .env.dist .env
-
-#TODO go to frontend
-#TODO generate this
-#TODO backend url
-#TODO admin url
-cp .env.dist .env
-
-#TODO go to admin
-#TODO generate this
-#TODO change graphqlurl
-#TODO change wallet url
-
-cp .env.dist .env
-
-#TODO import old database
-
 # Install nginx
 sudo apt-get install -y nginx
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/gradido/gradido/deployment/bare_metal/nginx/sites-available/gradido.conf /etc/nginx/sites-available
-sudo ln -s /etc/nginx/sites-available/gradido.conf /etc/nginx/sites-enabled
-sudo ln -s /home/gradido/gradido/deployment/bare_metal/nginx/sites-available/update-page.conf /etc/nginx/sites-available
-cd /etc/nginx
-sudo ln -s /home/gradido/gradido/deployment/bare_metal/nginx/common common
+# sudo ln -s /home/gradido/gradido/deployment/bare_metal/nginx/sites-available/gradido.conf /etc/nginx/sites-available
+# sudo ln -s /etc/nginx/sites-available/gradido.conf /etc/nginx/sites-enabled
+# sudo ln -s /home/gradido/gradido/deployment/bare_metal/nginx/sites-available/update-page.conf /etc/nginx/sites-available
+sudo ln -s /home/gradido/gradido/deployment/bare_metal/nginx/common /etc/nginx/
+
+# Allow nginx configuration and restart for gradido
+#TODO generate file
+sudo nano /etc/sudoers.d/gradido
+> gradido ALL=(ALL) NOPASSWD: /etc/init.d/nginx start,/etc/init.d/nginx stop,/etc/init.d/nginx restart
+sudo chmod a+rw /etc/nginx/sites-enabled
+
+# Install node 16.x
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
 
 # Install yarn
 sudo apt-get install -y curl
@@ -83,32 +61,22 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 sudo apt-get update
 sudo apt-get install -y yarn
 
-# Install node 16.x
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt-get install -y build-essential
-
 # Install pm2
 sudo yarn global add pm2
 
 # Install certbot
+sudo /etc/init.d/nginx stop
 sudo apt-get install -y certbot
-sudo apt-get install -y python3-certbot-nginx
-sudo certbot --certonly
+sudo certbot certonly
 > Enter email address (used for urgent renewal and security notices) > support@gradido.net
 > Please read the Terms of Service at > Y
 > Would you be willing, once your first certificate is successfully issued, to > N
 > No names were found in your configuration files. Please enter in your domain > stage1.gradido.net
+sudo /etc/init.d/nginx start
 
-git config pull.ff only
-
-# Allow nginx configuration and restart for gradido
-sudo nano /etc/sudoers.d/gradido
-> gradido ALL=(ALL) NOPASSWD: /etc/init.d/nginx start,/etc/init.d/nginx stop,/etc/init.d/nginx restart
-sudo chmod a+rw /etc/nginx/sites-enabled
-
-# Webhooks (optional)
+# Webhooks (optional) (for development)
 sudo apt install webhook
+# TODO generate
 nano ~/hooks.json
 ```
 [
@@ -118,7 +86,7 @@ nano ~/hooks.json
     "pass-arguments-to-command": [
       {
         "source": "string",
-        "name": "new_deployment"
+        "name": "master"
       },
     ],
     "command-working-directory": "/home/gradido/gradido/deployment/bare_metal",
@@ -137,7 +105,7 @@ nano ~/hooks.json
         {
           "match": {
             "type": "value",
-            "value": "refs/heads/new_deployment",
+            "value": "refs/heads/master",
             "parameter": {
               "source": "payload",
               "name": "ref"
@@ -153,3 +121,32 @@ nano ~/hooks.json
 webhook -hooks ~/hooks.json &
 # or for debugging
 webhook -hooks ~/hooks.json -verbose
+
+# create db user
+DB_USER=gradido 
+DB_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo);
+# create table 
+#create database gradido_community 
+#    DEFAULT CHARACTER SET utf8mb4
+#    DEFAULT COLLATE utf8mb4_unicode_ci;
+# GRANT ALL PRIVILEGES ON gradido_community.* TO '$DB_USER'@'localhost';
+sudo mysql <<EOFMYSQL
+    CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
+    GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'localhost';
+    FLUSH PRIVILEGES;
+EOFMYSQL
+
+# Configure database
+envsubst "$(env | sed -e 's/=.*//' -e 's/^/\$/g')" < $PROJECT_ROOT/database/.env.template > $PROJECT_ROOT/database/.env
+
+# Configure backend
+JWT_SECRET=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo);
+envsubst "$(env | sed -e 's/=.*//' -e 's/^/\$/g')" < $PROJECT_ROOT/backend/.env.template > $PROJECT_ROOT/backend/.env
+
+# Configure frontend
+envsubst "$(env | sed -e 's/=.*//' -e 's/^/\$/g')" < $PROJECT_ROOT/frontend/.env.template > $PROJECT_ROOT/frontend/.env
+
+# Configure admin
+envsubst "$(env | sed -e 's/=.*//' -e 's/^/\$/g')" < $PROJECT_ROOT/admin/.env.template > $PROJECT_ROOT/admin/.env
+
+#TODO import old database
