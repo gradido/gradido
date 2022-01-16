119b97b8791c70d0c055a2b9690c7296a7c12bad
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2022-01-16 11:40:23 +0100

    when building the ts sources we need dev mode

--- deployment/bare_metal/start.sh
+++ deployment/bare_metal/start.sh
@@ -77,14 +77,13 @@ else
   yarn up
 fi
 
-# TODO maybe handle this differently?
-export NODE_ENV=production
-
 # Install & build backend
 echo 'Updating backend<br>' >> $UPDATE_HTML
 cd $PROJECT_ROOT/backend
 yarn install
 yarn build
+# TODO maybe handle this differently?
+export NODE_ENV=production
 pm2 delete gradido-backend
 pm2 start --name gradido-backend "yarn --cwd $PROJECT_ROOT/backend start" -l $GRADIDO_LOG_PATH/pm2.backend.log --log-date-format 'DD-MM HH:mm:ss.SSS'
 pm2 save
