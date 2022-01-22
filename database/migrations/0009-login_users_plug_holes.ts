54afb80cc2828530973d868f51c9d4d0d435a690
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2022-01-22 05:42:01 +0100

    generate random private key for those accounts with an intact passphrase

--- database/migrations/0009-login_users_plug_holes.ts
+++ database/migrations/0009-login_users_plug_holes.ts
@@ -6,6 +6,15 @@
  */
 
 export async function upgrade(queryFn: (query: string, values?: any[]) => Promise<Array<any>>) {
+  // Generate a random private key where the remaining data is present
+  await queryFn(
+    `UPDATE login_users SET privkey = UNHEX(SHA1(RAND()))
+     WHERE privkey IS NULL
+     AND pubkey IS NOT NULL
+     AND password != 0
+     AND id IN (SELECT user_id FROM login_user_backups);`,
+  )
+
   // Remove incomplete data and set account as not activated yet.
   await queryFn(
     `UPDATE login_users SET password = 0, pubkey = NULL, email_checked = 0 WHERE privkey IS NULL;`,
