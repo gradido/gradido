2a4f66a03dd03cdf3ca3c16eca7539132ec90f04
elweyn <heine.hannes@gmail.com>
2022-01-22 10:05:15 +0100

    If optin code to old send user to the error page that says that the code is not active anymore.

--- frontend/src/views/Pages/ResetPassword.vue
+++ frontend/src/views/Pages/ResetPassword.vue
@@ -96,8 +96,9 @@ export default {
         })
         .catch((error) => {
           this.$toasted.global.error(error.message)
-          if (error.message.includes('Code is older than 10 minutes'))
-            this.$router.push('/password/reset')
+          if (error.message.includes('Code is older than 10 minutes')){
+            this.$router.push('/thx/login')
+          }
         })
     },
     setDisplaySetup() {
