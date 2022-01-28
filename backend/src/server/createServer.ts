d6a7d8ddebf487bcd11339ac7b380452bb590a0e
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2022-01-28 16:52:16 +0100

    adjust required database version

--- backend/src/server/createServer.ts
+++ backend/src/server/createServer.ts
@@ -28,7 +28,7 @@ import { elopageWebhook } from '../webhook/elopage'
 // TODO implement
 // import queryComplexity, { simpleEstimator, fieldConfigEstimator } from "graphql-query-complexity";
 
-const DB_VERSION = '0012-login_user_backups_unify_wordlist'
+const DB_VERSION = '0014-drop_unused_tables_with_data'
 
 const createServer = async (context: any = serverContext): Promise<any> => {
   // open mysql connection
