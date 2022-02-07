9d79e35f3643565a5966c3945ebc19b1d0281734
ogerly <fridolin@tutanota.com>
2022-02-07 10:34:54 +0100

    Refactor hasElopageBuys method to a utility method.

---
+++ backend/src/util/hasElopageBuys.ts
@@ -0,0 +1,9 @@
+
+import { getCustomRepository } from '@dbTools/typeorm'
+import { LoginElopageBuysRepository } from '../typeorm/repository/LoginElopageBuys'
+
+export async function hasElopageBuys(email: string): Promise<boolean> {
+  const loginElopageBuysRepository = getCustomRepository(LoginElopageBuysRepository)
+  const elopageBuyCount = await loginElopageBuysRepository.count({ payerEmail: email })
+  return elopageBuyCount > 0
+}
