ee94c655ef80064391aadc083a40e151c3a37108
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2022-02-08 15:59:59 +0100

    fix elopage buys not using repository but entity

--- backend/src/util/hasElopageBuys.ts
+++ backend/src/util/hasElopageBuys.ts
@@ -1,8 +1,6 @@
-import { getCustomRepository } from '@dbTools/typeorm'
-import { LoginElopageBuysRepository } from '../typeorm/repository/LoginElopageBuys'
+import { LoginElopageBuys } from '@entity/LoginElopageBuys'
 
 export async function hasElopageBuys(email: string): Promise<boolean> {
-  const loginElopageBuysRepository = getCustomRepository(LoginElopageBuysRepository)
-  const elopageBuyCount = await loginElopageBuysRepository.count({ payerEmail: email })
+  const elopageBuyCount = await LoginElopageBuys.count({ payerEmail: email })
   return elopageBuyCount > 0
 }
