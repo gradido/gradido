2843d22382f7f20d3b4969c49a383f3183d46cb8
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2022-02-09 11:07:36 +0100

    disallow inline styles

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint',
   },
-  extends: ['standard', 'plugin:vue/essential', 'plugin:prettier/recommended'],
+  extends: ['standard', 'plugin:vue/essential', 'plugin:vue/base', 'plugin:prettier/recommended'],
   // required to lint *.vue files
   plugins: ['vue', 'prettier', 'jest'],
   // add your custom rules here
@@ -16,6 +16,12 @@ module.exports = {
     'no-console': ['error'],
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+    'vue/no-static-inline-styles': [
+      'error',
+      {
+        allowBinding: false,
+      },
+    ],
     'prettier/prettier': [
       'error',
       {
