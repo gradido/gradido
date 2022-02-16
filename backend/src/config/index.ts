374841feb3bb8f9e10d8184583aa2be8d7d62b01
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2022-02-16 12:05:46 +0100

    corrected migration name & config db version

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const constants = {
-  DB_VERSION: '0021-elopagebuys_fields_nullable',
+  DB_VERSION: '0022-delete_decay_start_block',
   DECAY_START_TIME: new Date('2021-05-13 17:46:31'), // GMT+0
 }
 
