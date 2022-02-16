a3d697182dc88f7973f3148aa84c3db24ba04e0b
Moriz Wahl <moriz.wahl@gmx.de>
2022-02-16 12:26:27 +0100

    fix: Backend Unit Tests Running Again

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,21 +1,19 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = async () => {
-  process.env.TZ = 'UTC'
-  return {
-    verbose: true,
-    preset: 'ts-jest',
-    collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.ts', '!**/node_modules/**'],
-    moduleNameMapper: {
-      '@entity/(.*)': '<rootDir>/../database/build/entity/$1',
-      // This is hack to fix a problem with the library `ts-mysql-migrate` which does differentiate between its ts/js state
-      '@dbTools/(.*)': '<rootDir>/../database/src/$1',
-      /*
-      '@dbTools/(.*)':
-        process.env.NODE_ENV === 'development'
-          ? '<rootDir>/../database/src/$1'
-          : '<rootDir>/../database/build/src/$1',
-      */
-    },
-  }
+module.exports = {
+  verbose: true,
+  preset: 'ts-jest',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts', '!**/node_modules/**'],
+  setupFiles: ['<rootDir>/test/testSetup.ts'],
+  moduleNameMapper: {
+    '@entity/(.*)':
+      process.env.NODE_ENV === 'development'
+        ? '<rootDir>/../database/entity/$1'
+        : '<rootDir>/../database/build/entity/$1',
+    // This is hack to fix a problem with the library `ts-mysql-migrate` which does differentiate between its ts/js state
+    '@dbTools/(.*)':
+      process.env.NODE_ENV === 'development'
+        ? '<rootDir>/../database/src/$1'
+        : '<rootDir>/../database/build/src/$1',
+  },
 }
