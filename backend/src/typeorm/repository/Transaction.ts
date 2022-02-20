ecfcde8dd4168da8651c4a4fad4ec16f72a1bd56
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2022-02-20 12:56:10 +0100

    simplified query

--- backend/src/typeorm/repository/Transaction.ts
+++ backend/src/typeorm/repository/Transaction.ts
@@ -6,16 +6,6 @@ export class TransactionRepository extends Repository<Transaction> {
   async joinFullTransactionsByIds(transactionIds: number[]): Promise<Transaction[]> {
     return this.createQueryBuilder('transaction')
       .where('transaction.id IN (:...transactions)', { transactions: transactionIds })
-      .leftJoinAndSelect(
-        'transaction.transactionSendCoin',
-        'transactionSendCoin',
-        // 'transactionSendCoin.transaction_id = transaction.id',
-      )
-      .leftJoinAndSelect(
-        'transaction.transactionCreation',
-        'transactionCreation',
-        // 'transactionSendCoin.transaction_id = transaction.id',
-      )
       .getMany()
   }
 }
