8d9228aab86deb8d3ab42e2430f90741cc42a291
Moriz Wahl <moriz.wahl@gmx.de>
2022-02-21 14:30:06 +0100

    ensure mnemomic type files are copied to build

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -10,7 +10,6 @@ module.exports = {
       process.env.NODE_ENV === 'development'
         ? '<rootDir>/../database/entity/$1'
         : '<rootDir>/../database/build/entity/$1',
-    // This is hack to fix a problem with the library `ts-mysql-migrate` which does differentiate between its ts/js state
     '@dbTools/(.*)':
       process.env.NODE_ENV === 'development'
         ? '<rootDir>/../database/src/$1'
