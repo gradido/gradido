67cddadd1e11b03d737529a2d3a57b560e637acf
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2022-02-27 02:07:33 +0100

    graphql recognizes decimal type now and successfully starts

--- backend/src/graphql/schema.ts
+++ backend/src/graphql/schema.ts
@@ -1,13 +1,16 @@
 import { GraphQLSchema } from 'graphql'
 import { buildSchema } from 'type-graphql'
+import Decimal from 'decimal.js-light'
 import path from 'path'
 
 import isAuthorized from './directive/isAuthorized'
+import DecimalScalar from './scalar/Decimal'
 
 const schema = async (): Promise<GraphQLSchema> => {
   return buildSchema({
     resolvers: [path.join(__dirname, 'resolver', `!(*.test).{js,ts}`)],
     authChecker: isAuthorized,
+    scalarsMap: [{ type: Decimal, scalar: DecimalScalar }],
   })
 }
 
