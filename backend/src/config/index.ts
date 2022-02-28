fcb6b1c2d3c87d90dbbcc039264ee8d189b2ba7c
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2022-02-28 02:04:42 +0100

    update database version

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const constants = {
-  DB_VERSION: '0024-combine_transaction_tables',
+  DB_VERSION: '0025-emails_to_lower',
   DECAY_START_TIME: new Date('2021-05-13 17:46:31'), // GMT+0
 }
 
