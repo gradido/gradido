54e89f2eb77992d4b1bd0bd317fcb77468a0e0f1
ogerly <fridolin@tutanota.com>
2022-03-03 13:47:39 +0100

    unused scalar/Decimal, yarn lint --fix

--- backend/src/graphql/resolver/TransactionResolver.ts
+++ backend/src/graphql/resolver/TransactionResolver.ts
@@ -30,7 +30,7 @@ import { RIGHTS } from '../../auth/RIGHTS'
 import { User } from '../model/User'
 import { communityUser } from '../../util/communityUser'
 import { virtualDecayTransaction } from '../../util/virtualDecayTransaction'
-import Decimal from '../scalar/Decimal'
+// import Decimal from '../scalar/Decimal'
 
 @Resolver()
 export class TransactionResolver {
