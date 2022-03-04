c728b13f43b39a7d9f10643b0feba91b35206567
ogerly <fridolin@tutanota.com>
2022-03-04 15:33:49 +0100

    undo changes

--- docu/other/db/transactions.sql
+++ docu/other/db/transactions.sql
@@ -2,7 +2,7 @@
 CREATE TABLE `transactions` (
   `id` bigint(20) NOT NULL,
   `state_group_id` int(11) NOT NULL,
-  `type_id` int(11) NOT NULL,
+  `transaction_type_id` int(11) NOT NULL,
   `tx_hash` binary(32) NOT NULL,
   `received` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
