0094ee3bbc4152e21d959771cf975b49f41ce218
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2022-03-04 12:28:03 +0100

    removed unnecessary lint ignores

--- backend/src/graphql/model/Transaction.ts
+++ backend/src/graphql/model/Transaction.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { ObjectType, Field } from 'type-graphql'
 import { Decay } from './Decay'
 import { Transaction as dbTransaction } from '@entity/Transaction'
