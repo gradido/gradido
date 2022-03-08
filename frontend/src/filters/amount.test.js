fe10316d4777c804a30ff1a621b8c92d8c610cce
Moriz Wahl <moriz.wahl@gmx.de>
2022-03-08 06:53:36 +0100

    block for tests

--- frontend/src/filters/amount.test.js
+++ frontend/src/filters/amount.test.js
@@ -6,30 +6,32 @@ const i18nMock = {
 
 const { amount, GDD } = loadFilters(i18nMock)
 
-describe('amount', () => {
-  it('returns empty string when called with null', () => {
-    expect(amount(null)).toBe('')
+describe('amount filters', () => {
+  describe('amount', () => {
+    it('returns empty string when called with null', () => {
+      expect(amount(null)).toBe('')
+    })
+
+    it('returns 0 when called with 0', () => {
+      expect(amount(0)).toBe('0')
+    })
+
+    it('returns a leading proper minus sign when called with negative value', () => {
+      expect(amount(-1)).toBe('− 1')
+    })
   })
 
-  it('returns 0 when called with 0', () => {
-    expect(amount(0)).toBe('0')
-  })
+  describe('GDD', () => {
+    it('returns empty string when called with null', () => {
+      expect(GDD(null)).toBe('')
+    })
 
-  it('returns a leading proper minus sign when called with negative value', () => {
-    expect(amount(-1)).toBe('− 1')
-  })
-})
+    it('returns "+ 0 GDD" when called with 0', () => {
+      expect(GDD(0)).toBe('+ 0 GDD')
+    })
 
-describe('GDD', () => {
-  it('returns empty string when called with null', () => {
-    expect(GDD(null)).toBe('')
-  })
-
-  it('returns "+ 0 GDD" when called with 0', () => {
-    expect(GDD(0)).toBe('+ 0 GDD')
-  })
-
-  it('returns a leading proper minus sign when called with negative value', () => {
-    expect(GDD(-1)).toBe('− 1 GDD')
+    it('returns a leading proper minus sign when called with negative value', () => {
+      expect(GDD(-1)).toBe('− 1 GDD')
+    })
   })
 })
