049e1c4318c4dba7807172adb387c2f46c4ff73c
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2022-03-10 12:48:07 +0100

    admin stylelint config file

---
+++ admin/.stylelintrc.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  extends: ["stylelint-config-standard-scss", "stylelint-config-recommended-vue"],
+  overrides: [
+    {
+      files: "**/*.{scss}",
+      customSyntax: "postcss-scss",
+      extends: ["stylelint-config-standard-scss"],
+    },
+    {
+        files: "**/*.vue",
+        customSyntax: "postcss-html",
+        extends: ["stylelint-config-recommended-vue"],
+    }
+  ]
+
+};
