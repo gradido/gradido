b97837d41517c91b5e36984af1e4b379e2827117
ogerly <fridolin@tutanota.com>
2022-03-10 10:34:48 +0100

    test data for displaySetup in ShowTransactionLinkInformations.vue

--- frontend/src/pages/ShowTransactionLinkInformations.vue
+++ frontend/src/pages/ShowTransactionLinkInformations.vue
@@ -4,6 +4,7 @@
     <div class="header py-7 py-lg-8 pt-lg-9">
       <b-container>
         <div class="header-body text-center mb-7">
+          <div class="mb-5">resultDB : {{ resultDB }}</div>
           <p class="h1">
             {{ displaySetup.user.firstName }} {{ displaySetup.user.lastName }}
             {{ $t('wants to send you') }} {{ displaySetup.amount | GDD }}
@@ -25,7 +26,16 @@ export default {
   name: 'ShowTransactionLinkInformations',
   data() {
     return {
-      displaySetup: {},
+      resultDB: {},
+      displaySetup: {
+        amount: '123456',
+        linkTo: '',
+        user: {
+          publisherId: 1,
+          firstName: 'testName',
+          lastName: 'testOgerly',
+        },
+      },
     }
   },
   methods: {
@@ -41,6 +51,7 @@ export default {
           const {
             data: { queryTransactionLink },
           } = result
+          this.resultDB = queryTransactionLink
           this.displaySetup = queryTransactionLink
           this.$store.commit('publisherId', queryTransactionLink.user.publisherId)
         })
@@ -50,7 +61,8 @@ export default {
     },
   },
   created() {
-    this.setDisplaySetup()
+    this.setTransactionLinkInformation()
+    this.displaySetup.linkTo = this.$route.params.code
   },
 }
 </script>
