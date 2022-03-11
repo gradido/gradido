b3f7fd28a18280a5e00e5bc40f70654c2714d527
Moriz Wahl <moriz.wahl@gmx.de>
2022-03-11 15:54:59 +0100

    add transactionLinkId Column to transactions table

---
+++ database/migrations/0032-add-transaction-link-to-transaction.ts
@@ -0,0 +1,14 @@
+/* MIGRATION TO ADD transactionLinkId FIELDTO TRANSACTION */
+
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export async function upgrade(queryFn: (query: string, values?: any[]) => Promise<Array<any>>) {
+  await queryFn(
+    'ALTER TABLE `transactions` ADD COLUMN `transactionLinkId` int UNSIGNED DEFAULT NULL;',
+  )
+}
+
+export async function downgrade(queryFn: (query: string, values?: any[]) => Promise<Array<any>>) {
+  await queryFn('ALTER TABLE `transactions` DROP COLUMN `transactionLinkId`;')
+}
