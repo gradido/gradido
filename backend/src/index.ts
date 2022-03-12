549f2182310eb1a2c704c3ff1a6ed4030df4143f
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2022-03-12 03:52:01 +0100

    module-aliases in order to have things working with `NODE_ENV=production`

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+// resolve @-import-aliases
+import 'module-alias/register'
+
 import createServer from './server/createServer'
 
 // config
