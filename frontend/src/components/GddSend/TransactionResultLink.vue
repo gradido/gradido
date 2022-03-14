f15fe1d7cbb40c6db2fd3c0e63085d7052e61dbb
ogerly <fridolin@tutanota.com>
2022-03-14 10:11:53 +0100

    add component TransactionResultLink.vue

---
+++ frontend/src/components/GddSend/TransactionResultLink.vue
@@ -0,0 +1,36 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col>
+        <b-card class="p-0 gradido-custom-background">
+          <div class="p-4">
+            <div class="h3 mb-5">{{ $t('gdd_per_link.created') }}</div>
+            <clipboard-copy :code="linkResult.code" />
+          </div>
+          <p class="text-center mt-3">
+            <b-button variant="success" @click="$emit('on-reset')">{{ $t('form.close') }}</b-button>
+          </p>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+<script>
+import ClipboardCopy from '../ClipboardCopy.vue'
+
+export default {
+  name: 'TransactionResultLink',
+  components: {
+    ClipboardCopy,
+  },
+  props: {
+    linkResult: {
+      type: Object,
+      required: false,
+      default() {
+        return {}
+      },
+    },
+  },
+}
+</script>
