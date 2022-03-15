e5dc99dad60526ab6bc8c0867de4c4f262dcb631
ogerly <fridolin@tutanota.com>
2022-03-15 11:38:35 +0100

    add apollo funktion for query transactionLinkList

--- frontend/src/components/GddTransactionList.vue
+++ frontend/src/components/GddTransactionList.vue
@@ -43,7 +43,7 @@
           </template>
 
           <template #TRANSACTION_LINK>
-            <transaction-link
+            <transaction-link-summary
               class="list-group-item"
               v-bind="transactions[index]"
               :transactionLinkCount="transactionLinkCount"
@@ -71,7 +71,7 @@ import TransactionDecay from '@/components/Transactions/TransactionDecay'
 import TransactionSend from '@/components/Transactions/TransactionSend'
 import TransactionReceive from '@/components/Transactions/TransactionReceive'
 import TransactionCreation from '@/components/Transactions/TransactionCreation'
-import TransactionLink from '@/components/Transactions/TransactionLink'
+import TransactionLinkSummary from '@/components/Transactions/TransactionLinkSummary'
 
 export default {
   name: 'gdd-transaction-list',
@@ -82,7 +82,7 @@ export default {
     TransactionSend,
     TransactionReceive,
     TransactionCreation,
-    TransactionLink,
+    TransactionLinkSummary,
   },
   data() {
     return {
