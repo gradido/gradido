538a6f021fc9095bec1a8ed091001ae6bb801891
ogerly <fridolin@tutanota.com>
2022-03-15 15:52:29 +0100

    add components TransactionLink in CollapseLinksList.vue

--- frontend/src/components/DecayInformations/CollapseLinksList.vue
+++ frontend/src/components/DecayInformations/CollapseLinksList.vue
@@ -2,15 +2,27 @@
   <div class="collapse-links-list">
     <div class="d-flex">
       <div class="text-center pb-3 gradido-max-width">
-        <b>{{ $t('links-list.header') }}</b>
-        {{ transactionLinks }}
+        <div>
+          <b-row class="gradido-custom-background mb-2">
+            <b-col cols="2"><b>Betrag</b></b-col>
+            <b-col cols="2"><b>Vergänglichkeit</b></b-col>
+            <b-col cols="4"><b>Nachricht</b></b-col>
+            <b-col cols="2"><b>Abgelaufen</b></b-col>
+            <b-col cols="2"></b-col>
+          </b-row>
+          <transaction-link v-for="item in transactionLinks" :key="item.id" v-bind:item="item" />
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
+import TransactionLink from '@/components/TransactionLinks/TransactionLink.vue'
 export default {
   name: 'CollapseLinksList',
+  components: {
+    TransactionLink,
+  },
   props: {
     transactionLinks: { type: Array, required: true },
   },
