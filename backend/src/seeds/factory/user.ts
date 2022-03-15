d55caacf6b0908cd0cab2e774144eb69f977f215
Moriz Wahl <moriz.wahl@gmx.de>
2022-03-15 14:57:27 +0100

    seed peter lustig as admin

--- backend/src/seeds/factory/user.ts
+++ backend/src/seeds/factory/user.ts
@@ -4,18 +4,33 @@
 import { createUser, setPassword } from '@/seeds/graphql/mutations'
 import { User } from '@entity/User'
 import { LoginEmailOptIn } from '@entity/LoginEmailOptIn'
+import { ServerUser } from '@entity/ServerUser'
 import { UserInterface } from '@/seeds/users/UserInterface'
 
 export const createUserFactory = async (mutate: any, user: UserInterface): Promise<void> => {
   await mutate({ mutation: createUser, variables: user })
+  const dbUser = await User.findOne({ where: { email: user.email } })
+  if (!dbUser) throw new Error('Ups, no user found')
+
   if (user.emailChecked) {
-    const dbUser = await User.findOne({ where: { email: user.email } })
-    if (!dbUser) throw new Error('Ups, no user found')
     const optin = await LoginEmailOptIn.findOne({ where: { userId: dbUser.id } })
     if (!optin) throw new Error('Ups, no optin found')
     await mutate({
       mutation: setPassword,
       variables: { password: 'Aa12345_', code: optin.verificationCode },
     })
+  }
+
+  if (user.isAdmin) {
+    const admin = new ServerUser()
+    admin.username = dbUser.firstName
+    admin.password = 'please_refactor'
+    admin.email = dbUser.email
+    admin.role = 'admin'
+    admin.activated = 1
+    admin.lastLogin = new Date()
+    admin.created = dbUser.createdAt
+    admin.modified = dbUser.createdAt
+    admin.save()
   }
 }
