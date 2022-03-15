455a0ab754cc18eb315ac55fdc39b0ab93e05d16
Moriz Wahl <moriz.wahl@gmx.de>
2022-03-15 09:49:15 +0100

    add user interface and standard users

---
+++ backend/src/seeds/users/UserInterface.ts
@@ -0,0 +1,11 @@
+export interface UserInterface {
+  email?: string
+  firstName?: string
+  lastName?: string
+  // description?: string
+  createdAt?: Date
+  emailChecked?: boolean
+  language?: string
+  deletedAt?: Date
+  isAdmin?: boolean
+}
---
+++ backend/src/seeds/users/bibi-bloxberg.ts
@@ -0,0 +1,10 @@
+import { UserInterface } from './UserInterface'
+
+export const bibiBloxberg: UserInterface = {
+  email: 'bibi@bloxberg.de',
+  firstName: 'Bibi',
+  lastName: 'Bloxberg',
+  // description: 'Hex Hex',
+  emailChecked: true,
+  language: 'de',
+}
