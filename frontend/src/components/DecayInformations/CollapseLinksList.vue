ab3ad69cc8d24b64552a79628912cc498416c2d4
ogerly <fridolin@tutanota.com>
2022-03-16 18:23:30 +0100

    remove unused head row for transactionLinkList

--- frontend/src/components/DecayInformations/CollapseLinksList.vue
+++ frontend/src/components/DecayInformations/CollapseLinksList.vue
@@ -1,30 +1,16 @@
 <template>
   <div class="collapse-links-list">
     <div class="d-flex">
-      <div class="text-center pb-3 gradido-max-width">
+      <div class="gradido-max-width">
+        <hr />
         <div>
-          <b-row class="mb-2">
-            <b-col cols="2">
-              <b>{{ $t('form.amount') }}</b>
-            </b-col>
-            <b-col cols="2">
-              <b>{{ $t('decay.decay') }}</b>
-            </b-col>
-            <b-col cols="4">
-              <b>{{ $t('form.memo') }}</b>
-            </b-col>
-            <b-col cols="2">
-              <b>{{ $t('gdd_per_link.expired') }}</b>
-            </b-col>
-            <b-col cols="2"></b-col>
-          </b-row>
           <transaction-link
             v-for="item in transactionLinks"
             :key="item.id"
             v-bind:item="item"
             @reset-transaction-link-list="resetTransactionLinkList"
           />
-          <div class="mt-3 mb-3 text-center">
+          <div class="mb-3">
             <b-button
               v-if="!value.pending && value.itemsShown < transactionLinkCount"
               block
@@ -71,9 +57,9 @@ export default {
   computed: {
     buttonText() {
       const i = this.transactionLinkCount - this.value.itemsShown
-      if (i === 1) return this.$tc('link-reload', 0, { n: i })
-      if (i < 6) return this.$tc('link-reload', 1, { n: i })
-      return this.$tc('link-reload', 2, { n: 5 })
+      if (i === 1) return this.$tc('link-load', 0)
+      if (i < 6) return this.$tc('link-load', 1, { n: i })
+      return this.$tc('link-load', 2, { n: 5 })
     },
   },
 }
