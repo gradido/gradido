386279a1cb39efcb396324f6e23ca88f16730f0c
ogerly <fridolin@tutanota.com>
2022-03-17 13:12:43 +0100

    show tranaction link information page

--- frontend/src/pages/ShowTransactionLinkInformations.vue
+++ frontend/src/pages/ShowTransactionLinkInformations.vue
@@ -1,21 +1,33 @@
 <template>
   <div class="show-transaction-link-informations">
-    <!-- Header -->
-    <div class="header py-7 py-lg-8 pt-lg-9">
-      <b-container>
-        <div class="header-body text-center mb-7">
-          <p class="h1">
+    <div class="text-center"><b-img :src="img" fluid alt="logo"></b-img></div>
+    <b-container class="pt-5">
+      <div>
+        <b-jumbotron bg-variant="info" text-variant="dark" border-variant="dark">
+          <h1>
             {{ displaySetup.user.firstName }}
             {{ $t('transaction-link.send_you') }} {{ displaySetup.amount | GDD }}
-          </p>
-          <p class="h4">{{ displaySetup.memo }}</p>
-          <hr />
-          <b-button v-if="displaySetup.linkTo" :to="displaySetup.linkTo">
-            {{ $t('transaction-link.button') }}
-          </b-button>
-        </div>
-      </b-container>
-    </div>
+          </h1>
+        </b-jumbotron>
+      </div>
+
+      <div>
+        <b-jumbotron>
+          <div class="mb-6"><h2>Einlösen</h2></div>
+
+          <b-row>
+            <b-col col sm="12" md="6">
+              <p>Du hast noch kein Gradido Konto</p>
+              <b-button variant="primary" to="/register">Registriere ein neues Konto</b-button>
+            </b-col>
+            <b-col sm="12" md="6" class="mt-xs-6 mt-sm-6 mt-md-0">
+              <p>Du hast einen Gradido Konto</p>
+              <b-button variant="info" to="/login">Log dich ein</b-button>
+            </b-col>
+          </b-row>
+        </b-jumbotron>
+      </div>
+    </b-container>
   </div>
 </template>
 <script>
@@ -25,6 +37,7 @@ export default {
   name: 'ShowTransactionLinkInformations',
   data() {
     return {
+      img: '/img/brand/green.png',
       displaySetup: {
         user: {
           firstName: '',
