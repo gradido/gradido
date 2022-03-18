302b2705ca051cc5547e1a27a0e3405f2ce41b75
ogerly <fridolin@tutanota.com>
2022-03-18 19:13:36 +0100

    tests for ShowTransactionLinkInformations.vue

--- frontend/src/pages/ShowTransactionLinkInformations.spec.js
+++ frontend/src/pages/ShowTransactionLinkInformations.spec.js
@@ -1,5 +1,8 @@
 import { mount } from '@vue/test-utils'
 import ShowTransactionLinkInformations from './ShowTransactionLinkInformations'
+import { queryTransactionLink } from '@/graphql/queries'
+import { redeemTransactionLink } from '@/graphql/mutations'
+import { wrap } from 'regenerator-runtime'
 
 const localVue = global.localVue
 
@@ -7,8 +10,18 @@ const errorHandler = jest.fn()
 
 localVue.config.errorHandler = errorHandler
 
-const queryTransactionLink = jest.fn()
-queryTransactionLink.mockResolvedValue('success')
+const apolloQueryMock = jest.fn()
+apolloQueryMock.mockResolvedValue({
+  id: 92,
+  amount: '22',
+  memo: 'Abrakadabra drei, vier, fünf, sechs, hier steht jetzt ein Memotext! Hex hex ',
+  createdAt: '2022-03-17T16:10:28.000Z',
+  validUntil: '2022-03-31T16:10:28.000Z',
+  redeemedAt: '2022-03-18T10:08:43.000Z',
+  deletedAt: null,
+  user: { firstName: 'Bibi', publisherId: 0, email: 'bibi@bloxberg.de', __typename: 'User' },
+  __typename: 'TransactionLink',
+})
 
 const createMockObject = (code) => {
   return {
@@ -24,11 +37,11 @@ const createMockObject = (code) => {
         },
       },
       $apollo: {
-        query: queryTransactionLink,
+        query: apolloQueryMock,
       },
       $route: {
         params: {
-          code,
+          code: 'c0000c0000c0000',
         },
       },
     },
@@ -49,6 +62,19 @@ describe('ShowTransactionLinkInformations', () => {
 
     it('renders the component', () => {
       expect(wrapper.find('div.show-transaction-link-informations').exists()).toBeTruthy()
+    })
+
+    it('calls the queryTransactionLink query', () => {
+      expect(apolloQueryMock).toBeCalledWith({
+        query: queryTransactionLink,
+        variables: {
+          code: 'c0000c0000c0000',
+        },
+      })
+    })
+
+    it('has link one information link data', () => {
+      expect(apolloQueryMock.mockResolvedValue).toHaveLength(1)
     })
   })
 })
