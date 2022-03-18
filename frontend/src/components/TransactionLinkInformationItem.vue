26ec3f51bb7c8cafe458f79f58de0996178d8201
ogerly <fridolin@tutanota.com>
2022-03-18 19:15:19 +0100

    add slot system for transaction link information items

---
+++ frontend/src/components/TransactionLinkInformationItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <slot :name="type"></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TransactionLinkInformationItem',
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
