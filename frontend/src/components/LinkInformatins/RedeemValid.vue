0132d95cf82cc2d6231cd39258b3e44ce05912e8
ogerly <fridolin@tutanota.com>
2022-03-20 11:36:16 +0100

    remove props id and delete id from emit

--- frontend/src/components/LinkInformatins/RedeemValid.vue
+++ frontend/src/components/LinkInformatins/RedeemValid.vue
@@ -10,7 +10,7 @@
 
     <b-jumbotron>
       <div class="mb-3 text-center">
-        <b-button variant="primary" @click="$emit('redeem-link', id, amount)" size="lg">
+        <b-button variant="primary" @click="$emit('redeem-link', amount)" size="lg">
           {{ $t('gdd_per_link.redeem') }}
         </b-button>
       </div>
@@ -21,7 +21,6 @@
 export default {
   name: 'RedeemValid',
   props: {
-    id: { type: Number, required: false },
     user: { type: Object, required: false },
     amount: { type: String, required: false },
     memo: { type: String, required: false, default: '' },
