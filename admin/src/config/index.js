1d6dace9f46f7085839fb5bfd658c781a3be23c4
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2022-03-22 13:14:13 +0100

    implement admin config checking

--- admin/src/config/index.js
+++ admin/src/config/index.js
@@ -4,11 +4,20 @@
 // Load Package Details for some default values
 const pkg = require('../../package')
 
+const constants = {
+  CONFIG_VERSION: {
+    DEFAULT: 'DEFAULT',
+    EXPECTED: 'v1.2022-03-18',
+    CURRENT: '',
+  },
+}
+
 const version = {
   APP_VERSION: pkg.version,
   BUILD_COMMIT: process.env.BUILD_COMMIT || null,
   // self reference of `version.BUILD_COMMIT` is not possible at this point, hence the duplicate code
   BUILD_COMMIT_SHORT: (process.env.BUILD_COMMIT || '0000000').substr(0, 7),
+  PORT: process.env.PORT || 8080,
 }
 
 const environment = {
@@ -27,14 +36,24 @@ const debug = {
   DEBUG_DISABLE_AUTH: process.env.DEBUG_DISABLE_AUTH === 'true' || false,
 }
 
-const options = {}
+// Check config version
+constants.CONFIG_VERSION.CURRENT = process.env.CONFIG_VERSION || constants.CONFIG_VERSION.DEFAULT
+if (
+  ![constants.CONFIG_VERSION.EXPECTED, constants.CONFIG_VERSION.DEFAULT].includes(
+    constants.CONFIG_VERSION.CURRENT,
+  )
+) {
+  throw new Error(
+    `Fatal: Config Version incorrect - expected "${constants.CONFIG_VERSION.EXPECTED}" or "${constants.CONFIG_VERSION.DEFAULT}", but found "${constants.CONFIG_VERSION.CURRENT}"`,
+  )
+}
 
 const CONFIG = {
+  ...constants,
   ...version,
   ...environment,
   ...endpoints,
-  ...options,
   ...debug,
 }
 
-export default CONFIG
+module.exports = CONFIG
