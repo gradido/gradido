a8f228f3a9608c2c230bb2215e3b0128590ad939
ogerly <fridolin@tutanota.com>
2022-03-24 09:06:37 +0100

    add extra order for QrCode components, add component FigureQrCode.vue

---
+++ frontend/src/components/QrCode/FigureQrCode.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="figure-qr-code">
+    <figure class="qrcode">
+      <vue-qrcode :value="text" type="image/png" class="qrbox"></vue-qrcode>
+      <img class="qrcode__image" src="img/gdd-coin.png" alt="GDD" />
+    </figure>
+  </div>
+</template>
+<script>
+import VueQrcode from 'vue-qrcode'
+
+export default {
+  name: 'FigureQrCode',
+  components: {
+    VueQrcode,
+  },
+  props: {
+    text: { type: String, required: true },
+  },
+}
+</script>
+<style scoped>
+.qrcode {
+  display: inline-block;
+  font-size: 0;
+  margin-bottom: 0;
+  position: relative;
+}
+.qrbox {
+  width: 400px;
+}
+.qrcode__image {
+  background-color: #fff;
+  border: 0.25rem solid #fff;
+  border-radius: 0.25rem;
+  box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.25);
+  height: 15%;
+  left: 50%;
+  overflow: hidden;
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 15%;
+}
+</style>
