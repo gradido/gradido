db8e11264869f0919bb9c3cea97f657ff5e5158b
ogerly <fridolin@tutanota.com>
2022-03-25 20:19:01 +0100

    add count in graphql query

--- admin/src/graphql/listTransactionLinksAdmin.js
+++ admin/src/graphql/listTransactionLinksAdmin.js
@@ -3,15 +3,18 @@ import gql from 'graphql-tag'
 export const listTransactionLinksAdmin = gql`
   query ($currentPage: Int = 1, $pageSize: Int = 5, $userId: Int!) {
     listTransactionLinksAdmin(currentPage: $currentPage, pageSize: $pageSize, userId: $userId) {
-      id
-      amount
-      holdAvailableAmount
-      memo
-      code
-      createdAt
-      validUntil
-      redeemedAt
-      deletedAt
+      linkCount
+      linkList {
+        id
+        amount
+        holdAvailableAmount
+        memo
+        code
+        createdAt
+        validUntil
+        redeemedAt
+        deletedAt
+      }
     }
   }
 `
