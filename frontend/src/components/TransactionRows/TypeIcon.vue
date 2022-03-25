fe2d1caa108d951456e42a46496cd43c0cf5835b
ogerly <fridolin@tutanota.com>
2022-03-25 21:48:47 +0100

    add icon if transactionLinkId on transaction

--- frontend/src/components/TransactionRows/TypeIcon.vue
+++ frontend/src/components/TransactionRows/TypeIcon.vue
@@ -2,6 +2,13 @@
   <div class="type-icon">
     <div class="gdd-transaction-list-item-icon">
       <b-icon :icon="icon" :class="color" class="m-mb-1 font2em" />
+      <b-icon
+        v-if="transactionLinkId"
+        icon="link45deg"
+        variant="muted"
+        class="m-mb-1"
+        :title="$t('gdd_per_link.redeemed-title')"
+      />
     </div>
   </div>
 </template>
@@ -16,6 +23,11 @@ export default {
     color: {
       type: String,
       required: true,
+    },
+    transactionLinkId: {
+      type: Number,
+      required: false,
+      default: null,
     },
   },
 }
