380bc0787c27c13b8a5dc78f0ddf5a33d0d507b2
ogerly <fridolin@tutanota.com>
2022-03-27 11:04:33 +0200

    remove particles-bg-vue from system

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,12 @@
 <template>
   <div id="app" class="font-sans text-gray-800">
     <div>
-      <particles-bg v-if="$store.state.coinanimation" type="custom" :config="config" :bg="true" />
       <component :is="$route.meta.requiresAuth ? 'DashboardLayout' : 'AuthLayoutGDD'" />
     </div>
   </div>
 </template>
 
 <script>
-import { ParticlesBg } from 'particles-bg-vue'
 import icon from './icon.js'
 import DashboardLayout from '@/layouts/DashboardLayout_gdd.vue'
 import AuthLayoutGDD from '@/layouts/AuthLayout_gdd.vue'
@@ -16,7 +14,6 @@ import AuthLayoutGDD from '@/layouts/AuthLayout_gdd.vue'
 export default {
   name: 'app',
   components: {
-    ParticlesBg,
     DashboardLayout,
     AuthLayoutGDD,
   },
