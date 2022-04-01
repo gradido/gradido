ff04cf0cb5b911238badc61efe73cbf248cf1a30
Moriz Wahl <moriz.wahl@gmx.de>
2022-04-01 14:08:55 +0200

    do not round in decay model as it is used in utils on creation

--- backend/src/graphql/model/Decay.ts
+++ backend/src/graphql/model/Decay.ts
@@ -11,7 +11,7 @@ export class Decay {
     duration: number | null,
   ) {
     this.balance = balance
-    this.decay = decay.toDecimalPlaces(2, Decimal.ROUND_FLOOR)
+    this.decay = decay
     this.start = start
     this.end = end
     this.duration = duration
