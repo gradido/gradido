7ec35bbfb238440caea2874b6aaf0b42de31b152
Moriz Wahl <moriz.wahl@gmx.de>
2022-04-11 15:50:02 +0200

    define context interface

--- backend/src/server/context.ts
+++ backend/src/server/context.ts
@@ -1,9 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { Role } from '@/auth/Role'
+import { User as dbUser } from '@entity/User'
 
-const context = (args: any) => {
+export interface Context {
+  token: string | null
+  setHeaders: { key: string, value: string }[]
+  role?: Role
+  user?: dbUser
+}
+
+const context = (args: any): Context => {
   const authorization = args.req.headers.authorization
-  let token = null
+  let token: string | null = null
   if (authorization) {
     token = authorization.replace(/^Bearer /, '')
   }
