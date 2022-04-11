875c97374d13b564c856c3672f5eeb9516aaf809
ogerly <fridolin@tutanota.com>
2022-04-11 08:05:13 +0200

    add vue-meta in dashboard-plugin.js

--- frontend/src/plugins/dashboard-plugin.js
+++ frontend/src/plugins/dashboard-plugin.js
@@ -18,6 +18,8 @@ import 'vue-loading-overlay/dist/vue-loading.css'
 
 import VueApollo from 'vue-apollo'
 
+import VueMeta from 'vue-meta'
+
 export default {
   install(Vue) {
     Vue.use(GlobalComponents)
@@ -29,5 +31,9 @@ export default {
     Vue.use(FlatPickr)
     Vue.use(Loading)
     Vue.use(VueApollo)
+    Vue.use(VueMeta, {
+      // optional pluginOptions
+      refreshOnceOnNavigation: true,
+    })
   },
 }
