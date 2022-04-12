a6e70363392ef88aef007c721cf5c5649c3970f8
Moriz Wahl <moriz.wahl@gmx.de>
2022-04-12 21:34:33 +0200

    remove unused fields from balance model

--- backend/src/graphql/model/Balance.ts
+++ backend/src/graphql/model/Balance.ts
@@ -5,32 +5,19 @@ import Decimal from 'decimal.js-light'
 export class Balance {
   constructor(data: {
     balance: Decimal
-    decay: Decimal
-    lastBookedBalance: Decimal
     balanceGDT: number | null
     count: number
     linkCount: number
-    lastBookedDate?: Date | null
   }) {
     this.balance = data.balance
-    this.decay = data.decay
-    this.lastBookedBalance = data.lastBookedBalance
     this.balanceGDT = data.balanceGDT || null
     this.count = data.count
     this.linkCount = data.linkCount
-    this.lastBookedDate = data.lastBookedDate || null
   }
 
   // the actual balance, decay included
   @Field(() => Decimal)
   balance: Decimal
-
-  // the decay since the last booked balance
-  @Field(() => Decimal)
-  decay: Decimal
-
-  @Field(() => Decimal)
-  lastBookedBalance: Decimal
 
   @Field(() => Number, { nullable: true })
   balanceGDT: number | null
@@ -42,8 +29,4 @@ export class Balance {
   // the count of transaction links
   @Field(() => Number)
   linkCount: number
-
-  // may be null as there may be no transaction
-  @Field(() => Date, { nullable: true })
-  lastBookedDate: Date | null
 }
