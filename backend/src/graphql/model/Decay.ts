6691be813f640f3e119432901e4ee20b7a5354fb
Moriz Wahl <moriz.wahl@gmx.de>
2022-04-20 12:44:21 +0200

    decontruct input

--- backend/src/graphql/model/Decay.ts
+++ backend/src/graphql/model/Decay.ts
@@ -1,22 +1,24 @@
 import { ObjectType, Field, Int } from 'type-graphql'
 import Decimal from 'decimal.js-light'
 
+interface DecayInterface {
+  balance: Decimal
+  decay: Decimal
+  roundedDecay: Decimal
+  start: Date | null
+  end: Date | null
+  duration: number | null
+}
+
 @ObjectType()
 export class Decay {
-  constructor(data: {
-    balance: Decimal
-    decay: Decimal
-    roundedDecay: Decimal
-    start: Date | null
-    end: Date | null
-    duration: number | null
-  }) {
-    this.balance = data.balance
-    this.decay = data.decay
-    this.roundedDecay = data.roundedDecay
-    this.start = data.start
-    this.end = data.end
-    this.duration = data.duration
+  constructor({ balance, decay, roundedDecay, start, end, duration }: DecayInterface) {
+    this.balance = balance
+    this.decay = decay
+    this.roundedDecay = roundedDecay
+    this.start = start
+    this.end = end
+    this.duration = duration
   }
 
   @Field(() => Decimal)
