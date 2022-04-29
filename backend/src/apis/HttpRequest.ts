754dc16e989b22da90594b1c656a78df93501f1d
Claus-Peter Hübner <claus-peter.huebner@email.de>
2022-04-30 00:25:09 +0200

    test loggers included

--- backend/src/apis/HttpRequest.ts
+++ backend/src/apis/HttpRequest.ts
@@ -1,10 +1,17 @@
 import axios from 'axios'
+import log4js from 'log4js'
+import CONFIG from '@/config'
+
+log4js.configure(CONFIG.LOG4JS_CONFIG)
+const logger = log4js.getLogger('http')
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const apiPost = async (url: string, payload: unknown): Promise<any> => {
+  logger.trace('POST: url=' + url + ' payload=' + payload)
   return axios
     .post(url, payload)
     .then((result) => {
+      logger.trace('POST-Response: result=' + result)
       if (result.status !== 200) {
         throw new Error('HTTP Status Error ' + result.status)
       }
@@ -20,9 +27,11 @@ export const apiPost = async (url: string, payload: unknown): Promise<any> => {
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const apiGet = async (url: string): Promise<any> => {
+  logger.trace('GET: url=' + url)
   return axios
     .get(url)
     .then((result) => {
+      logger.trace('GET-Response: result=' + result)
       if (result.status !== 200) {
         throw new Error('HTTP Status Error ' + result.status)
       }
