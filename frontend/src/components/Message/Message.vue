e597ef4acc5995a3be593da4e840c864aab820c7
Wolfgang Huß <wolle.huss@pjannto.com>
2022-05-05 12:13:07 +0200

    Change to methods to computed in Message component after Moriz suggestion

--- frontend/src/components/Message/Message.vue
+++ frontend/src/components/Message/Message.vue
@@ -6,7 +6,7 @@
           <p class="h1 test-message-headline">{{ headline }}</p>
           <p class="h4 test-message-subtitle">{{ subtitle }}</p>
           <hr />
-          <b-button v-if="showButton()" class="test-message-button" :to="buttonLinkTo()">
+          <b-button v-if="showButton" class="test-message-button" :to="buttonLinkTo">
             {{ buttonText }}
           </b-button>
         </div>
@@ -24,7 +24,7 @@ export default {
     buttonText: { type: String, required: false, default: null },
     linkTo: { type: String, required: false, default: null },
   },
-  methods: {
+  computed: {
     showButton() {
       return this.buttonText && this.linkTo
     },
