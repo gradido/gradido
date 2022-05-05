c10115397568550cfaf69eb5317a97e2f8e0f049
Wolfgang Huß <wolle.huss@pjannto.com>
2022-05-05 16:30:35 +0200

    Fix quickly the account sum problem for the seeding of bob's transactions

--- backend/src/seeds/index.ts
+++ backend/src/seeds/index.ts
@@ -66,7 +66,10 @@ const run = async () => {
 
   // create GDD
   for (let i = 0; i < creations.length; i++) {
+    const now = new Date().getTime() // we have to wait a little! quick fix for account sum problem of bob@baumeister.de
     await creationFactory(seedClient, creations[i])
+    // eslint-disable-next-line no-empty
+    while (new Date().getTime() < now + 1000) {} // we have to wait a little! quick fix for account sum problem of bob@baumeister.de
   }
 
   // create Transaction Links
