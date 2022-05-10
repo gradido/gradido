6ce850dc1e3d9ad273fa87986ffc72363806c15a
Moriz Wahl <moriz.wahl@gmx.de>
2022-05-10 19:31:45 +0200

    read log level from config to override default

--- backend/src/server/logger.ts
+++ backend/src/server/logger.ts
@@ -1,7 +1,13 @@
 import log4js, { Logger } from 'log4js'
 import CONFIG from '@/config'
 
-log4js.configure(CONFIG.LOG4JS_CONFIG)
+import { readFileSync } from 'fs'
+
+const options = JSON.parse(readFileSync(CONFIG.LOG4JS_CONFIG, 'utf-8'))
+
+options.categories.default.level = CONFIG.LOG_LEVEL
+
+log4js.configure(options)
 
 export const getLogger = (name: string): Logger => {
   return log4js.getLogger(name)
