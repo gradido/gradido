ca3cec8927b1cfa2575ba38ca67584ef1e353055
ogerly <fridolin@tutanota.com>
2022-05-19 06:48:47 +0200

    add replace emotjis from text to '' 2

--- frontend/src/components/GddSend/TransactionForm.vue
+++ frontend/src/components/GddSend/TransactionForm.vue
@@ -189,7 +189,7 @@ export default {
     onSubmit() {
       this.normalizeAmount(true)
       // remove emotjis from memo
-      this.form.memo
+      this.form.memo = this.form.memo
         .replace(
           /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
           '',
