4c7fc10d8f13378759a30f51589282d1ccf338bb
Moriz Wahl <moriz.wahl@gmx.de>
2022-05-30 10:57:25 +0200

    change DB version

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -10,7 +10,7 @@ Decimal.set({
 })
 
 const constants = {
-  DB_VERSION: '0036-unique_previous_in_transactions',
+  DB_VERSION: '0037-contributions_table',
   DECAY_START_TIME: new Date('2021-05-13 17:46:31'), // GMT+0
   LOG4JS_CONFIG: 'log4js-config.json',
   // default log level on production should be info
