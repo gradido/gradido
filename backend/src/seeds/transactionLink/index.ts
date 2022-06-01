3f8c1169ba9d7377dfaec58c55a07596a235d410
Wolfgang Huß <wolle.huss@pjannto.com>
2022-06-01 10:19:46 +0200

    Fix wrongly changed month on transaction links data

--- backend/src/seeds/transactionLink/index.ts
+++ backend/src/seeds/transactionLink/index.ts
@@ -5,7 +5,7 @@ export const transactionLinks: TransactionLinkInterface[] = [
     email: 'bibi@bloxberg.de',
     amount: 19.99,
     memo: 'Leider wollte niemand meine Gradidos zum Neujahr haben :(',
-    createdAt: new Date(2022, 1, 1),
+    createdAt: new Date(2022, 0, 1),
   },
   {
     email: 'bibi@bloxberg.de',
