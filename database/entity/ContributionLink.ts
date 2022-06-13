83c8f9731e87749de795968905b45287f3bb2981
Moriz Wahl <moriz.wahl@gmx.de>
2022-06-13 12:05:13 +0200

    remove duplicate file

--- database/entity/ContributionLink.ts
+++ database/entity/ContributionLink.ts
@@ -1,1 +1,1 @@
-export { ContributionLinks } from './0038-add_contribution_links_table/ContributionLinks'
+export { ContributionLink } from './0038-add_contribution_links_table/ContributionLink'
--- database/entity/ContributionLinks.ts
+++
@@ -1,1 +0,0 @@
-export { ContributionLink } from './0038-add_contribution_links_table/ContributionLink'
