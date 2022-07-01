9816adc745323d3b0922113d913449ba1bef5a3e
Moriz Wahl <moriz.wahl@gmx.de>
2022-07-01 17:24:51 +0200

    remove unused global middleware

--- backend/src/graphql/schema.ts
+++ backend/src/graphql/schema.ts
@@ -1,7 +1,6 @@
 import { GraphQLSchema } from 'graphql'
 import { buildSchema } from 'type-graphql'
 import path from 'path'
-// import { EventHandler } from '@/middleware/EventHandler'
 
 import isAuthorized from './directive/isAuthorized'
 import DecimalScalar from './scalar/Decimal'
@@ -12,7 +11,6 @@ const schema = async (): Promise<GraphQLSchema> => {
     resolvers: [path.join(__dirname, 'resolver', `!(*.test).{js,ts}`)],
     authChecker: isAuthorized,
     scalarsMap: [{ type: Decimal, scalar: DecimalScalar }],
-    //    globalMiddlewares: [EventHandler],
   })
 }
 
