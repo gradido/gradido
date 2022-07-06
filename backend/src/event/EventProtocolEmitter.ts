46e2473a88b828c65ae594a7f899b4d7a9a3e2c8
Claus-Peter Hübner <claus-peter.huebner@email.de>
2022-07-07 01:48:33 +0200

    remove EventEmitter from events-modul and replace it with writeEvent-Methode

--- backend/src/event/EventProtocolEmitter.ts
+++ backend/src/event/EventProtocolEmitter.ts
@@ -1,4 +1,3 @@
-import { EventEmitter } from 'events'
 import { Event } from '@/event/Event'
 import { backendLogger as logger } from '@/server/logger'
 // import { EventProtocolType } from './EventProtocolType'
@@ -6,7 +5,8 @@ import { EventProtocol } from '@entity/EventProtocol'
 // import { getConnection } from '@dbTools/typeorm'
 import CONFIG from '@/config'
 
-class EventProtocolEmitter extends EventEmitter {
+class EventProtocolEmitter {
+  /* }extends EventEmitter { */
   private events: Event[]
 
   public addEvent(event: Event) {
@@ -21,9 +21,25 @@ class EventProtocolEmitter extends EventEmitter {
     logger.info(`EventProtocol - isEnabled=${CONFIG.EVENT_PROTOCOL_ENABLED}`)
     return CONFIG.EVENT_PROTOCOL_ENABLED
   }
+
+  public async writeEvent(event: Event): Promise<void> {
+    // if (!eventProtocol.isEnabled()) return
+    logger.info(`writeEvent(${JSON.stringify(event)})`)
+    const dbEvent = new EventProtocol()
+    dbEvent.type = event.type
+    dbEvent.createdAt = event.createdAt
+    dbEvent.userId = event.userId
+    if (event.xUserId) dbEvent.xUserId = event.xUserId
+    if (event.xCommunityId) dbEvent.xCommunityId = event.xCommunityId
+    if (event.contributionId) dbEvent.contributionId = event.contributionId
+    if (event.transactionId) dbEvent.transactionId = event.transactionId
+    if (event.amount) dbEvent.amount = event.amount
+    await dbEvent.save()
+  }
 }
 export const eventProtocol = new EventProtocolEmitter()
 
+/*
 eventProtocol.on('error', (err) => {
   logger.error(`ERROR in EventProtocol: ${err}`)
 })
@@ -37,18 +53,4 @@ eventProtocol.on('writeEvents', async (events: Event[]) => {
 eventProtocol.on('writeEvent', async (event: Event) => {
   await writeEvent(event)
 })
-
-const writeEvent = async (event: Event): Promise<void> => {
-  // if (!eventProtocol.isEnabled()) return
-  logger.info(`writeEvent(${JSON.stringify(event)})`)
-  const dbEvent = new EventProtocol()
-  dbEvent.type = event.type
-  dbEvent.createdAt = event.createdAt
-  dbEvent.userId = event.userId
-  if (event.xUserId) dbEvent.xUserId = event.xUserId
-  if (event.xCommunityId) dbEvent.xCommunityId = event.xCommunityId
-  if (event.contributionId) dbEvent.contributionId = event.contributionId
-  if (event.transactionId) dbEvent.transactionId = event.transactionId
-  if (event.amount) dbEvent.amount = event.amount
-  await dbEvent.save()
-}
+*/
