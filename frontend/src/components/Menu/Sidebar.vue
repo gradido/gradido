7676e7e7f3be322e3319fc58419d6a3298c08424
ogerly <fridolin@tutanota.com>
2022-07-07 08:15:24 +0200

    create page Community.vue

--- frontend/src/components/Menu/Sidebar.vue
+++ frontend/src/components/Menu/Sidebar.vue
@@ -20,6 +20,10 @@
             <b-icon icon="gear" aria-hidden="true"></b-icon>
             {{ $t('navigation.profile') }}
           </b-nav-item>
+          <b-nav-item to="/community" class="mb-3">
+            <b-icon icon="people" aria-hidden="true"></b-icon>
+            {{ $t('navigation.community') }}
+          </b-nav-item>
         </b-nav>
         <hr />
         <b-nav vertical class="w-100">
---
+++ frontend/src/pages/Community.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="community-page">
+    <div>
+      <b-tabs content-class="mt-3" align="center">
+        <b-tab :title="$t('community.writing')" active>
+          <p>{{ $t('community.writing') }}</p>
+        </b-tab>
+        <b-tab :title="$t('community.myContributions')">
+          <p>{{ $t('community.myContributions') }}</p>
+        </b-tab>
+        <b-tab :title="$t('navigation.community')">
+          <p>{{ $t('navigation.community') }}</p>
+        </b-tab>
+      </b-tabs>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Community',
+}
+</script>
