c3d62db1e5bdb8d2eeaf7b4eefaca40313d3fded
ogerly <fridolin@tutanota.com>
2022-07-08 11:34:55 +0200

    add componente ContributionList.vue

---
+++ frontend/src/components/Contributions/ContributionList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="contribution-list container">
+    <div v-for="item in items" :key="item.id" class="pb-3">
+      {{ item }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
