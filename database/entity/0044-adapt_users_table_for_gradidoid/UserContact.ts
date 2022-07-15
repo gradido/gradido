138891dfe36ad44f5375cabe388995195850f2e1
Claus-Peter Hübner <claus-peter.huebner@email.de>
2022-07-15 16:37:49 +0200

    add user_contacts table

---
+++ database/entity/0044-adapt_users_table_for_gradidoid/UserContact.ts
@@ -0,0 +1,40 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, DeleteDateColumn } from 'typeorm'
+
+@Entity('user_contacts', { engine: 'InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci' })
+export class UserContact extends BaseEntity {
+  @PrimaryGeneratedColumn('increment', { unsigned: true })
+  id: number
+
+  @Column({
+    name: 'type',
+    length: 100,
+    nullable: true,
+    default: null,
+    collation: 'utf8mb4_unicode_ci',
+  })
+  type: string
+
+  @Column({ name: 'users_id', type: 'int', unsigned: true, nullable: false })
+  usersId?: number | null
+
+  @Column({ length: 255, unique: true, nullable: false, collation: 'utf8mb4_unicode_ci' })
+  email: string
+
+  @Column({ name: 'email_hash', type: 'binary', length: 32, default: null, nullable: true })
+  emailHash: Buffer
+
+  @Column({ name: 'email_checked', type: 'bool', nullable: false, default: false })
+  emailChecked: boolean
+
+  @Column({ length: 255, unique: false, nullable: true, collation: 'utf8mb4_unicode_ci' })
+  phone: string
+
+  @Column({ name: 'created', default: () => 'CURRENT_TIMESTAMP', nullable: false })
+  createdAt: Date
+
+  @DeleteDateColumn()
+  updatedAt: Date | null
+
+  @DeleteDateColumn()
+  deletedAt: Date | null
+}
---
+++ database/entity/UserContact.ts
@@ -0,0 +1,1 @@
+export { UserContact } from './0044-adapt_users_table_for_gradidoid/UserContact'
