f0c83321fab57ae0d48d6feba34eb40223edc6ff
Moriz Wahl <moriz.wahl@gmx.de>
2022-07-19 14:55:02 +0200

    fix broeken .envs and config

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -12,6 +12,8 @@ Decimal.set({
 const constants = {
   DB_VERSION: '0043-add_event_protocol_table',
   DECAY_START_TIME: new Date('2021-05-13 17:46:31'), // GMT+0
+  LOG4JS_CONFIG: 'log4js-config.json',
+  // default log level on production should be info
   LOG_LEVEL: process.env.LOG_LEVEL || 'info',
   CONFIG_VERSION: {
     DEFAULT: 'DEFAULT',
@@ -52,6 +54,8 @@ const community = {
   COMMUNITY_URL: process.env.COMMUNITY_URL || 'http://localhost/',
   COMMUNITY_REGISTER_URL: process.env.COMMUNITY_REGISTER_URL || 'http://localhost/register',
   COMMUNITY_REDEEM_URL: process.env.COMMUNITY_REDEEM_URL || 'http://localhost/redeem/{code}',
+  COMMUNITY_REDEEM_CONTRIBUTION_URL:
+    process.env.COMMUNITY_REDEEM_CONTRIBUTION_URL || 'http://localhost/redeem/CL-{code}',
   COMMUNITY_DESCRIPTION:
     process.env.COMMUNITY_DESCRIPTION || 'Die lokale Entwicklungsumgebung von Gradido.',
 }
