b5b5527b59db7844d40dc3c55142a10d35dc339f
elweyn <heine.hannes@gmail.com>
2022-07-28 15:41:16 +0200

    Add community item to the mobile device.

--- frontend/src/components/Menu/Navbar.vue
+++ frontend/src/components/Menu/Navbar.vue
@@ -52,6 +52,10 @@
           <b-icon icon="layout-text-sidebar-reverse" aria-hidden="true"></b-icon>
           {{ $t('navigation.transactions') }}
         </b-nav-item>
+        <b-nav-item to="/community" class="mb-3">
+          <b-icon icon="people" aria-hidden="true"></b-icon>
+          {{ $t('navigation.community') }}
+        </b-nav-item>
         <b-nav-item to="/profile" class="mb-3">
           <b-icon icon="gear" aria-hidden="true"></b-icon>
           {{ $t('navigation.profile') }}
@@ -60,9 +64,6 @@
         <b-nav-item v-if="$store.state.hasElopage" :href="elopageUri" class="mb-3" target="_blank">
           <b-icon icon="link45deg" aria-hidden="true"></b-icon>
           {{ $t('navigation.members_area') }}
-          <b-badge v-if="!$store.state.hasElopage" pill variant="danger">
-            {{ $t('math.exclaim') }}
-          </b-badge>
         </b-nav-item>
         <b-nav-item class="mb-3" v-if="$store.state.isAdmin" @click="$emit('admin')">
           <b-icon icon="shield-check" aria-hidden="true"></b-icon>
