b124921d339114db6b212f61b0b0b660cbfb1be9
ogerly <fridolin@tutanota.com>
2022-08-08 15:02:47 +0200

    add test for CommunityStatistic.vue

---
+++ admin/src/components/CommunityStatistic.spec.js
@@ -0,0 +1,38 @@
+import { mount } from '@vue/test-utils'
+import CommunityStatistic from './CommunityStatistic'
+
+const localVue = global.localVue
+
+const mocks = {
+  $t: jest.fn((t) => t),
+}
+
+const propsData = {
+  value: {
+    totalUsers: '123',
+    activeUsers: '100',
+    deletedUsers: '5',
+    totalGradidoCreated: '2500',
+    totalGradidoDecayed: '200',
+    totalGradidoAvailable: '500',
+    totalGradidoUnbookedDecayed: '111',
+  },
+}
+
+describe('CommunityStatistic', () => {
+  let wrapper
+
+  const Wrapper = () => {
+    return mount(CommunityStatistic, { localVue, mocks, propsData })
+  }
+
+  describe('mount', () => {
+    beforeEach(() => {
+      wrapper = Wrapper()
+    })
+
+    it('renders the Div Element ".community-statistic"', () => {
+      expect(wrapper.find('div.community-statistic').exists()).toBe(true)
+    })
+  })
+})
