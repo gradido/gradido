722b0e4757dc5c04ade2afb479cd3f350e9f3f11
Alexander Friedland <ogerly@users.noreply.github.com>
2022-08-16 11:37:10 +0200

    Update frontend/src/components/GddSend/TransactionConfirmationLink.spec.js
    Co-authored-by: Moriz Wahl <moriz.wahl@gmx.de>

--- frontend/src/components/GddSend/TransactionConfirmationLink.spec.js
+++ frontend/src/components/GddSend/TransactionConfirmationLink.spec.js
@@ -47,7 +47,7 @@ describe('GddSend confirm', () => {
       })
     })
 
-    describe('has totalBalance under 0', () => {
+    describe('has total balance equal 0', () => {
       beforeEach(async () => {
         await wrapper.setProps({
           balance: 0,
