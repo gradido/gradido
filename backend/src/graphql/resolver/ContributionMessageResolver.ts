281f6cde3feb3cd04b054aeaf94564f8d6d74b98
elweyn <heine.hannes@gmail.com>
2022-08-22 10:39:56 +0200

    Update contributionStatus to new state.

--- backend/src/graphql/resolver/ContributionMessageResolver.ts
+++ backend/src/graphql/resolver/ContributionMessageResolver.ts
@@ -5,6 +5,7 @@ import { Args, Authorized, Ctx, Mutation, Resolver } from 'type-graphql'
 import ContributionMessageArgs from '@arg/ContributionMessageArgs'
 import { Contribution } from '@entity/Contribution'
 import { ContributionMessageType } from '@enum/MessageType'
+import { ContributionStatus } from '../enum/ContributionStatus'
 
 @Resolver()
 export class ContributionResolver {
@@ -26,8 +27,18 @@ export class ContributionResolver {
     contributionMessage.message = message
     contributionMessage.userId = user.id
     contributionMessage.type = ContributionMessageType.DIALOG
-
     ContributionMessage.save(contributionMessage)
+
+    if (user.isAdmin && contribution.contributionStatus === ContributionStatus.PENDING) {
+      contribution.contributionStatus = ContributionStatus.IN_PROGRESS
+    } else if (
+      !user.isAdmin &&
+      contribution.contributionStatus === ContributionStatus.IN_PROGRESS
+    ) {
+      contribution.contributionStatus = ContributionStatus.PENDING
+    }
+    Contribution.save(contribution)
+
     return contributionMessage
   }
 }
