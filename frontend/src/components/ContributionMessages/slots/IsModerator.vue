0def6472d524ee7484e9ad4a894a3aa1ad24dc80
ogerly <fridolin@tutanota.com>
2022-08-23 14:30:03 +0200

    add files for ContributionMessages

---
+++ frontend/src/components/ContributionMessages/slots/IsModerator.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="slot-is-moderator">
+    <b-avatar square text="AA"></b-avatar>
+    <span class="ml-2 mr-2">{{ message.firstName }} {{ message.lastName }}</span>
+    <div class="mt-2">{{ message }}</div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    message: {
+      type: Object,
+      default() {
+        return {}
+      },
+    },
+  },
+}
+</script>
+<style>
+.slot-is-moderator {
+  clear: both;
+  background-color: rgb(250, 158, 207);
+  width: 75%;
+  margin-top: 20px;
+}
+</style>
