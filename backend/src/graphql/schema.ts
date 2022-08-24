08eb5945f6866744369aa963040caf1fa6dc9064
Moriz Wahl <moriz.wahl@gmx.de>
2022-08-24 15:19:56 +0200

    try to implement apollo caching, does not work

--- backend/src/graphql/schema.ts
+++ backend/src/graphql/schema.ts
@@ -6,6 +6,8 @@ import isAuthorized from './directive/isAuthorized'
 import DecimalScalar from './scalar/Decimal'
 import Decimal from 'decimal.js-light'
 
+import { SchemaDirectiveVisitor } from 'graphql-tools'
+
 const schema = async (): Promise<GraphQLSchema> => {
   return buildSchema({
     resolvers: [path.join(__dirname, 'resolver', `!(*.test).{js,ts}`)],
@@ -13,5 +15,17 @@ const schema = async (): Promise<GraphQLSchema> => {
     scalarsMap: [{ type: Decimal, scalar: DecimalScalar }],
   })
 }
+
+/*
+const addDirectives = async (schema: Promise<GraphQLSchema>): Promise<void> => {
+  const sc = await schema
+  SchemaDirectiveVisitor.visitSchemaDirectives(sc, {
+    sample: [path.join(__dirname, 'directive', `!(isAuthorized).{js,ts}`)],
+  })
+}
+
+
+addDirectives(schema())
+*/
 
 export default schema
