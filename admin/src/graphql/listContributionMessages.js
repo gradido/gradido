87341f300fd46f4897c2c7a6efa99b97e299f0f6
ogerly <fridolin@tutanota.com>
2022-08-25 10:30:37 +0200

    add graphql query listContributionMessages.js

---
+++ admin/src/graphql/listContributionMessages.js
@@ -0,0 +1,24 @@
+import gql from 'graphql-tag'
+
+export const listContributionMessages = gql`
+  query ($contributionId: Int, $pageSize: Int = 25, $currentPage: Int = 1, $order: Order = DESC) {
+    listContributionMessages(
+      contributionId: $contributionId
+      pageSize: $pageSize
+      currentPage: $currentPage
+      order: $order
+    ) {
+      count
+      messages {
+        id
+        message
+        createdAt
+        updatedAt
+        type
+        userFirstName
+        userLastName
+        userId
+      }
+    }
+  }
+`
