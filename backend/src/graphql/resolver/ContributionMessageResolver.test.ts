7156f1afd5a0c9a3166acc441eaf7fc61bd286d6
elweyn <heine.hannes@gmail.com>
2022-08-29 12:58:01 +0200

    Setup Test file for ContributionMessageResolver.

---
+++ backend/src/graphql/resolver/ContributionMessageResolver.test.ts
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+
+import { bibiBloxberg } from '@/seeds/users/bibi-bloxberg'
+import { cleanDB, resetToken, testEnvironment } from '@test/helpers'
+import { GraphQLError } from 'graphql'
+import { userFactory } from '@/seeds/factory/user'
+import { creationFactory } from '@/seeds/factory/creation'
+import { creations } from '@/seeds/creation/index'
+import { peterLustig } from '@/seeds/users/peter-lustig'
+import { createContributionMessage } from '@/seeds/graphql/mutations'
+
+let mutate: any, query: any, con: any
+let testEnv: any
+let result: any
+
+beforeAll(async () => {
+  testEnv = await testEnvironment()
+  mutate = testEnv.mutate
+  query = testEnv.query
+  con = testEnv.con
+  await cleanDB()
+})
+
+afterAll(async () => {
+  await cleanDB()
+  await con.close()
+})
+
+describe('ContributionMessageResolver', () => {
+  describe('createContributionMessage', () => {
+    describe('unauthenticated', () => {
+      it('returns an error', async () => {
+        await expect(
+          mutate({
+            mutation: createContributionMessage,
+            variables: { contributionId: 1, message: 'This is a test message' },
+          }),
+        ).resolves.toEqual(
+          expect.objectContaining({
+            errors: [new GraphQLError('401 Unauthorized')],
+          }),
+        )
+      })
+    })
+  })
+})
