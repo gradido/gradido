d49d5f6bb7bcb271a38888a98548bcac6755f3c3
elweyn <heine.hannes@gmail.com>
2022-09-09 09:20:14 +0200

    Remove addedContributionMessageEmail test from this file.

--- backend/src/mailer/sendTransactionReceivedEmail.test.ts
+++ backend/src/mailer/sendTransactionReceivedEmail.test.ts
@@ -1,7 +1,4 @@
-import {
-  sendAddedContributionMessageEmail,
-  sendTransactionReceivedEmail,
-} from './sendTransactionReceivedEmail'
+import { sendTransactionReceivedEmail } from './sendTransactionReceivedEmail'
 import { sendEMail } from './sendEMail'
 import Decimal from 'decimal.js-light'
 
@@ -37,36 +34,6 @@ describe('sendTransactionReceivedEmail', () => {
         expect.stringContaining('Bibi Bloxberg') &&
         expect.stringContaining('(bibi@bloxberg.de)') &&
         expect.stringContaining('Vielen herzlichen Dank für den neuen Hexenbesen!') &&
-        expect.stringContaining('http://localhost/overview'),
-    })
-  })
-})
-
-describe('sendAddedContributionMessageEmail', () => {
-  beforeEach(async () => {
-    jest.clearAllMocks()
-    await sendAddedContributionMessageEmail({
-      senderFirstName: 'Peter',
-      senderLastName: 'Lustig',
-      recipientFirstName: 'Bibi',
-      recipientLastName: 'Bloxberg',
-      recipientEmail: 'bibi@bloxberg.de',
-      senderEmail: 'peter@lustig.de',
-      contributionMemo: 'Vielen herzlichen Dank für den neuen Hexenbesen!',
-      message: 'Was für ein Besen ist es geworden?',
-      overviewURL: 'http://localhost/overview',
-    })
-  })
-
-  it('calls sendEMail', () => {
-    expect(sendEMail).toBeCalledWith({
-      to: `Bibi Bloxberg <bibi@bloxberg.de>`,
-      subject: 'Gradido Frage zur Schöpfung',
-      text:
-        expect.stringContaining('Hallo Bibi Bloxberg') &&
-        expect.stringContaining('Peter Lustig') &&
-        expect.stringContaining('Vielen herzlichen Dank für den neuen Hexenbesen!') &&
-        expect.stringContaining('Was für ein Besen ist es geworden?') &&
         expect.stringContaining('http://localhost/overview'),
     })
   })
