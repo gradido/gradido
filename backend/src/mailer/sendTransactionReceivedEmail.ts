d2ff8991001e97cf12e99043edca787be8e0f91b
elweyn <heine.hannes@gmail.com>
2022-09-09 07:41:45 +0200

    Add method for sending EMail when their is a new contributionMessage from admin.

--- backend/src/mailer/sendTransactionReceivedEmail.ts
+++ backend/src/mailer/sendTransactionReceivedEmail.ts
@@ -2,6 +2,7 @@ import { backendLogger as logger } from '@/server/logger'
 import Decimal from 'decimal.js-light'
 import { sendEMail } from './sendEMail'
 import { transactionReceived } from './text/transactionReceived'
+import { contributionMessageReceived } from './text/contributionMessageReceived'
 
 export const sendTransactionReceivedEmail = (data: {
   senderFirstName: string
@@ -24,5 +25,28 @@ export const sendTransactionReceivedEmail = (data: {
     to: `${data.recipientFirstName} ${data.recipientLastName} <${data.email}>`,
     subject: transactionReceived.de.subject,
     text: transactionReceived.de.text(data),
+  })
+}
+
+export const sendAddedContributionMessageEmail = (data: {
+  senderFirstName: string
+  senderLastName: string
+  recipientFirstName: string
+  recipientLastName: string
+  recipientEmail: string
+  senderEmail: string
+  contributionMemo: string
+  message: string
+  overviewURL: string
+}): Promise<boolean> => {
+  logger.info(
+    `sendEmail(): to=${data.recipientFirstName} ${data.recipientLastName} <${data.recipientEmail}>,
+    subject=${contributionMessageReceived.de.subject},
+    text=${contributionMessageReceived.de.text(data)}`,
+  )
+  return sendEMail({
+    to: `${data.recipientFirstName} ${data.recipientLastName} <${data.recipientEmail}>`,
+    subject: contributionMessageReceived.de.subject,
+    text: contributionMessageReceived.de.text(data),
   })
 }
