032348f8c27e9480ab774082f3b8f7c366a43abd
elweyn <heine.hannes@gmail.com>
2022-09-09 09:19:49 +0200

    Remove addedContributionMessageEmail from this file.

--- backend/src/mailer/sendTransactionReceivedEmail.ts
+++ backend/src/mailer/sendTransactionReceivedEmail.ts
@@ -27,26 +27,3 @@ export const sendTransactionReceivedEmail = (data: {
     text: transactionReceived.de.text(data),
   })
 }
-
-export const sendAddedContributionMessageEmail = (data: {
-  senderFirstName: string
-  senderLastName: string
-  recipientFirstName: string
-  recipientLastName: string
-  recipientEmail: string
-  senderEmail: string
-  contributionMemo: string
-  message: string
-  overviewURL: string
-}): Promise<boolean> => {
-  logger.info(
-    `sendEmail(): to=${data.recipientFirstName} ${data.recipientLastName} <${data.recipientEmail}>,
-    subject=${contributionMessageReceived.de.subject},
-    text=${contributionMessageReceived.de.text(data)}`,
-  )
-  return sendEMail({
-    to: `${data.recipientFirstName} ${data.recipientLastName} <${data.recipientEmail}>`,
-    subject: contributionMessageReceived.de.subject,
-    text: contributionMessageReceived.de.text(data),
-  })
-}
