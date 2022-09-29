899b74d60a4b83317fc4c1e8fbcef5810e3d1cb8
mahula <lenzmath@posteo.de>
2022-09-29 12:39:21 +0200

    wallet frontend: merge components - functionality of slot compoments IsModerator and IsNotModerator is added to component ContributionMessagesListItem - the slot components are removed, since not required anymore

--- frontend/src/components/ContributionMessages/ContributionMessagesListItem.vue
+++ frontend/src/components/ContributionMessages/ContributionMessagesListItem.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="contribution-messages-list-item">
-    <is-not-moderator v-if="isNotModerator" :message="message"></is-not-moderator>
-    <is-moderator v-else :message="message"></is-moderator>
+    <div v-if="isNotModerator" class="is-not-moderator text-right">
+      <b-avatar :text="initialLetters" variant="info"></b-avatar>
+      <span class="ml-2 mr-2">{{ message.userFirstName }} {{ message.userLastName }}</span>
+      <span class="ml-2">{{ $d(new Date(message.createdAt), 'short') }}</span>
+      <div class="mt-2">{{ message.message }}</div>
+    </div>
+    <div v-else class="is-moderator text-left">
+      <b-avatar square :text="initialLetters" variant="warning"></b-avatar>
+      <span class="ml-2 mr-2">{{ message.userFirstName }} {{ message.userLastName }}</span>
+      <span class="ml-2">{{ $d(new Date(message.createdAt), 'short') }}</span>
+      <small class="ml-4 text-success">{{ $t('community.moderator') }}</small>
+      <div class="mt-2">{{ message.message }}</div>
+    </div>
   </div>
 </template>
-<script>
-import IsModerator from '@/components/ContributionMessages/slots/IsModerator.vue'
-import IsNotModerator from '@/components/ContributionMessages/slots/IsNotModerator.vue'
 
+<script>
 export default {
   name: 'ContributionMessagesListItem',
-  components: {
-    IsModerator,
-    IsNotModerator,
-  },
   props: {
     message: {
       type: Object,
@@ -36,3 +41,19 @@ export default {
   },
 }
 </script>
+<style>
+.is-not-moderator {
+  float: right;
+  background-color: rgb(261, 204, 221);
+  width: 75%;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  clear: both;
+}
+.is-moderator {
+  clear: both;
+  background-color: rgb(255, 255, 128);
+  width: 75%;
+  margin-top: 20px;
+}
+</style>
