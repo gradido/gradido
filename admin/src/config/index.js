267a48c970d56a5c7b04588122ff59f42162a3fa
Wolfgang Huß <wolle.huss@pjannto.com>
2022-10-11 10:08:51 +0200

    Increase config version from 'v1.2022-03-18' to 'v10.2022-09-20'

--- admin/src/config/index.js
+++ admin/src/config/index.js
@@ -7,7 +7,7 @@ const pkg = require('../../package')
 const constants = {
   CONFIG_VERSION: {
     DEFAULT: 'DEFAULT',
-    EXPECTED: 'v1.2022-03-18',
+    EXPECTED: 'v10.2022-09-20',
     CURRENT: '',
   },
 }
