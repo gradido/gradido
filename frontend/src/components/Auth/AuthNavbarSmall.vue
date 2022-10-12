7702b88083865d7896e1a0e658f4b69725e554e0
mahula <lenzmath@posteo.de>
2022-10-12 16:59:51 +0200

    add link computation to auth nav bar

--- frontend/src/components/Auth/AuthNavbarSmall.vue
+++ frontend/src/components/Auth/AuthNavbarSmall.vue
@@ -2,9 +2,9 @@
   <div class="navbar-small">
     <b-navbar class="navi">
       <b-navbar-nav>
-        <b-nav-item to="/register" class="authNavbar">{{ $t('signup') }}</b-nav-item>
+        <b-nav-item :to="register" class="authNavbar">{{ $t('signup') }}</b-nav-item>
         <span class="mt-1">{{ $t('math.pipe') }}</span>
-        <b-nav-item to="/login" class="authNavbar">{{ $t('signin') }}</b-nav-item>
+        <b-nav-item :to="login" class="authNavbar">{{ $t('signin') }}</b-nav-item>
       </b-navbar-nav>
     </b-navbar>
   </div>
@@ -13,6 +13,16 @@
 <script>
 export default {
   name: 'AuthNavbarSmall',
+  computed: {
+    login() {
+      if (this.$route.params.code) return '/login/' + this.$route.params.code
+      return '/login'
+    },
+    register() {
+      if (this.$route.params.code) return '/register/' + this.$route.params.code
+      return '/register'
+    },
+  },
 }
 </script>
 <style scoped>
