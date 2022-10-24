a11352aaad21f9a352fdc77590ee47c4415cca20
Moriz Wahl <moriz.wahl@gmx.de>
2022-10-24 16:49:05 +0200

    linkify messages as component in admin interface

--- admin/src/components/ContributionMessages/slots/ContributionMessagesListItem.vue
+++ admin/src/components/ContributionMessages/slots/ContributionMessagesListItem.vue
@@ -5,32 +5,27 @@
       <span class="ml-2 mr-2">{{ message.userFirstName }} {{ message.userLastName }}</span>
       <span class="ml-2">{{ $d(new Date(message.createdAt), 'short') }}</span>
       <small class="ml-4 text-success">{{ $t('moderator') }}</small>
-      <div class="mt-2">
-        <span v-for="({ type, text }, index) in linkifiedMessage" :key="index">
-          <b-link v-if="type === 'link'" :to="text">{{ text }}</b-link>
-          <span v-else>{{ text }}</span>
-        </span>
-      </div>
+      <linkify-message :linkifiedMessage="linkifiedMessage"></linkify-message>
     </div>
     <div v-else class="text-left is-not-moderator">
       <b-avatar variant="info"></b-avatar>
       <span class="ml-2 mr-2">{{ message.userFirstName }} {{ message.userLastName }}</span>
       <span class="ml-2">{{ $d(new Date(message.createdAt), 'short') }}</span>
-      <div class="mt-2">
-        <span v-for="({ type, text }, index) in linkifiedMessage" :key="index">
-          <b-link v-if="type === 'link'" :to="text">{{ text }}</b-link>
-          <span v-else>{{ text }}</span>
-        </span>
-      </div>
+      <linkify-message :linkifiedMessage="linkifiedMessage"></linkify-message>
     </div>
   </div>
 </template>
 <script>
-const PATTERN =
+import LinkifyMessage from '@/components/ContributionMessages/LinkifyMessage.vue'
+
+const LINK_PATTERN =
   /(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*))/i
 
 export default {
   name: 'ContributionMessagesListItem',
+  components: {
+    LinkifyMessage,
+  },
   props: {
     message: {
       type: Object,
@@ -42,7 +37,7 @@ export default {
       const linkified = []
       let string = this.message.message
       let match
-      while ((match = string.match(PATTERN))) {
+      while ((match = string.match(LINK_PATTERN))) {
         if (match.index > 0)
           linkified.push({ type: 'text', text: string.substring(0, match.index) })
         linkified.push({ type: 'link', text: match[0] })
