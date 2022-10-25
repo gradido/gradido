5b9cfe5f28729ce467de00f67784aed6155a7b28
Moriz Wahl <moriz.wahl@gmx.de>
2022-10-25 02:58:09 +0200

    computed for linkified message object in linkify message component

--- frontend/src/components/ContributionMessages/LinkifyMessage.vue
+++ frontend/src/components/ContributionMessages/LinkifyMessage.vue
@@ -8,12 +8,29 @@
 </template>
 
 <script>
+const LINK_REGEX_PATTERN = /(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*))/i
+
 export default {
   name: 'LinkifyMessage',
   props: {
-    linkifiedMessage: {
-      type: Array,
+    message: {
+      type: String,
       required: true,
+    },
+  },
+  computed: {
+    linkifiedMessage() {
+      const linkified = []
+      let string = this.message
+      let match
+      while ((match = string.match(LINK_REGEX_PATTERN))) {
+        if (match.index > 0)
+          linkified.push({ type: 'text', text: string.substring(0, match.index) })
+        linkified.push({ type: 'link', text: match[0] })
+        string = string.substring(match.index + match[0].length)
+      }
+      if (string.length > 0) linkified.push({ type: 'text', text: string })
+      return linkified
     },
   },
 }
