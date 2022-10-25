da7ac58a22f3e7937864b1b52945a85487014945
Moriz Wahl <moriz.wahl@gmx.de>
2022-10-25 02:49:56 +0200

    computed for linkified message object in linkify message component

--- admin/src/components/ContributionMessages/slots/ContributionMessagesListItem.vue
+++ admin/src/components/ContributionMessages/slots/ContributionMessagesListItem.vue
@@ -5,21 +5,18 @@
       <span class="ml-2 mr-2">{{ message.userFirstName }} {{ message.userLastName }}</span>
       <span class="ml-2">{{ $d(new Date(message.createdAt), 'short') }}</span>
       <small class="ml-4 text-success">{{ $t('moderator') }}</small>
-      <linkify-message :linkifiedMessage="linkifiedMessage"></linkify-message>
+      <linkify-message :message="message.message"></linkify-message>
     </div>
     <div v-else class="text-left is-not-moderator">
       <b-avatar variant="info"></b-avatar>
       <span class="ml-2 mr-2">{{ message.userFirstName }} {{ message.userLastName }}</span>
       <span class="ml-2">{{ $d(new Date(message.createdAt), 'short') }}</span>
-      <linkify-message :linkifiedMessage="linkifiedMessage"></linkify-message>
+      <linkify-message :message="message.message"></linkify-message>
     </div>
   </div>
 </template>
 <script>
 import LinkifyMessage from '@/components/ContributionMessages/LinkifyMessage.vue'
-
-const LINK_PATTERN =
-  /(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*))/i
 
 export default {
   name: 'ContributionMessagesListItem',
@@ -30,21 +27,6 @@ export default {
     message: {
       type: Object,
       required: true,
-    },
-  },
-  computed: {
-    linkifiedMessage() {
-      const linkified = []
-      let string = this.message.message
-      let match
-      while ((match = string.match(LINK_PATTERN))) {
-        if (match.index > 0)
-          linkified.push({ type: 'text', text: string.substring(0, match.index) })
-        linkified.push({ type: 'link', text: match[0] })
-        string = string.substring(match.index + match[0].length)
-      }
-      if (string.length > 0) linkified.push({ type: 'text', text: string })
-      return linkified
     },
   },
 }
