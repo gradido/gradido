684826e3c735b139639d8c25fcb46c086aef8b5b
Claus-Peter Hübner <claus-peter.huebner@email.de>
2022-10-27 01:44:43 +0200

    add clientRequestTime as string column

---
+++ database/migrations/0052-add_clientrequesttime_to_contributions.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export async function upgrade(queryFn: (query: string, values?: any[]) => Promise<Array<any>>) {
+  await queryFn(
+    `ALTER TABLE contributions ADD COLUMN client_request_time varchar(28) NULL DEFAULT NULL AFTER contribution_date;`,
+  )
+}
+
+export async function downgrade(queryFn: (query: string, values?: any[]) => Promise<Array<any>>) {
+  await queryFn(`ALTER TABLE contributions DROP COLUMN client_request_time;`)
+}
