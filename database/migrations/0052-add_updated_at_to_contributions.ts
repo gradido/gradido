12f64a3cbd8938f74feccd1d1e5ca10e049391f1
elweyn <heine.hannes@gmail.com>
2022-10-28 11:28:19 +0200

    Add after statement to alter table.

--- database/migrations/0052-add_updated_at_to_contributions.ts
+++ database/migrations/0052-add_updated_at_to_contributions.ts
@@ -2,7 +2,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 export async function upgrade(queryFn: (query: string, values?: any[]) => Promise<Array<any>>) {
-  await queryFn(`ALTER TABLE \`contributions\` ADD COLUMN \`updated_at\` datetime DEFAULT NULL;`)
+  await queryFn(
+    `ALTER TABLE \`contributions\` ADD COLUMN \`updated_at\` datetime DEFAULT NULL AFTER \`transaction_id\`;`,
+  )
 }
 
 export async function downgrade(queryFn: (query: string, values?: any[]) => Promise<Array<any>>) {
