32bdeba5dce48e388174110f1e2c33eccc56182c
ogerly <fridolin@tutanota.com>
2022-11-02 14:02:16 +0100

    add skeleton system

--- frontend/src/pages/TopStorys.vue
+++ frontend/src/pages/TopStorys.vue
@@ -1,8 +1,28 @@
 <template>
-  <div>Top Storys</div>
+  <div>
+    <b-card v-if="skeleton">
+      {{ $t('navigation.topStories') }}
+      <b-skeleton animation="wave" width="85%"></b-skeleton>
+      <b-skeleton animation="wave" width="55%"></b-skeleton>
+      <b-skeleton animation="wave" width="70%"></b-skeleton>
+    </b-card>
+    <b-card v-else>
+      {{ $t('navigation.topStories') }}
+    </b-card>
+  </div>
 </template>
 <script>
 export default {
   name: 'TopStorys',
+  data() {
+    return {
+      skeleton: true,
+    }
+  },
+  created() {
+    setTimeout(() => {
+      this.skeleton = false
+    }, 1500)
+  },
 }
 </script>
