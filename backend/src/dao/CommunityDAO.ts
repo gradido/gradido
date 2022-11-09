70bcfb5d0a22f2db84b586a2f3ed11c1fa418217
Claus-Peter Hübner <claus-peter.huebner@email.de>
2022-11-09 01:03:37 +0100

    first DAO

--- backend/src/dao/CommunityDAO.ts
+++ backend/src/dao/CommunityDAO.ts
@@ -1,11 +1,13 @@
 import CONFIG from '@/config'
 import { Community as DbCommunity } from '@entity/Community'
 import { backendLogger as logger } from '@/server/logger'
-import { Community } from '@/graphql/model/Community'
 import { CommunityFederation as DbFederation } from '@entity/CommunityFederation'
 import { CommunityApiVersion as DbApiVersion } from '@entity/CommunityApiVersion'
+import { v4 as uuidv4 } from 'uuid'
+import { Community as FdCommunity } from '@/federation/model/Community'
+import { decryptCommunityPrivateKey, encryptCommunityPrivateKey } from '@/util/encryptionTools'
 
-export async function readHomeCommunity(): Promise<Community> {
+export async function readHomeCommunity(): Promise<FdCommunity> {
   const dbCom = await DbCommunity.findOneOrFail({ name: CONFIG.COMMUNITY_NAME }).catch(() => {
     logger.error(`Community with name=${CONFIG.COMMUNITY_NAME} does not exists`)
     throw new Error(`Community with name=${CONFIG.COMMUNITY_NAME} does not exists`)
@@ -23,26 +25,74 @@ export async function readHomeCommunity(): Promise<Community> {
     order: { validFrom: 'DESC' },
   })
 
-  const community = new Community()
+  if (!dbApi || !dbApi[0]) {
+    logger.error(
+      `HomeCommunity with malformed configuration! missing ApiVersion for federationid=${dbFed.id}`,
+    )
+    throw new Error(
+      `HomeCommunity with malformed configuration! missing ApiVersion for federationid=${dbFed.id}`,
+    )
+  }
+
+  const community = new FdCommunity(dbCom.name, dbApi[0].url, dbCom.description)
   community.id = dbCom.id
   community.name = dbCom.name
   community.description = dbCom.description
   community.uuid = dbCom.uuid
   community.createdAt = dbCom.createdAt
-  if (dbApi) {
-    community.url = dbApi[0].url
-    community.apiVersion = dbApi[0].apiVersion
-    community.validFrom = dbApi[0].validFrom
-  }
+  community.publicKey = dbFed.pubKey
+  community.privKey = decryptCommunityPrivateKey(
+    dbFed.privateKey,
+    dbCom.uuid,
+    CONFIG.LOGIN_SERVER_KEY,
+  )
+  community.url = dbApi[0].url
+  community.apiVersion = dbApi[0].apiVersion
+  community.validFrom = dbApi[0].validFrom
 
   return community
 }
 
-/*
-export async function createHomeCommunity(community: Community): Promise<Community> {
-  const dbCom = DbCommunity.create()
-  dbCom.name = community.name
-  dbCom.description = community.description
-  dbCom.
+export async function createHomeCommunity(
+  name: string,
+  url: string,
+  descript: string,
+  publicKey: Buffer,
+  privateKey: Buffer,
+): Promise<FdCommunity> {
+  // start federation with empty federation tables
+  DbCommunity.clear()
+  DbFederation.clear()
+  DbApiVersion.clear()
+
+  let dbCom = DbCommunity.create()
+  dbCom.name = name
+  dbCom.description = descript
+  dbCom.uuid = uuidv4()
+  dbCom = await dbCom.save()
+
+  let dbFed = DbFederation.create()
+  dbFed.communityId = dbCom.id
+  dbFed.foreign = false
+  dbFed.privateKey = encryptCommunityPrivateKey(privateKey, dbCom.uuid, CONFIG.LOGIN_SERVER_KEY)
+  dbFed.pubKey = publicKey.
+  dbFed = await dbFed.save()
+
+  let dbApi = DbApiVersion.create()
+  dbApi.communityFederationID = dbFed.id
+  dbApi.apiVersion = '1.0'
+  dbApi.url = url
+  dbApi.validFrom = new Date()
+  dbApi = await dbApi.save()
+
+  const community = new FdCommunity(name, url, descript)
+  community.id = dbCom.id
+  community.uuid = dbCom.uuid
+  community.createdAt = dbCom.createdAt
+  community.privKey = dbFed.privateKey
+  community.publicKey = dbFed.pubKey
+  community.apiVersion = dbApi.apiVersion
+  community.validFrom = dbApi.validFrom
+
+  return community
 }
-*/
