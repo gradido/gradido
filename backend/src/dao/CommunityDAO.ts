16d435b2e0354c9cb718e6cc121942f76f64e7ea
Claus-Peter Hübner <claus-peter.huebner@email.de>
2022-11-09 18:33:00 +0100

    new method to add new remote community

--- backend/src/dao/CommunityDAO.ts
+++ backend/src/dao/CommunityDAO.ts
@@ -1,10 +1,13 @@
 import CONFIG from '@/config'
 import { Community as DbCommunity } from '@entity/Community'
 import { backendLogger as logger } from '@/server/logger'
-import { CommunityFederation as DbFederation } from '@entity/CommunityFederation'
+import {
+  CommunityFederation,
+  CommunityFederation as DbFederation,
+} from '@entity/CommunityFederation'
 import { CommunityApiVersion as DbApiVersion } from '@entity/CommunityApiVersion'
 import { v4 as uuidv4 } from 'uuid'
-import { Community as FdCommunity } from '@/federation/model/Community'
+import { FdCommunity } from '@/federation/model/FdCommunity'
 import { decryptCommunityPrivateKey, encryptCommunityPrivateKey } from '@/util/encryptionTools'
 
 export async function readHomeCommunity(): Promise<FdCommunity> {
@@ -15,8 +18,8 @@ export async function readHomeCommunity(): Promise<FdCommunity> {
   // there is only one federation entry for the home community with foreign flag = false
   const dbFed = await DbFederation.findOneOrFail({ communityId: dbCom.id, foreign: false }).catch(
     () => {
-      logger.error(`Community with name=${CONFIG.COMMUNITY_NAME} does not exists`)
-      throw new Error(`Community with name=${CONFIG.COMMUNITY_NAME} does not exists`)
+      logger.error(`Missing CommunityFederation for Community name=${CONFIG.COMMUNITY_NAME}`)
+      throw new Error(`Missing CommunityFederation for Community name=${CONFIG.COMMUNITY_NAME}`)
     },
   )
   // read the entries with the youngest ValidFrom at first
@@ -40,12 +43,12 @@ export async function readHomeCommunity(): Promise<FdCommunity> {
   community.description = dbCom.description
   community.uuid = dbCom.uuid
   community.createdAt = dbCom.createdAt
-  community.publicKey = dbFed.pubKey
+  community.publicKey = dbFed.pubKey.toString('hex')
   community.privKey = decryptCommunityPrivateKey(
     dbFed.privateKey,
     dbCom.uuid,
     CONFIG.LOGIN_SERVER_KEY,
-  )
+  ).toString('hex')
   community.url = dbApi[0].url
   community.apiVersion = dbApi[0].apiVersion
   community.validFrom = dbApi[0].validFrom
@@ -73,9 +76,10 @@ export async function createHomeCommunity(
 
   let dbFed = DbFederation.create()
   dbFed.communityId = dbCom.id
+  dbFed.uuid = dbCom.uuid
   dbFed.foreign = false
   dbFed.privateKey = encryptCommunityPrivateKey(privateKey, dbCom.uuid, CONFIG.LOGIN_SERVER_KEY)
-  dbFed.pubKey = publicKey.
+  dbFed.pubKey = publicKey
   dbFed = await dbFed.save()
 
   let dbApi = DbApiVersion.create()
@@ -89,10 +93,46 @@ export async function createHomeCommunity(
   community.id = dbCom.id
   community.uuid = dbCom.uuid
   community.createdAt = dbCom.createdAt
-  community.privKey = dbFed.privateKey
-  community.publicKey = dbFed.pubKey
+  community.privKey = dbFed.privateKey.toString('hex')
+  community.publicKey = dbFed.pubKey.toString('hex')
   community.apiVersion = dbApi.apiVersion
   community.validFrom = dbApi.validFrom
 
   return community
+}
+
+export async function addUnknownFederationCommunity(
+  remotePublicKey: Buffer,
+  remoteApiVersion: string,
+  remoteUrl: string,
+): Promise<boolean> {
+  logger.info(`addUnknownFederationCommunity()...`)
+  const dbCom = await DbCommunity.findOneOrFail({ name: CONFIG.COMMUNITY_NAME }).catch(() => {
+    logger.error(`Community with name=${CONFIG.COMMUNITY_NAME} does not exists`)
+    throw new Error(`Community with name=${CONFIG.COMMUNITY_NAME} does not exists`)
+  })
+  await DbFederation.findOneOrFail({
+    communityId: dbCom.id,
+    foreign: true,
+    pubKey: remotePublicKey,
+  }).catch(async () => {
+    logger.info(
+      `add unknown RemoteCommunity with pubKey=${remotePublicKey}, url=${remoteUrl}, api=${remoteApiVersion}`,
+    )
+    const dbFed = DbFederation.create()
+    dbFed.communityId = dbCom.id
+    dbFed.foreign = true
+    dbFed.pubKey = remotePublicKey
+    await dbFed.save()
+
+    let dbApi = DbApiVersion.create()
+    dbApi.communityFederationID = dbFed.id
+    dbApi.apiVersion = remoteApiVersion
+    dbApi.url = remoteUrl
+    dbApi.validFrom = new Date()
+    dbApi = await dbApi.save()
+
+    return true
+  })
+  return false
 }
