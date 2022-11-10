2a91237fa7d442dc762c342089fddf419245d35a
Claus-Peter Hübner <claus-peter.huebner@email.de>
2022-11-10 01:45:07 +0100

    extend constructor and add optional attributes

--- backend/src/federation/model/FdCommunity.ts
+++ backend/src/federation/model/FdCommunity.ts
@@ -1,28 +1,44 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { CommunityFederation as DbFederation } from '@entity/CommunityFederation'
+import { CommunityApiVersion as DbApiVersion } from '@entity/CommunityApiVersion'
 import { ObjectType, Field } from 'type-graphql'
 
 @ObjectType()
 export class FdCommunity {
-  constructor(name: string, url: string, descript: string) {
+  constructor(
+    name: string,
+    url: string,
+    descript: string,
+    dbFed?: DbFederation,
+    dbApi?: DbApiVersion,
+  ) {
     this.name = name
     this.url = url
     this.description = descript
+    if (dbFed) {
+      this.publicKey = dbFed.pubKey.toString('hex')
+    }
+    if (dbApi) {
+      this.apiVersion = dbApi.apiVersion
+      this.url = dbApi.url
+      this.validFrom = dbApi.validFrom
+    }
   }
 
-  @Field(() => Number)
+  @Field(() => Number, { nullable: true })
   id: number
 
-  @Field(() => String)
+  @Field(() => String, { nullable: true })
   uuid: string
 
-  @Field(() => String)
+  @Field(() => String, { nullable: true })
   name: string
 
   @Field(() => String)
   url: string
 
-  @Field(() => String)
+  @Field(() => String, { nullable: true })
   description: string
 
   @Field(() => Date)
@@ -31,7 +47,7 @@ export class FdCommunity {
   @Field(() => String)
   publicKey: string
 
-  @Field(() => String)
+  @Field(() => String, { nullable: true })
   privKey: string
 
   @Field(() => String)
