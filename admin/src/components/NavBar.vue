bd3593551e66bf1b02931efd12d18c97948e5d5f
mahula <lenzmath@posteo.de>
2022-11-14 11:29:59 +0100

    add separate statistic page to admin area

--- admin/src/components/NavBar.vue
+++ admin/src/components/NavBar.vue
@@ -10,6 +10,7 @@
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item to="/">{{ $t('navbar.overview') }}</b-nav-item>
+          <b-nav-item to="/statistic">{{ $t('navbar.statistic') }}</b-nav-item>
           <b-nav-item to="/user">{{ $t('navbar.user_search') }}</b-nav-item>
           <b-nav-item to="/creation">{{ $t('navbar.multi_creation') }}</b-nav-item>
           <b-nav-item
