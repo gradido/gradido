b741aff5006b3173d65dd4aca98c4f4fdccd1f5b
Claus-Peter Hübner <claus-peter.huebner@email.de>
2022-11-15 22:09:05 +0100

    configs for additional instance of database and backend on same server

---
+++ separate_instance/database/src/config/index.ts
@@ -0,0 +1,40 @@
+// ATTENTION: DO NOT PUT ANY SECRETS IN HERE (or the .env)
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+const constants = {
+  CONFIG_VERSION: {
+    DEFAULT: 'DEFAULT',
+    EXPECTED: 'v1.2022-03-18',
+    CURRENT: '',
+  },
+}
+
+const database = {
+  DB_HOST: process.env.DB_HOST || 'localhost',
+  DB_PORT: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3307,
+  DB_USER: process.env.DB_USER || 'root',
+  DB_PASSWORD: process.env.DB_PASSWORD || '',
+  DB_DATABASE: process.env.DB_DATABASE || 'gradido_community',
+}
+
+const migrations = {
+  MIGRATIONS_TABLE: process.env.MIGRATIONS_TABLE || 'migrations',
+}
+
+// Check config version
+constants.CONFIG_VERSION.CURRENT = process.env.CONFIG_VERSION || constants.CONFIG_VERSION.DEFAULT
+if (
+  ![constants.CONFIG_VERSION.EXPECTED, constants.CONFIG_VERSION.DEFAULT].includes(
+    constants.CONFIG_VERSION.CURRENT,
+  )
+) {
+  throw new Error(
+    `Fatal: Config Version incorrect - expected "${constants.CONFIG_VERSION.EXPECTED}" or "${constants.CONFIG_VERSION.DEFAULT}", but found "${constants.CONFIG_VERSION.CURRENT}"`,
+  )
+}
+
+const CONFIG = { ...constants, ...database, ...migrations }
+
+export default CONFIG
