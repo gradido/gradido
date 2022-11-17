0fdb3e4401686f52128ecb07209e0c1ee83f4a2a
Wolfgang Huß <wolle.huss@pjannto.com>
2022-11-17 15:27:07 +0100

    Test 'sendAccountActivationEmail'

--- backend/src/emails/sendEmailVariants.test.ts
+++ backend/src/emails/sendEmailVariants.test.ts
@@ -1,5 +1,5 @@
 import CONFIG from '@/config'
-import { sendAccountMultiRegistrationEmail } from './sendEmailVariants'
+import { sendAccountActivationEmail, sendAccountMultiRegistrationEmail } from './sendEmailVariants'
 import { sendEmailTranslated } from './sendEmailTranslated'
 
 CONFIG.EMAIL = true
@@ -18,6 +18,75 @@ jest.mock('./sendEmailTranslated', () => {
 
 describe('sendEmailVariants', () => {
   let result: Record<string, unknown> | null
+
+  describe('sendAccountActivationEmail', () => {
+    beforeAll(async () => {
+      result = await sendAccountActivationEmail({
+        firstName: 'Peter',
+        lastName: 'Lustig',
+        email: 'peter@lustig.de',
+        language: 'en',
+        activationLink: 'http://localhost/checkEmail/6627633878930542284',
+        timeDurationObject: { hours: 24, minutes: 0 },
+      })
+    })
+
+    describe('calls "sendEmailTranslated"', () => {
+      it('with expected parameters', () => {
+        expect(sendEmailTranslated).toBeCalledWith({
+          receiver: {
+            to: 'Peter Lustig <peter@lustig.de>',
+          },
+          template: 'sendAccountActivation',
+          locals: {
+            firstName: 'Peter',
+            lastName: 'Lustig',
+            locale: 'en',
+            activationLink: 'http://localhost/checkEmail/6627633878930542284',
+            timeDurationObject: { hours: 24, minutes: 0 },
+            resendLink: CONFIG.EMAIL_LINK_FORGOTPASSWORD,
+          },
+        })
+      })
+
+      it('has expected result', () => {
+        expect(result).toMatchObject({
+          envelope: {
+            from: 'info@gradido.net',
+            to: ['peter@lustig.de'],
+          },
+          message: expect.any(String),
+          originalMessage: expect.objectContaining({
+            to: 'Peter Lustig <peter@lustig.de>',
+            from: 'Gradido (nicht antworten) <info@gradido.net>',
+            attachments: [],
+            subject: 'Gradido: Email Verification',
+            html:
+              expect.stringContaining('<title>Gradido: Email Verification</title>') &&
+              expect.stringContaining('>Gradido: Email Verification</h1>') &&
+              expect.stringContaining(
+                'Your email address has just been registered with Gradido.',
+              ) &&
+              expect.stringContaining(
+                'Please click on this link to complete the registration and activate your Gradido account:',
+              ) &&
+              expect.stringContaining(
+                '<a href="http://localhost/checkEmail/6627633878930542284">http://localhost/checkEmail/6627633878930542284</a>',
+              ) &&
+              expect.stringContaining('or copy the link above into your browser window.') &&
+              expect.stringContaining(
+                'The link has a validity of {hours} hours and {minutes} minutes. If the validity of the link has already expired, you can have a new link sent to you here by entering your email address:',
+              ) &&
+              expect.stringContaining(
+                `<a href="${CONFIG.EMAIL_LINK_FORGOTPASSWORD}">${CONFIG.EMAIL_LINK_FORGOTPASSWORD}</a>`,
+              ) &&
+              expect.stringContaining('Sincerely yours,<br><span>your Gradido team'),
+            text: expect.stringContaining('GRADIDO: EMAIL VERIFICATION'),
+          }),
+        })
+      })
+    })
+  })
 
   describe('sendAccountMultiRegistrationEmail', () => {
     beforeAll(async () => {
