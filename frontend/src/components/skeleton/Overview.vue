a44f9583c2ddb09fef36a40d50ef2b91de40b6b1
ogerly <fridolin@tutanota.com>
2022-11-17 17:00:56 +0100

    skeleton struktur, bg-image svg

---
+++ frontend/src/components/skeleton/Overview.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <b-row class="text-center">
+      <b-col>
+        <b-skeleton-img no-aspect animation="wave" height="118px"></b-skeleton-img>
+      </b-col>
+      <b-col cols="6">
+        <b-skeleton animation="wave" class="mt-4 pt-5"></b-skeleton>
+      </b-col>
+      <b-col>
+        <div class="b-right m-4">
+          <b-row>
+            <b-col><b-skeleton type="avatar"></b-skeleton></b-col>
+            <b-col>
+              <b-skeleton></b-skeleton>
+              <b-skeleton></b-skeleton>
+            </b-col>
+          </b-row>
+        </div>
+      </b-col>
+    </b-row>
+    <b-row class="text-center mt-5">
+      <b-col>
+        <b-skeleton animation="wave" width="85%"></b-skeleton>
+        <b-skeleton animation="wave" width="55%"></b-skeleton>
+        <b-skeleton animation="wave" width="70%"></b-skeleton>
+      </b-col>
+      <b-col cols="6">
+        <b-skeleton animation="wave" width="85%"></b-skeleton>
+        <b-skeleton animation="wave" width="55%"></b-skeleton>
+        <b-skeleton animation="wave" width="70%"></b-skeleton>
+        <b-skeleton animation="wave" width="85%"></b-skeleton>
+        <b-skeleton animation="wave" width="55%"></b-skeleton>
+        <b-skeleton animation="wave" width="70%"></b-skeleton>
+      </b-col>
+      <b-col>
+        <b-skeleton animation="wave" width="85%"></b-skeleton>
+        <b-skeleton animation="wave" width="55%"></b-skeleton>
+        <b-skeleton animation="wave" width="70%"></b-skeleton>
+      </b-col>
+    </b-row>
+  </div>
+</template>
