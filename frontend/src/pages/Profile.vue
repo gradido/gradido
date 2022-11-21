5ea426506cd83a050faeaa1a77d110e8bfbbd5df
ogerly <fridolin@tutanota.com>
2022-11-21 13:35:28 +0100

    repared profil

--- frontend/src/pages/Profile.vue
+++ frontend/src/pages/Profile.vue
@@ -1,36 +1,80 @@
 <template>
-  <div fluid="sm" class="mr-2">
-    <user-card :balance="balance" :transactionCount="transactionCount"></user-card>
-    <user-data />
-    <hr />
-    <user-password />
-    <hr />
-    <user-language />
-    <hr />
-    <user-newsletter />
+  <div class="profile break">
+    <b-card class="justify-content-center">
+      <b-form @submit.prevent="submit">
+        <b-row>
+          <b-col>
+            <first-name v-model="firstName" />
+          </b-col>
+          <b-col>
+            <last-name v-model="lastName" />
+          </b-col>
+        </b-row>
+        <div></div>
+        <div></div>
+
+        <b-row class="mt-5">
+          <b-col><language-switch /></b-col>
+          <b-col><job v-model="job" /></b-col>
+        </b-row>
+        <div class="h3 mt-5">Email und Passwort</div>
+        <b-row>
+          <b-col><input-email v-model="email" /></b-col>
+          <b-col><input-password /></b-col>
+        </b-row>
+        <div class="h3 mt-5">Style & News</div>
+        <b-row>
+          <b-col>Newsletter abonnieren</b-col>
+          <b-col>on / of</b-col>
+          <b-col><b-form-checkbox switch size="lg"></b-form-checkbox></b-col>
+        </b-row>
+        <b-row class="mt-3">
+          <b-col>Lightning Mode</b-col>
+          <b-col>on / of</b-col>
+          <b-col><b-form-checkbox switch size="lg"></b-form-checkbox></b-col>
+        </b-row>
+        <b-button type="submit" class="mt-5">speichern</b-button>
+      </b-form>
+    </b-card>
   </div>
 </template>
 <script>
-import UserCard from '@/components/UserCard.vue'
-import UserData from '@/components/UserSettings/UserData.vue'
-import UserPassword from '@/components/UserSettings/UserPassword.vue'
-import UserLanguage from '@/components/UserSettings/UserLanguage.vue'
-import UserNewsletter from '@/components/UserSettings/UserNewsletter.vue'
+import FirstName from '@/components/Inputs/FirstName.vue'
+import LastName from '@/components/Inputs/LastName.vue'
+import LanguageSwitch from '@/components/LanguageSwitch.vue'
+import Job from '@/components/Inputs/Job.vue'
+import InputEmail from '@/components/Inputs/InputEmail.vue'
+import InputPassword from '@/components/Inputs/InputPassword.vue'
 
 export default {
   name: 'Profile',
   components: {
-    UserCard,
-    UserData,
-    UserPassword,
-    UserLanguage,
-    UserNewsletter,
+    FirstName,
+    LastName,
+    LanguageSwitch,
+    Job,
+    InputEmail,
+    InputPassword,
   },
   props: {
     balance: { type: Number, default: 0 },
     transactionCount: { type: Number, default: 0 },
   },
+  data() {
+    return {
+      firstName: this.$store.state.firstName,
+      lastName: this.$store.state.lastName,
+      job: '',
+      email: this.$store.state.email,
+    }
+  },
   methods: {
+    submit() {
+      console.log('submit')
+      console.log(this.firstName)
+      console.log(this.lastName)
+      console.log(this.job)
+    },
     updateTransactions(pagination) {
       this.$emit('update-transactions', pagination)
     },
@@ -40,16 +84,3 @@ export default {
   },
 }
 </script>
-<style>
-.card-border-radius {
-  border-radius: 0px 5px 5px 0px !important;
-}
-@media screen and (max-width: 1235px) {
-  .card-border-radius {
-    border-radius: 0px !important;
-  }
-}
-.card-background-gray {
-  background-color: #ebebeba3 !important;
-}
-</style>
