a954ad906fb6a5f82f09b89680ec40016eef4069
ogerly <fridolin@tutanota.com>
2022-11-21 13:35:28 +0100

    repared profil

---
+++ frontend/src/components/Inputs/LastName.vue
@@ -0,0 +1,40 @@
+<template>
+  <div role="group" class="last-name">
+    <label for="input-lastName">Nachname:</label>
+    <b-form-input
+      id="input-lastName"
+      v-model="lastName"
+      :state="lastNameState"
+      aria-describedby="input-live-help input-live-feedback"
+      placeholder="Enter your lastName"
+      trim
+    ></b-form-input>
+
+    <!-- This will only be shown if the preceding input has an invalid state -->
+    <b-form-invalid-feedback id="input-live-feedback">
+      Enter at least 3 letters
+    </b-form-invalid-feedback>
+
+    <!-- This is a form text block (formerly known as help block) -->
+    <b-form-text id="input-live-help">Dein Nachname</b-form-text>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'lastName',
+  props: {
+    value: { type: String, default: '' },
+  },
+  data() {
+    return {
+      lastName: this.value,
+    }
+  },
+  computed: {
+    lastNameState() {
+      return this.lastName.length > 2
+    },
+  },
+}
+</script>
--- frontend/src/pages/Settings.vue
+++ frontend/src/pages/Settings.vue
@@ -1,28 +1,55 @@
 <template>
-  <div>
-    <b-card v-if="skeleton">
-      {{ $t('navigation.settings') }}
-      <b-skeleton animation="wave" width="85%"></b-skeleton>
-      <b-skeleton animation="wave" width="55%"></b-skeleton>
-      <b-skeleton animation="wave" width="70%"></b-skeleton>
-    </b-card>
-    <b-card v-else>
-      {{ $t('navigation.settings') }}
-    </b-card>
+  <div fluid="sm" class="mr-2">
+    <user-card :balance="balance" :transactionCount="transactionCount"></user-card>
+    <user-data />
+    <hr />
+    <user-password />
+    <hr />
+    <user-language />
+    <hr />
+    <user-newsletter />
   </div>
 </template>
 <script>
+import UserCard from '@/components/UserSettings/UserCard.vue'
+import UserData from '@/components/UserSettings/UserData.vue'
+import UserPassword from '@/components/UserSettings/UserPassword.vue'
+import UserLanguage from '@/components/UserSettings/UserLanguage.vue'
+import UserNewsletter from '@/components/UserSettings/UserNewsletter.vue'
+
 export default {
-  name: 'Settings',
-  data() {
-    return {
-      skeleton: true,
-    }
+  name: 'Profile',
+  components: {
+    UserCard,
+    UserData,
+    UserPassword,
+    UserLanguage,
+    UserNewsletter,
+  },
+  props: {
+    balance: { type: Number, default: 0 },
+    transactionCount: { type: Number, default: 0 },
+  },
+  methods: {
+    updateTransactions(pagination) {
+      this.$emit('update-transactions', pagination)
+    },
   },
   created() {
-    setTimeout(() => {
-      this.skeleton = false
-    }, 1500)
+    this.updateTransactions(0)
   },
 }
 </script>
+<style>
+.card-border-radius {
+  border-radius: 0px 5px 5px 0px !important;
+}
+@media screen and (max-width: 1235px) {
+  .card-border-radius {
+    border-radius: 0px !important;
+  }
+}
+.card-background-gray {
+  background-color: #ebebeba3 !important;
+}
+</style>
