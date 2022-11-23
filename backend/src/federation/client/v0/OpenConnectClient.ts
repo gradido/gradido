882f682ce3f63659aafcdf577e42e24eca4f76ed
Claus-Peter Hübner <claus-peter.huebner@email.de>
2022-11-23 03:43:35 +0100

    change apiversion directory and references

--- backend/src/federation/client/v0/OpenConnectClient.ts
+++ backend/src/federation/client/v0/OpenConnectClient.ts
@@ -1,14 +1,14 @@
 import { GraphQLClient, gql } from 'graphql-request'
 import { backendLogger as logger } from '@/server/logger'
 import { FdCommunity } from '@/federation/graphql/v0/model/FdCommunity'
-import { SecretKeyCryptographyEncrypt } from '@/util/encryptionTools'
+import { SecretKeyCryptographyDecrypt, SecretKeyCryptographyEncrypt } from '@/util/encryptionTools'
 
 export async function requestOpenConnect(
   homeCom: FdCommunity,
   fedCom: FdCommunity,
 ): Promise<boolean | undefined> {
   let endpoint = fedCom.url.endsWith('/') ? fedCom.url : fedCom.url + '/'
-  endpoint = `${endpoint}graphql/v${fedCom.apiVersion}_openConnect`
+  endpoint = `${endpoint}graphql/${fedCom.apiVersion}_openConnect`
   logger.info(`requestOpenConnect with endpoint='${endpoint}'...`)
 
   const graphQLClient = new GraphQLClient(endpoint, {
@@ -21,24 +21,35 @@ export async function requestOpenConnect(
   logger.info(`graphQLClient=${JSON.stringify(graphQLClient)}`)
   const query = gql`
     query ($pubKey: String!, $encryptedUrl: String!) {
-      openConnect(pubKey: $pubKey, encryptedUrl: $encryptedUrl)
+      v0_openConnect(pubKey: $pubKey, encryptedUrl: $encryptedUrl)
     }
   `
-  const encryptedUrl = SecretKeyCryptographyEncrypt(
+  const encryptedUrlBuf = SecretKeyCryptographyEncrypt(
     Buffer.from(homeCom.url),
-    Buffer.from(fedCom.publicKey),
+    Buffer.from(fedCom.publicKey, 'hex'),
   )
+  const encryptedUrl = encryptedUrlBuf.toString('hex')
   const variables = { pubKey: homeCom.publicKey, encryptedUrl: encryptedUrl }
+  logger.debug(`variables=${JSON.stringify(variables)}`)
 
   try {
-    const data = await graphQLClient.request(query, variables)
-    logger.info(`Response-Data: ${JSON.stringify(data)}`)
+    const { data, errors, extensions, headers, status } = await graphQLClient.rawRequest(
+      query,
+      variables,
+    )
+    logger.debug(
+      `Response-Data: ${JSON.stringify(
+        { data, errors, extensions, headers, status },
+        undefined,
+        2,
+      )}`,
+    )
     if (data) {
-      const json = JSON.parse(data.toString('ascii'))
-      logger.info(`Response-Data: ${json}`)
-      return json.openConnect
+      logger.debug(`Response-Data: ${data.v0_openConnect}`)
+      logger.info(`requestOpenConnect processed successfully`)
+      return data.v0_openConnect
     }
-    logger.info(`requestOpenConnect processed successfully`)
+    logger.error(`requestOpenConnect processed without response data...`)
   } catch (err) {
     logger.error(`Request-Error: ${JSON.stringify(err)}`)
   }
@@ -51,7 +62,7 @@ export async function requestOpenConnectRedirect(
   fedCom: FdCommunity,
 ): Promise<boolean | undefined> {
   let endpoint = fedCom.url.endsWith('/') ? fedCom.url : fedCom.url + '/'
-  endpoint = `${endpoint}graphql/v${fedCom.apiVersion}_openConnectRedirect`
+  endpoint = `${endpoint}graphql/${fedCom.apiVersion}_openConnectRedirect`
   logger.info(`requestOpenConnectRedirect with endpoint='${endpoint}'...`)
 
   const graphQLClient = new GraphQLClient(endpoint, {
@@ -63,18 +74,18 @@ export async function requestOpenConnectRedirect(
   })
   logger.info(`graphQLClient=${JSON.stringify(graphQLClient)}`)
   const query = gql`
-    query ($oneTimeCode: String!, $url: String!, $encryptedRedirctUrl: String!) {
-      openConnectRedirect(
+    query ($oneTimeCode: String!, $url: String!, $encryptedRedirectUrl: String!) {
+      v0_openConnectRedirect(
         oneTimeCode: $oneTimeCode
         url: $url
-        encryptedRedirctUrl: $encryptedRedirctUrl
+        encryptedRedirectUrl: $encryptedRedirectUrl
       )
     }
   `
   const encryptedUrl = SecretKeyCryptographyEncrypt(
     Buffer.from(homeComRedirectUrl),
-    Buffer.from(fedCom.publicKey),
-  )
+    Buffer.from(fedCom.publicKey, 'hex'),
+  ).toString('hex')
   const variables = {
     oneTimeCode: oneTimeCode,
     url: homeCom.url,
@@ -82,14 +93,23 @@ export async function requestOpenConnectRedirect(
   }
 
   try {
-    const data = await graphQLClient.request(query, variables)
-    logger.info(`Response-Data: ${JSON.stringify(data)}`)
+    const { data, errors, extensions, headers, status } = await graphQLClient.rawRequest(
+      query,
+      variables,
+    )
+    logger.debug(
+      `Response-Data: ${JSON.stringify(
+        { data, errors, extensions, headers, status },
+        undefined,
+        2,
+      )}`,
+    )
     if (data) {
-      const json = JSON.parse(data.toString('ascii'))
-      logger.info(`Response-Data: ${json}`)
-      return json.openConnectRedirect
+      logger.debug(`Response-Data: ${data.v0_openConnectRedirect}`)
+      logger.info(`requestOpenConnectRedirect processed successfully`)
+      return data.v0_openConnectRedirect
     }
-    logger.info(`requestOpenConnectRedirect processed successfully`)
+    logger.error(`requestOpenConnectRedirect processed without response data...`)
   } catch (err) {
     logger.error(`Request-Error: ${JSON.stringify(err)}`)
   }
@@ -100,7 +120,7 @@ export async function requestOpenConnectOneTime(
   fedCom: FdCommunity,
   oneTimeCode: string,
   decryptedRemoteUrl: string,
-): Promise<boolean | undefined> {
+): Promise<string | undefined> {
   const endpoint = decryptedRemoteUrl
   logger.info(`requestOpenConnectOneTime with endpoint='${endpoint}'...`)
 
@@ -114,25 +134,38 @@ export async function requestOpenConnectOneTime(
   logger.info(`graphQLClient=${JSON.stringify(graphQLClient)}`)
   const query = gql`
     query ($oneTimeCode: String!, $uuid: String!) {
-      openConnectOneTime(oneTimeCode: $oneTimeCode, encryptedUuid: $uuid) {
+      v0_openConnectOneTime(oneTimeCode: $oneTimeCode, encryptedUuid: $uuid) {
       encryptedUuid
     }
   `
   const encryptedUuid = SecretKeyCryptographyEncrypt(
     Buffer.from(homeCom.uuid),
-    Buffer.from(fedCom.publicKey),
-  )
+    Buffer.from(fedCom.publicKey, 'hex'),
+  ).toString('hex')
   const variables = { oneTimeCode: oneTimeCode, encryptedUuid: encryptedUuid }
 
   try {
-    const data = await graphQLClient.request(query, variables)
-    logger.info(`Response-Data: ${JSON.stringify(data)}`)
+    const { data, errors, extensions, headers, status } = await graphQLClient.rawRequest(
+      query,
+      variables,
+    )
+    logger.debug(
+      `Response-Data: ${JSON.stringify(
+        { data, errors, extensions, headers, status },
+        undefined,
+        2,
+      )}`,
+    )
     if (data) {
-      const json = JSON.parse(data.toString('ascii'))
-      logger.info(`Response-Data: ${json}`)
-      return json.encryptedUuid
+      const decryptedUuid = SecretKeyCryptographyDecrypt(
+        Buffer.from(data.v0_openConnectOneTime.encryptedUuid),
+        Buffer.from(homeCom.privKey, 'hex'),
+      ).toString('hex')
+      logger.debug(`Response-Data: uuid=${decryptedUuid}`)
+      logger.info(`requestOpenConnectOneTime processed successfully`)
+      return decryptedUuid
     }
-    logger.info(`requestOpenConnectOneTime processed successfully`)
+    logger.error(`requestOpenConnectOneTime processed without response data`)
   } catch (err) {
     logger.error(`Request-Error: ${JSON.stringify(err)}`)
   }
