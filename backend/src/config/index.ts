6dc556a7b812ba81a88b52191d91b14b4a0d8ed6
Claus-Peter Hübner <claus-peter.huebner@email.de>
2022-11-25 17:53:58 +0100

    add new config property

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -10,14 +10,14 @@ Decimal.set({
 })
 
 const constants = {
-  DB_VERSION: '0056-consistent_transactions_table',
+  DB_VERSION: '0054-add_communities_table',
   DECAY_START_TIME: new Date('2021-05-13 17:46:31-0000'), // GMT+0
   LOG4JS_CONFIG: 'log4js-config.json',
   // default log level on production should be info
   LOG_LEVEL: process.env.LOG_LEVEL || 'info',
   CONFIG_VERSION: {
     DEFAULT: 'DEFAULT',
-    EXPECTED: 'v13.2022-12-20',
+    EXPECTED: 'v13.2022-11-25',
     CURRENT: '',
   },
 }
@@ -120,6 +120,7 @@ if (
 const federation = {
   FEDERATION_DHT_TOPIC: process.env.FEDERATION_DHT_TOPIC || null,
   FEDERATION_DHT_SEED: process.env.FEDERATION_DHT_SEED || null,
+  FEDERATION_COMMUNITY_URL: process.env.FEDERATION_COMMUNITY_URL || null,
 }
 
 const CONFIG = {
