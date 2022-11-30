a18358f3b92174a84ea69168230c03972a3330f6
ogerly <fridolin@tutanota.com>
2022-11-30 16:06:02 +0100

    reslut style send per link

--- frontend/src/components/ClipboardCopy.vue
+++ frontend/src/components/ClipboardCopy.vue
@@ -1,20 +1,34 @@
 <template>
   <div class="clipboard-copy">
-    <div v-if="canCopyLink" size="lg" class="mb-3">
-      <div class="text-center h2">
-        {{ link }}
+    <div v-if="canCopyLink" size="lg" class="mb-5">
+      <div class="d-flex">
+        <div>
+          <label>Gradido Link</label>
+          <div class="pointer text-center bg-secondary gradido-border-radius p-4" @click="copyLink">
+            {{ link }}
+            <!-- <b-icon icon="clipboard-plus" class="ml-5"></b-icon> -->
+          </div>
+        </div>
+        <div class="ml-5">
+          <label>Kopieren</label>
+          <div>
+            <b-button @click="copyLinkWithText" class="p-4">
+              <b-icon icon="link45deg"></b-icon>
+            </b-button>
+          </div>
+        </div>
       </div>
 
-      <div class="text-center mt-5">
-        <b-button size="sm" text="Button" variant="gradido" @click="copyLink">
+      <!-- <div class="text-center mt-5">
+          <b-button size="sm" text="Button" variant="gradido" @click="copyLink">
           {{ $t('gdd_per_link.copy-link') }}
         </b-button>
-        <hr />
+        <hr />  
         <b-button size="sm" text="Button" variant="gradido" @click="copyLinkWithText">
           {{ $t('gdd_per_link.copy-link-with-text') }}
         </b-button>
         <hr />
-      </div>
+      </div> -->
     </div>
     <div v-else>
       <div class="alert-danger p-3">{{ $t('gdd_per_link.not-copied') }}</div>
