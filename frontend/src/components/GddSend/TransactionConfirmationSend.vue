4a349e6017e31714c40c1c3eaa0f7a278abc32be
ogerly <fridolin@tutanota.com>
2022-11-30 15:44:36 +0100

    gdd send and send per link pannel style

--- frontend/src/components/GddSend/TransactionConfirmationSend.vue
+++ frontend/src/components/GddSend/TransactionConfirmationSend.vue
@@ -1,6 +1,63 @@
 <template>
-  <div class="transaction-confirm-send bg-white appBoxShadow gradido-border-radius p-3">
-    <b-row class="confirm-box-send">
+  <div class="transaction-confirm-send">
+    <div class="bg-white appBoxShadow gradido-border-radius p-3">
+      <div class="h3 mb-4">{{ $t('form.send_check') }}</div>
+      <b-row class="mt-5">
+        <b-col cols="2">
+          <b-avatar :text="avatarText" variant="success" size="4em"></b-avatar>
+        </b-col>
+        <b-col>
+          <div class="h4">
+            {{ email }}
+          </div>
+          <div class="mt-3 h5">{{ $t('form.memo') }}</div>
+          <div>{{ memo }}</div>
+        </b-col>
+        <b-col cols="3">
+          <div class="small">{{ $t('send_gdd') }}</div>
+          <div>{{ amount | GDD }}</div>
+        </b-col>
+      </b-row>
+
+      <b-row class="mt-5 pr-3 text-color-gdd-yellow h3">
+        <b-col cols="2" class="text-right">
+          <b-icon class="text-color-gdd-yellow" icon="droplet-half"></b-icon>
+        </b-col>
+        <b-col>{{ $t('advanced-calculation') }}</b-col>
+      </b-row>
+      <b-row class="pr-3" offset="2">
+        <b-col offset="2">{{ $t('form.current_balance') }}</b-col>
+        <b-col>{{ balance | GDD }}</b-col>
+      </b-row>
+      <b-row class="pr-3">
+        <b-col offset="2">
+          <strong>{{ $t('form.your_amount') }}</strong>
+        </b-col>
+        <b-col class="borderbottom">
+          <strong>{{ (amount * -1) | GDD }}</strong>
+        </b-col>
+      </b-row>
+      <b-row class="pr-3">
+        <b-col offset="2">{{ $t('form.new_balance') }}</b-col>
+        <b-col>{{ (balance - amount) | GDD }}</b-col>
+      </b-row>
+      <b-row class="mt-5 p-5">
+        <b-col>
+          <b-button @click="$emit('on-reset')">{{ $t('back') }}</b-button>
+        </b-col>
+        <b-col class="text-right">
+          <b-button
+            variant="gradido"
+            :disabled="disabled"
+            @click="$emit('send-transaction'), (disabled = true)"
+          >
+            {{ $t('form.send_now') }}
+          </b-button>
+        </b-col>
+      </b-row>
+    </div>
+
+    <!-- <b-row class="confirm-box-send">
       <b-col>
         <div class="display-4 pb-4">{{ $t('form.send_check') }}</div>
         <b-list-group class="">
@@ -31,9 +88,9 @@
           </b-input-group>
         </b-list-group>
       </b-col>
-    </b-row>
+    </b-row> -->
 
-    <b-container class="bv-example-row mt-5 mb-5">
+    <!-- <b-container class="bv-example-row mt-5 mb-5">
       <div class="alert-heading text-left h3">{{ $t('advanced-calculation') }}</div>
       <b-row class="pr-3">
         <b-col class="text-right">{{ $t('form.current_balance') }}</b-col>
@@ -51,22 +108,7 @@
         <b-col class="text-right">{{ $t('form.new_balance') }}</b-col>
         <b-col class="text-right">{{ (balance - amount) | GDD }}</b-col>
       </b-row>
-    </b-container>
-
-    <b-row class="mt-4">
-      <b-col>
-        <b-button @click="$emit('on-reset')">{{ $t('back') }}</b-button>
-      </b-col>
-      <b-col class="text-right">
-        <b-button
-          variant="primary"
-          :disabled="disabled"
-          @click="$emit('send-transaction'), (disabled = true)"
-        >
-          {{ $t('form.send_now') }}
-        </b-button>
-      </b-col>
-    </b-row>
+    </b-container> -->
   </div>
 </template>
 <script>
