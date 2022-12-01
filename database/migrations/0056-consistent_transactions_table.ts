d6243aae9d5e5e9ddd12a91a8b5a2bc254c7deb3
Moriz Wahl <moriz.wahl@gmx.de>
2022-12-01 17:10:35 +0100

    add user contact for missing users

--- database/migrations/0056-consistent_transactions_table.ts
+++ database/migrations/0056-consistent_transactions_table.ts
@@ -19,11 +19,17 @@ export async function upgrade(queryFn: (query: string, values?: any[]) => Promis
       )
     } while (countIds[0] > 0)
 
+    const userContact = await queryFn(`
+      INSERT INTO user_contacts
+      (type, user_id, email, email_checked, created_at, deleted_at)
+      VALUES
+      ('EMAIL', ${missingUserIds[i].user_id}, 'deleted.user${missingUserIds[i].user_id}@gradido.net', 0, NOW(), NOW());`)
+
     await queryFn(`
       INSERT INTO users
-      (id, gradido_id, first_name, last_name, deleted_at, password_encryption_type, created_at, language)
+      (id, gradido_id, email_id, first_name, last_name, deleted_at, password_encryption_type, created_at, language)
       VALUES
-      (${missingUserIds[i].user_id}, '${gradidoId}', 'DELETED', 'USER', NOW(), 0, NOW(), 'de');`)
+      (${missingUserIds[i].user_id}, '${gradidoId}', ${userContact.insertId}, 'DELETED', 'USER', NOW(), 0, NOW(), 'de');`)
   }
 }
 
