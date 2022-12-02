a8571cefec5fda8370f766fbfcea2b04f948551c
mahula <lenzmath@posteo.de>
2022-12-02 09:52:46 +0100

    fix cypress config

--- e2e-tests/cypress/tests/cypress.config.ts
+++ e2e-tests/cypress/tests/cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "cypress";
 import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
 import browserify from "@badeball/cypress-cucumber-preprocessor/browserify";
 
+let resetPasswordLink: string;
+
 async function setupNodeEvents(
   on: Cypress.PluginEvents,
   config: Cypress.PluginConfigOptions
