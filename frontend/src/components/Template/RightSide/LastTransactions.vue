fab990e84820e1743d038698bbb0e941fc7b1a75
ogerly <fridolin@tutanota.com>
2022-12-07 10:48:42 +0100

    empty list groups hide in LastTransactions.vue

--- frontend/src/components/Template/RightSide/LastTransactions.vue
+++ frontend/src/components/Template/RightSide/LastTransactions.vue
@@ -6,60 +6,30 @@
         <b-icon icon="three-dots-vertical"></b-icon>
       </b-col>
     </b-row>
-    <b-list-group flush horizontal>
-      <b-list-group-item v-for="(transaction, index) in transactions" :key="transaction.id">
-        <div
+    <b-list-group class="mt-5">
+      <div v-for="(transaction, index) in transactions" :key="transaction.id">
+        <b-list-group-item
           v-if="
             index <= 7 &&
             transaction.typeId !== 'DECAY' &&
             transaction.typeId !== 'LINK_SUMMARY' &&
             transaction.typeId !== 'CREATION'
           "
-          class="d-flex align-items-center"
         >
-          <b-avatar
-            :text="transaction.linkedUser.firstName[0] + transaction.linkedUser.lastName[0]"
-            :variant="transaction.typeId === 'SEND' ? 'danger' : 'success'"
-            class="mr-3"
-          ></b-avatar>
-          <span class="mr-auto">
-            {{ transaction.linkedUser.firstName }} {{ transaction.linkedUser.lastName }}
-          </span>
-          <b-badge>{{ transaction.amount | GDD }}</b-badge>
-        </div>
-      </b-list-group-item>
-    </b-list-group>
-    <!-- <div class="mt-3 mb-3" v-for="(transaction, index) in transactions" :key="transaction.id">
-      <b-row
-        v-if="
-          index <= 7 &&
-          transaction.typeId !== 'DECAY' &&
-          transaction.typeId !== 'LINK_SUMMARY' &&
-          transaction.typeId !== 'CREATION'
-        "
-      >
-        <b-col>
-          <div class="d-flex">
+          <div class="d-flex align-items-center">
             <b-avatar
               :text="transaction.linkedUser.firstName[0] + transaction.linkedUser.lastName[0]"
-              :variant="transaction.typeId === 'SEND' ? 'success' : 'danger'"
+              :variant="transaction.typeId === 'SEND' ? 'danger' : 'success'"
               class="mr-3"
             ></b-avatar>
-            <div class="small">
-              <div class="font-weight-bold">
-                {{ transaction.linkedUser.firstName }} {{ transaction.linkedUser.lastName }}
-              </div>
-              {{ $d(new Date(transaction.balanceDate), 'short') }}
-            </div>
+            <span class="mr-auto">
+              {{ transaction.linkedUser.firstName }} {{ transaction.linkedUser.lastName }}
+            </span>
+            <b-badge>{{ transaction.amount | GDD }}</b-badge>
           </div>
-
-          <b-row class="mb-2 text-right">
-            <b-col class="small">{{ $d(new Date(transaction.balanceDate), 'short') }}</b-col>
-            <b-col class="small">{{ transaction.amount | GDD }}</b-col>
-          </b-row>
-        </b-col>
-      </b-row>
-    </div> -->
+        </b-list-group-item>
+      </div>
+    </b-list-group>
   </div>
 </template>
 <script>
