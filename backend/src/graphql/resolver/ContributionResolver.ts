01abf700306fc6e0f186b4ecce4adb2544240b2a
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2022-12-22 15:56:37 +0100

    include logger for error when creation is not successful

--- backend/src/graphql/resolver/ContributionResolver.ts
+++ backend/src/graphql/resolver/ContributionResolver.ts
@@ -644,7 +644,8 @@ export class ContributionResolver {
       })
     } catch (e) {
       await queryRunner.rollbackTransaction()
-      throw new Error(`Creation was not successful.`)
+      logger.error('Creation was not successful', e)
+      throw new Error('Creation was not successful.')
     } finally {
       await queryRunner.release()
       releaseLock()
