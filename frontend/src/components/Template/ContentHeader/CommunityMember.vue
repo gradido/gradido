8195c4437a2f2fc12873ce60a50222c1cc9faa71
ogerly <fridolin@tutanota.com>
2022-12-30 12:35:08 +0100

    mobile contribution list style

--- frontend/src/components/Template/ContentHeader/CommunityMember.vue
+++ frontend/src/components/Template/ContentHeader/CommunityMember.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="community-member">
+  <div class="community-member mt-3 mt-lg-0">
     <div class="text-center">
       <b-badge class="position-absolute mt--2 ml--5 px-3" variant="success">
         {{ $t('member') }}
@@ -9,11 +9,11 @@
       class="community-member bg-white appBoxShadow gradido-border-radius p-4 border border-success"
     >
       <b-row>
-        <b-col cols="12" md="8" lg="8">
+        <b-col cols="9">
           <div class="h4">{{ $t('community.communityMember') }}</div>
           <div>{{ CONFIG.COMMUNITY_NAME }}</div>
         </b-col>
-        <b-col cols="12" md="4" lg="4" align-self="end" class="border-left border-light">
+        <b-col cols="3" align-self="end" class="border-left border-light">
           <b-icon icon="people"></b-icon>
           {{ totalUsers }}
         </b-col>
