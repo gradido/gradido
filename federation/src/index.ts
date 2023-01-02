3a7e23b48a110bd2ca39c4cafefaca3ecb36ff6a
Claus-Peter Hübner <claus-peter.huebner@email.de>
2023-01-02 23:50:25 +0100

    remove dht-hub feature from federation module

--- federation/src/index.ts
+++ federation/src/index.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import createServer from './server/createServer'
-import { startDHT } from '@/dht_node/index'
 
 // config
 import CONFIG from './config'
@@ -10,7 +9,6 @@ async function main() {
   // eslint-disable-next-line no-console
   console.log(`FEDERATION_PORT=${CONFIG.FEDERATION_PORT}`)
   console.log(`FEDERATION_API=${CONFIG.FEDERATION_API}`)
-  console.log(`configured: FEDERATION_DHT_TOPIC=${CONFIG.FEDERATION_DHT_TOPIC}`)
   const { app } = await createServer()
   
   app.listen(CONFIG.FEDERATION_PORT, () => {
@@ -21,17 +19,6 @@ async function main() {
       console.log(`GraphIQL available at http://localhost:${CONFIG.FEDERATION_PORT}`)
     }
   })
-
-  // start DHT hyperswarm when DHT_TOPIC is set in .env
-  if (CONFIG.FEDERATION_DHT_TOPIC) {
-    // eslint-disable-next-line no-console
-    console.log(
-      `starting Federation on ${CONFIG.FEDERATION_DHT_TOPIC} ${
-        CONFIG.FEDERATION_DHT_SEED ? 'with seed...' : 'without seed...'
-      }`,
-    )
-    await startDHT(CONFIG.FEDERATION_DHT_TOPIC) // con,
-  }
 }
 
 main().catch((e) => {
