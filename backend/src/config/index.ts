ed43c19ff423ebebc248ee2e8d5b977316bd6787
Claus-Peter Hübner <claus-peter.huebner@email.de>
2023-01-04 00:40:43 +0100

    add FEDERATION_VALIDATE_COMMUNITY_TIMER to config

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -17,7 +17,7 @@ const constants = {
   LOG_LEVEL: process.env.LOG_LEVEL || 'info',
   CONFIG_VERSION: {
     DEFAULT: 'DEFAULT',
-    EXPECTED: 'v14.2022-11-22',
+    EXPECTED: 'v15.2023-01-03',
     CURRENT: '',
   },
 }
