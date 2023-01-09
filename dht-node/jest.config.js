85bc0c4a49094d4a589a3c7f0175ffda317c5244
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-01-09 11:17:58 +0100

    jest & lint (not clean yet)

---
+++ dht-node/jest.config.js
@@ -0,0 +1,27 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  verbose: true,
+  preset: 'ts-jest',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts', '!**/node_modules/**', '!src/seeds/**', '!build/**'],
+  setupFiles: ['<rootDir>/test/testSetup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/test/extensions.ts'],
+  modulePathIgnorePatterns: ['<rootDir>/build/'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '@arg/(.*)': '<rootDir>/src/graphql/arg/$1',
+    '@enum/(.*)': '<rootDir>/src/graphql/enum/$1',
+    '@model/(.*)': '<rootDir>/src/graphql/model/$1',
+    '@union/(.*)': '<rootDir>/src/graphql/union/$1',
+    '@repository/(.*)': '<rootDir>/src/typeorm/repository/$1',
+    '@test/(.*)': '<rootDir>/test/$1',
+    '@entity/(.*)':
+      process.env.NODE_ENV === 'development'
+        ? '<rootDir>/../database/entity/$1'
+        : '<rootDir>/../database/build/entity/$1',
+    '@dbTools/(.*)':
+      process.env.NODE_ENV === 'development'
+        ? '<rootDir>/../database/src/$1'
+        : '<rootDir>/../database/build/src/$1',
+  },
+}
---
+++ dht-node/test/testSetup.ts
@@ -0,0 +1,41 @@
+import CONFIG from '@/config'
+import { backendLogger as logger } from '@/server/logger'
+import { i18n } from '@/server/localization'
+
+CONFIG.EMAIL = true
+CONFIG.EMAIL_TEST_MODUS = false
+
+jest.setTimeout(1000000)
+
+jest.mock('@/server/logger', () => {
+  const originalModule = jest.requireActual('@/server/logger')
+  return {
+    __esModule: true,
+    ...originalModule,
+    backendLogger: {
+      addContext: jest.fn(),
+      trace: jest.fn(),
+      debug: jest.fn(),
+      warn: jest.fn(),
+      info: jest.fn(),
+      error: jest.fn(),
+      fatal: jest.fn(),
+    },
+  }
+})
+
+jest.mock('@/server/localization', () => {
+  const originalModule = jest.requireActual('@/server/localization')
+  return {
+    __esModule: true,
+    ...originalModule,
+    i18n: {
+      init: jest.fn(),
+      // configure: jest.fn(),
+      // __: jest.fn(),
+      // setLocale: jest.fn(),
+    },
+  }
+})
+
+export { logger, i18n }
