9547eff793258d42b9b70899c9e62220fad2f4ce
Claus-Peter Hübner <claus-peter.huebner@email.de>
2023-01-12 18:02:15 +0100

    Merge remote-tracking branch 'origin/master' into 2428-feature-federation-implement-multiple-apollo-graphql-endpoints

--- federation/src/graphql/api/schema.ts
+++ federation/src/graphql/api/schema.ts
@@ -1,9 +1,12 @@
-import path from 'path'
+import path from "path";
 // config
-import CONFIG from '../../config'
-import { federationLogger as logger } from '@/server/logger'
+import CONFIG from "../../config";
+import { federationLogger as logger } from "@/server/logger";
 
-export const getApiResolvers = () => {
-  logger.info(`getApiResolvers...${CONFIG.FEDERATION_API}`)
-  return path.join(__dirname, `./${CONFIG.FEDERATION_API}/resolver/*Resolver.{ts,js}`)
-}
+export const getApiResolvers = (): string => {
+  logger.info(`getApiResolvers...${CONFIG.FEDERATION_API}`);
+  return path.join(
+    __dirname,
+    `./${CONFIG.FEDERATION_API}/resolver/*Resolver.{ts,js}`
+  );
+};
