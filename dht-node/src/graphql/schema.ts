62722bf19318a1e885fdb99d39f3881234e9079e
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-01-12 15:36:06 +0100

    remove decimal.js, cleanups

--- dht-node/src/graphql/schema.ts
+++ dht-node/src/graphql/schema.ts
@@ -2,13 +2,9 @@ import { GraphQLSchema } from 'graphql'
 import { buildSchema } from 'type-graphql'
 import path from 'path'
 
-import DecimalScalar from './scalar/Decimal'
-import Decimal from 'decimal.js-light'
-
 const schema = async (): Promise<GraphQLSchema> => {
   return await buildSchema({
     resolvers: [path.join(__dirname, `./resolver/*Resolver.{ts,js}`)],
-    scalarsMap: [{ type: Decimal, scalar: DecimalScalar }],
   })
 }
 
