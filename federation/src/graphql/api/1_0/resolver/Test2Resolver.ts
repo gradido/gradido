e7b27a32ee47c6d3a9e92e02ba0a26e0f49a2798
elweyn <heine.hannes@gmail.com>
2023-01-25 12:29:34 +0100

    Refactor GetTestApiResult & Add second resolver to version 1_0

--- federation/src/graphql/api/1_0/resolver/Test2Resolver.ts
+++ federation/src/graphql/api/1_0/resolver/Test2Resolver.ts
@@ -1,12 +1,12 @@
-import { Field, ObjectType, Query, Resolver } from 'type-graphql'
+import { Query, Resolver } from 'type-graphql'
 import { federationLogger as logger } from '@/server/logger'
 import { GetTestApiResult } from '../../GetTestApiResult'
 
 @Resolver()
-export class TestResolver {
+export class Test2Resolver {
   @Query(() => GetTestApiResult)
-  async test(): Promise<GetTestApiResult> {
-    logger.info(`test api 1_0`)
+  async test2(): Promise<GetTestApiResult> {
+    logger.info(`test api 2 1_0`)
     return new GetTestApiResult('1_0')
   }
 }
