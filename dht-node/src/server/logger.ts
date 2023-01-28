b15d16aecca6c1bf5d83f37aac7262bc6043c348
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-01-28 05:18:06 +0100

    clean logger

--- dht-node/src/server/logger.ts
+++ dht-node/src/server/logger.ts
@@ -6,21 +6,15 @@ import { readFileSync } from 'fs'
 const options = JSON.parse(readFileSync(CONFIG.LOG4JS_CONFIG, 'utf-8'))
 
 options.categories.dht.level = CONFIG.LOG_LEVEL
-options.categories.apollo.level = CONFIG.LOG_LEVEL
 let filename: string = options.appenders.dht.filename
 options.appenders.dht.filename = filename.replace(
   'apiversion-%v',
   'dht-' + CONFIG.FEDERATION_DHT_TOPIC,
 )
-filename = options.appenders.access.filename
-filename = options.appenders.apollo.filename
 filename = options.appenders.errorFile.filename
 
 log4js.configure(options)
 
-const apolloLogger = log4js.getLogger('apollo')
 const logger = log4js.getLogger('dht')
 
-// backendLogger.addContext('user', 'unknown')
-
-export { apolloLogger, logger }
+export { logger }
