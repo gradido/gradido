540dd47eb61ea4456454bf106fd2071685ebf449
Claus-Peter Hübner <claus-peter.huebner@email.de>
2023-01-31 01:04:18 +0100

    linting

--- backend/src/federation/validateCommunities.test.ts
+++ backend/src/federation/validateCommunities.test.ts
@@ -4,7 +4,7 @@
 import { logger } from '@test/testSetup'
 import { Community as DbCommunity } from '@entity/Community'
 import { testEnvironment, cleanDB } from '@test/helpers'
-import { startValidateCommunities, validateCommunities } from './validateCommunities'
+import { validateCommunities } from './validateCommunities'
 
 let con: any
 let testEnv: any
