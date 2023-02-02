84aeec281abaa1015bb0dcf25b5310c887f6ca73
ogerly <fridolin@tutanota.com>
2023-02-02 09:29:59 +0100

    change query listAllContribution add statusFilter

--- admin/src/graphql/listAllContributions.js
+++ admin/src/graphql/listAllContributions.js
@@ -1,8 +1,18 @@
 import gql from 'graphql-tag'
 
 export const listAllContributions = gql`
-  query ($currentPage: Int = 1, $pageSize: Int = 25, $order: Order = DESC) {
-    listAllContributions(currentPage: $currentPage, pageSize: $pageSize, order: $order) {
+  query (
+    $currentPage: Int = 1
+    $pageSize: Int = 5
+    $order: Order = DESC
+    $statusFilter: [ContributionStatus!]
+  ) {
+    listAllContributions(
+      currentPage: $currentPage
+      pageSize: $pageSize
+      order: $order
+      statusFilter: $statusFilter
+    ) {
       contributionCount
       contributionList {
         id
