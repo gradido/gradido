a28a46cf08eb421aa7709b1b6f9673fba9239ad0
mahula <lenzmath@posteo.de>
2023-02-02 15:43:26 +0100

    disable video recording in e2e tests

--- e2e-tests/cypress/tests/cypress.config.ts
+++ e2e-tests/cypress/tests/cypress.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
     supportFile: "cypress/support/index.ts",
     viewportHeight: 720,
     viewportWidth: 1280,
-    video: true,
+    video: false,
     retries: {
       runMode: 2,
       openMode: 0,
