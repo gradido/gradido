46e8a55d2e69cec40c2ff1e6257506b60b26dcd7
elweyn <heine.hannes@gmail.com>
2023-02-06 09:48:20 +0100

    remove unused variable raeuber.

--- backend/src/graphql/resolver/ContributionResolver.test.ts
+++ backend/src/graphql/resolver/ContributionResolver.test.ts
@@ -95,13 +95,12 @@ afterAll(async () => {
 
 describe('ContributionResolver', () => {
   let bibi: any
-  let raueber: any
   let peter: any
 
   beforeAll(async () => {
     bibi = await userFactory(testEnv, bibiBloxberg)
     admin = peter = await userFactory(testEnv, peterLustig)
-    raueber = await userFactory(testEnv, raeuberHotzenplotz)
+    await userFactory(testEnv, raeuberHotzenplotz)
     const bibisCreation = creations.find((creation) => creation.email === 'bibi@bloxberg.de')
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     await creationFactory(testEnv, bibisCreation!)
