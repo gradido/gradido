2139592dad7a88d97112e537eb526cd7bfc671c7
Claus-Peter Hübner <claus-peter.huebner@email.de>
2023-02-06 21:56:14 +0100

    change SQL-Statement notation

--- database/migrations/0060-add_verified_at_to communities.ts
+++ database/migrations/0060-add_verified_at_to communities.ts
@@ -8,10 +8,10 @@
 
 export async function upgrade(queryFn: (query: string, values?: any[]) => Promise<Array<any>>) {
   await queryFn(
-    `ALTER TABLE communities ADD COLUMN verified_at datetime(3) DEFAULT NULL AFTER last_announced_at;`,
+    'ALTER TABLE `communities` ADD COLUMN `verified_at` datetime(3) DEFAULT NULL AFTER `last_announced_at`;',
   )
 }
 
 export async function downgrade(queryFn: (query: string, values?: any[]) => Promise<Array<any>>) {
-  await queryFn(`ALTER TABLE communities DROP COLUMN verified_at;`)
+  await queryFn('ALTER TABLE `communities` DROP COLUMN `verified_at`;')
 }
