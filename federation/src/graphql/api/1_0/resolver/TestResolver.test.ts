dcd7e0b7c323ddf78e478a5d34d12f9536858e87
Claus-Peter Hübner <claus-peter.huebner@email.de>
2023-02-08 00:38:10 +0100

    unit test, but is not working

---
+++ federation/src/graphql/api/1_0/resolver/TestResolver.test.ts
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+
+import { createTestClient } from 'apollo-server-testing'
+import createServer from '@/server/createServer'
+
+jest.mock('@/config')
+
+let query: any
+
+// to do: We need a setup for the tests that closes the connection
+let con: any
+
+beforeAll(async () => {
+  const server = await createServer()
+  con = server.con
+  query = createTestClient(server.apollo).query
+})
+
+afterAll(async () => {
+  await con.close()
+})
+
+describe('TestResolver', () => {
+  const getTestQuery = `
+    query {
+      test {
+        api
+      }
+    }
+  `
+
+  describe('getTestApi', () => {
+    it('returns 1_0', async () => {
+      await expect(query({ query: getTestQuery })).resolves.toMatchObject({
+        data: {
+          api: '1_0',
+        },
+      })
+    })
+  })
+})
