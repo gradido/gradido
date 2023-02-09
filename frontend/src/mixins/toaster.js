c7e3425f29663eba5f073a9eab3f1a3bef88d3ac
Alexander Friedland <fridolin@tutanota.com>
2023-02-09 16:38:31 +0100

    Update frontend/src/mixins/toaster.js
    Co-authored-by: Moriz Wahl <moriz.wahl@gmx.de>

--- frontend/src/mixins/toaster.js
+++ frontend/src/mixins/toaster.js
@@ -25,7 +25,6 @@ export const toasters = {
       this.toast(message, {
         title: this.$t('navigation.info'),
         variant: 'warning',
-        noAutoHide: true,
       })
     },
     toast(message, options) {
