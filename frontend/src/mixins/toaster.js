72e0e90e2ef6ecf6351f8d3b76e87d342ab9fdcb
ogerly <fridolin@tutanota.com>
2023-02-09 16:53:44 +0100

    remove code 2

--- frontend/src/mixins/toaster.js
+++ frontend/src/mixins/toaster.js
@@ -4,21 +4,18 @@ export const toasters = {
       this.toast(message, {
         title: this.$t('success'),
         variant: 'success',
-        autoHideDelay: 5000,
       })
     },
     toastError(message) {
       this.toast(message, {
         title: this.$t('error.error'),
         variant: 'danger',
-        autoHideDelay: 5000,
       })
     },
     toastInfo(message) {
       this.toast(message, {
         title: this.$t('navigation.info'),
         variant: 'warning',
-        autoHideDelay: 5000,
       })
     },
     toastInfoNoHide(message) {
@@ -32,6 +29,7 @@ export const toasters = {
       if (message.replace) message = message.replace(/^GraphQL error: /, '')
       this.$root.$bvToast.toast(message, {
         appendToast: true,
+        autoHideDelay: 5000,
         solid: true,
         toaster: 'b-toaster-top-right',
         headerClass: 'gdd-toaster-title',
