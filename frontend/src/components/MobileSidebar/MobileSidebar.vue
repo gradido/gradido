a1b926da711215a4b12a51bc66f72632e486f074
ogerly <fridolin@tutanota.com>
2023-02-23 22:01:30 +0100

    style sidebar, add icons

--- frontend/src/components/MobileSidebar/MobileSidebar.vue
+++ frontend/src/components/MobileSidebar/MobileSidebar.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <b-sidebar id="sidebar-mobile" :backdrop="true" bg-variant="transparent">
+    <b-sidebar
+      id="sidebar-mobile"
+      sidebar-class="sidebar-radius"
+      width="220px"
+      :backdrop="true"
+      bg-variant="white"
+      no-header-close
+    >
+      <b-img src="img/svg/lines.png" />
       <div class="px-3 py-2">
         <sidebar @admin="$emit('admin')" @logout="$emit('logout')" :shadow="false" />
       </div>
@@ -17,3 +25,9 @@ export default {
   },
 }
 </script>
+<style>
+.sidebar-radius {
+  border-bottom-right-radius: 26px;
+  border-top-right-radius: 26px;
+}
+</style>
