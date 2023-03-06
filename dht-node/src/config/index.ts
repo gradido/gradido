cbec3384e3f65c9635d7096bdef77078ef40c12b
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-03-06 16:33:01 +0100

    update DB_VERSIOn config requirement

--- dht-node/src/config/index.ts
+++ dht-node/src/config/index.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const constants = {
-  DB_VERSION: '0061-event_refactoring',
+  DB_VERSION: '0062-event_contribution_confirm',
   LOG4JS_CONFIG: 'log4js-config.json',
   // default log level on production should be info
   LOG_LEVEL: process.env.LOG_LEVEL || 'info',
