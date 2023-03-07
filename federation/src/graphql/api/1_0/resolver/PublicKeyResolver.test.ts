c71e293e6732ae68ca7673c6698e445f6d311d10
Claus-Peter Hübner <claus-peter.huebner@email.de>
2023-03-07 03:48:43 +0100

    next try for test

--- federation/src/graphql/api/1_0/resolver/PublicKeyResolver.test.ts
+++ federation/src/graphql/api/1_0/resolver/PublicKeyResolver.test.ts
@@ -42,13 +42,14 @@ describe('PublicKeyResolver', () => {
     })
 
     it('returns homeCommunity-publicKey', async () => {
-      await expect(query({ query: getPublicKeyQuery })).resolves.toMatchObject({
+      await expect(query({ query: getPublicKeyQuery })).resolves.toEqual(expect.objectContaining({
         data: {
           getPublicKey: {
             publicKey: 'homeCommunity-publicKey',
           },
         },
       })
+      )
     })
   })
 })
