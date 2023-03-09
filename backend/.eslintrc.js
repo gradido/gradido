e3add830c36fbe3416dbeafd7b916981ccbfabe8
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-03-09 14:28:35 +0100

    turn @typescript-eslint/prefer-regex-exec

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -26,10 +26,12 @@ module.exports = {
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'plugin:type-graphql/recommended',
       ],
-      // allow explicitly defined dangling promises
       rules: {
+        // allow explicitly defined dangling promises
         '@typescript-eslint/no-floating-promises': ['error', { ignoreVoid: true }],
         'no-void': ['error', { allowAsStatement: true }],
+        // ignore prefer-regexp-exec rule to allow string.match(regex)
+        '@typescript-eslint/prefer-regexp-exec': 'off',
       },
       parserOptions: {
         tsconfigRootDir: __dirname,
