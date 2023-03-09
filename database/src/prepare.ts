7d548968012e9a7ad6f0e855804c6042c07667f8
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-03-10 00:21:27 +0100

    refactor all main scripts

--- database/src/prepare.ts
+++ database/src/prepare.ts
@@ -1,15 +1,8 @@
-/* PREPARE SCRIPT
- *
- * This file ensures operations our migration library
- * can not take care of are done.
- * This applies to all Database Operations like
- * creating, deleting, renaming Databases.
- */
+import { createConnection } from 'mysql2/promise'
 
-import { createConnection, RowDataPacket } from 'mysql2/promise'
 import CONFIG from './config'
 
-export default async (): Promise<void> => {
+const createDatabase = async (): Promise<void> => {
   const con = await createConnection({
     host: CONFIG.DB_HOST,
     port: CONFIG.DB_PORT,
@@ -25,6 +18,8 @@ export default async (): Promise<void> => {
       DEFAULT CHARACTER SET utf8mb4
       DEFAULT COLLATE utf8mb4_unicode_ci;`)
 
+  /* LEGACY CODE
+  import { RowDataPacket } from 'mysql2/promise'
   // Check if old migration table is present, delete if needed
   const [rows] = await con.query(`SHOW TABLES FROM \`${CONFIG.DB_DATABASE}\` LIKE 'migrations';`)
   if ((<RowDataPacket>rows).length > 0) {
@@ -37,6 +32,9 @@ export default async (): Promise<void> => {
       console.log('Found and dropped old migrations table')
     }
   }
+  */
 
   await con.end()
 }
+
+export { createDatabase }
