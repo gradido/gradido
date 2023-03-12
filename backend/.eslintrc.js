193da5fd0f4d085dbc11c5a8b624f0f2382ac75e
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-03-12 02:11:54 +0100

    update config

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -17,10 +17,10 @@ module.exports = {
       },
     ],
     // jest
-    'jest/no-disabled-tests': 'off', // TODO
+    'jest/no-disabled-tests': 'error',
     'jest/no-focused-tests': 'error',
     'jest/no-identical-title': 'error',
-    'jest/prefer-to-have-length': 'warn',
+    'jest/prefer-to-have-length': 'error',
     'jest/valid-expect': 'error',
   },
   overrides: [
