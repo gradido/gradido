14c499cd3b8f7518622dfb069b9755d4dbf2f256
clauspeterhuebner <86960882+clauspeterhuebner@users.noreply.github.com>
2023-03-14 23:44:31 +0100

    Update deployment/bare_metal/start.sh
    Co-authored-by: Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>

--- deployment/bare_metal/start.sh
+++ deployment/bare_metal/start.sh
@@ -237,7 +237,6 @@ done
 
 
 # let nginx showing gradido
-cd $SCRIPT_DIR
 echo 'Configuring nginx to serve gradido again' >> $UPDATE_HTML
 ln -s /etc/nginx/sites-available/gradido.conf /etc/nginx/sites-enabled/
 rm /etc/nginx/sites-enabled/update-page.conf
