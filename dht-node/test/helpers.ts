48efa927f5d115f6346c0e2e44a9ea74beb2159a
mahula <lenzmath@posteo.de>
2023-03-16 11:31:55 +0100

    test dth node workflow without docker build dependencies

--- dht-node/test/helpers.ts
+++ dht-node/test/helpers.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 
-// TEST
+// TESTS
 
 import CONFIG from '@/config'
 import connection from '@/typeorm/connection'
