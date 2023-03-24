7149de1b93bfbe0f834ca6650c4ecbc8590651f4
elweyn <heine.hannes@gmail.com>
2023-03-24 08:12:57 +0100

    remove email from graphql mutation

--- frontend/src/graphql/mutations.js
+++ frontend/src/graphql/mutations.js
@@ -1,14 +1,14 @@
 import gql from 'graphql-tag'
 
 export const subscribeNewsletter = gql`
-  mutation($email: String!, $language: String!) {
-    subscribeNewsletter(email: $email, language: $language)
+  mutation($language: String!) {
+    subscribeNewsletter(language: $language)
   }
 `
 
 export const unsubscribeNewsletter = gql`
-  mutation($email: String!) {
-    unsubscribeNewsletter(email: $email)
+  mutation {
+    unsubscribeNewsletter
   }
 `
 
