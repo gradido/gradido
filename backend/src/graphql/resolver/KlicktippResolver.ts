4081a9eecf829d944d1edff32b7a9d202048507f
elweyn <heine.hannes@gmail.com>
2023-03-24 08:34:33 +0100

    remove Args import

--- backend/src/graphql/resolver/KlicktippResolver.ts
+++ backend/src/graphql/resolver/KlicktippResolver.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
-import { Resolver, Query, Authorized, Arg, Mutation, Args, Ctx } from 'type-graphql'
+import { Resolver, Query, Authorized, Arg, Mutation, Ctx } from 'type-graphql'
 
 import {
   getKlickTippUser,
