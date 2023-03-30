e1c6764978c010f7cd58a4de807c22c5178464c6
elweyn <heine.hannes@gmail.com>
2023-03-30 16:30:11 +0200

    Remove unused queries

--- backend/src/graphql/resolver/KlicktippResolver.ts
+++ backend/src/graphql/resolver/KlicktippResolver.ts
@@ -1,10 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-import { Resolver, Query, Authorized, Arg, Mutation, Ctx } from 'type-graphql'
+import { Resolver, Authorized, Mutation, Ctx } from 'type-graphql'
 
 import {
-  getKlickTippUser,
-  getKlicktippTagMap,
   unsubscribe,
   klicktippSignIn,
 } from '@/apis/KlicktippController'
@@ -14,18 +10,6 @@ import { Context, getUser } from '@/server/context'
 
 @Resolver()
 export class KlicktippResolver {
-  @Authorized([RIGHTS.GET_KLICKTIPP_USER])
-  @Query(() => String)
-  async getKlicktippUser(@Arg('email') email: string): Promise<string> {
-    return await getKlickTippUser(email)
-  }
-
-  @Authorized([RIGHTS.GET_KLICKTIPP_TAG_MAP])
-  @Query(() => String)
-  async getKlicktippTagMap(): Promise<string> {
-    return await getKlicktippTagMap()
-  }
-
   @Authorized([RIGHTS.UNSUBSCRIBE_NEWSLETTER])
   @Mutation(() => Boolean)
   async unsubscribeNewsletter(@Ctx() context: Context): Promise<boolean> {
