dc9efdae6713f14ae4d91d657c8babf40cc00e57
Claus-Peter Hübner <claus-peter.huebner@email.de>
2023-03-31 20:12:59 +0200

    first migration step

---
+++ database/migrations/0065-x-community-sendcoins-transactions_table.ts
@@ -0,0 +1,134 @@
+/* MIGRATION TO add users that have a transaction but do not exist */
+
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export async function upgrade(queryFn: (query: string, values?: any[]) => Promise<Array<any>>) {
+  await queryFn(
+    'ALTER TABLE `transactions` MODIFY COLUMN `previous` int(10) unsigned DEFAULT NULL NULL AFTER `id`;',
+  )
+  await queryFn(
+    'ALTER TABLE `transactions` MODIFY COLUMN `type_id` int(10) DEFAULT NULL NULL AFTER `previous`;',
+  )
+  await queryFn(
+    'ALTER TABLE `transactions` MODIFY COLUMN `transaction_link_id` int(10) unsigned DEFAULT NULL NULL AFTER `type_id`;',
+  )
+  await queryFn(
+    'ALTER TABLE `transactions` MODIFY COLUMN `amount` decimal(40,20) DEFAULT NULL NULL AFTER `transaction_link_id`;',
+  )
+  await queryFn(
+    'ALTER TABLE `transactions` MODIFY COLUMN `balance` decimal(40,20) DEFAULT NULL NULL AFTER `amount`;',
+  )
+  await queryFn(
+    'ALTER TABLE `transactions` MODIFY COLUMN `balance_date` datetime(3) DEFAULT current_timestamp(3) NOT NULL AFTER `balance`;',
+  )
+  await queryFn(
+    'ALTER TABLE `transactions` MODIFY COLUMN `decay` decimal(40,20) DEFAULT NULL NULL AFTER `balance_date`;',
+  )
+  await queryFn(
+    'ALTER TABLE `transactions` MODIFY COLUMN `decay_start` datetime(3) DEFAULT NULL NULL AFTER `decay`;',
+  )
+  await queryFn(
+    'ALTER TABLE `transactions` MODIFY COLUMN `memo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL AFTER `decay_start`;',
+  )
+  await queryFn(
+    'ALTER TABLE `transactions` MODIFY COLUMN `creation_date` datetime(3) DEFAULT NULL NULL AFTER `memo`;',
+  )
+  await queryFn(
+    'ALTER TABLE `transactions` MODIFY COLUMN `user_id` int(10) unsigned NOT NULL AFTER `creation_date`;',
+  )
+  await queryFn(
+    'ALTER TABLE `transactions` ADD COLUMN `user_gradido_id` char(36) DEFAULT NULL NULL AFTER `user_id`;',
+  )
+  await queryFn(
+    'ALTER TABLE `transactions` ADD COLUMN `user_community_uuid` char(36) DEFAULT NULL NULL AFTER `user_gradido_id`;',
+  )
+  await queryFn(
+    'ALTER TABLE `transactions` ADD COLUMN `user_name` varchar(512) COLLATE utf8mb4_unicode_ci DEFAULT NULL NULL AFTER `user_community_uuid`;',
+  )
+  await queryFn(
+    'ALTER TABLE `transactions` MODIFY COLUMN `linked_user_id` int(10) unsigned DEFAULT NULL NULL AFTER `user_name`;',
+  )
+  await queryFn(
+    'ALTER TABLE `transactions` ADD COLUMN `linked_user_gradido_id` char(36) DEFAULT NULL NULL AFTER `linked_user_id`;',
+  )
+  await queryFn(
+    'ALTER TABLE `transactions` ADD COLUMN `linked_user_community_uuid` char(36) DEFAULT NULL NULL AFTER `linked_user_gradido_id`;',
+  )
+  await queryFn(
+    'ALTER TABLE `transactions` ADD COLUMN `linked_user_name` varchar(512) COLLATE utf8mb4_unicode_ci DEFAULT NULL NULL AFTER `linked_user_community_uuid`;',
+  )
+  await queryFn(
+    'ALTER TABLE `transactions` MODIFY COLUMN `linked_transaction_id` int(10) DEFAULT NULL NULL AFTER `linked_user_name`;',
+  )
+
+  await queryFn(`
+      CREATE TABLE IF NOT EXISTS \`pending_transactions\` (
+        \`id\` int(10) unsigned NOT NULL AUTO_INCREMENT,
+        \`previous\` int(10) unsigned DEFAULT NULL NULL,
+        \`type_id\` int(10) DEFAULT NULL NULL,
+        \`transaction_link_id\` int(10) unsigned DEFAULT NULL NULL,
+        \`amount\` decimal(40,20) DEFAULT NULL NULL,
+        \`balance\` decimal(40,20) DEFAULT NULL NULL,
+        \`balance_date\` datetime(3) DEFAULT current_timestamp(3) NOT NULL,
+        \`decay\` decimal(40,20) DEFAULT NULL NULL,
+        \`decay_start\` datetime(3) DEFAULT NULL NULL,
+        \`memo\` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+        \`creation_date\` datetime(3) DEFAULT NULL NULL,
+        \`user_id\` int(10) unsigned NOT NULL,
+        \`user_gradido_id\` char(36) NOT NULL,
+        \`user_community_uuid\` char(36) NOT NULL,
+        \`user_name\` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
+        \`linked_user_id\` int(10) unsigned DEFAULT NULL NULL,
+        \`linked_user_gradido_id\` char(36) NOT NULL,
+        \`linked_user_community_uuid\` char(36) NOT NULL,
+        \`linked_user_name\` varchar(512) NULL,
+        \`linked_transaction_id\` int(10) DEFAULT NULL NULL,
+        \`x_transaction_state\` varchar(100) NOT NULL COMMENT 'States to handle 2-Phase-Commit handshake',
+        \`created_at\` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+        \`updated_at\` datetime(3) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(3),
+        \`deleted_at\` datetime(3) NULL DEFAULT NULL,
+        PRIMARY KEY (\`id\`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;`)
+
+  /*
+  const userIds = await queryFn(`
+    SELECT user_id FROM transactions
+    WHERE NOT EXISTS (SELECT id FROM users WHERE id = user_id) GROUP BY user_id;`)
+
+  for (let i = 0; i < missingUserIds.length; i++) {
+    let gradidoId = ''
+    let countIds: any[] = []
+    do {
+      gradidoId = uuidv4()
+      countIds = await queryFn(
+        `SELECT COUNT(*) FROM \`users\` WHERE \`gradido_id\` = "${gradidoId}"`,
+      )
+    } while (countIds[0] > 0)
+
+    const userContact = (await queryFn(`
+      INSERT INTO user_contacts
+      (type, user_id, email, email_checked, created_at, deleted_at)
+      VALUES
+      ('EMAIL', ${missingUserIds[i].user_id}, 'deleted.user${missingUserIds[i].user_id}@gradido.net', 0, NOW(), NOW());`)) as unknown as OkPacket
+
+    await queryFn(`
+      INSERT INTO users
+      (id, gradido_id, email_id, first_name, last_name, deleted_at, password_encryption_type, created_at, language)
+      VALUES
+      (${missingUserIds[i].user_id}, '${gradidoId}', ${userContact.insertId}, 'DELETED', 'USER', NOW(), 0, NOW(), 'de');`)
+  }
+  */
+}
+
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+export async function downgrade(queryFn: (query: string, values?: any[]) => Promise<Array<any>>) {
+  await queryFn('ALTER TABLE `transactions` DROP COLUMN `user_gradido_id`;')
+  await queryFn('ALTER TABLE `transactions` DROP COLUMN `user_community_uuid`;')
+  await queryFn('ALTER TABLE `transactions` DROP COLUMN `user_name`;')
+  await queryFn('ALTER TABLE `transactions` DROP COLUMN `linked_user_gradido_id`;')
+  await queryFn('ALTER TABLE `transactions` DROP COLUMN `linked_user_community_uuid`;')
+  await queryFn('ALTER TABLE `transactions` DROP COLUMN `linked_user_name`;')
+  await queryFn(`DROP TABLE IF EXISTS pending_transactions;`)
+}
