30b7501a6e217605217050647e672e53b6fb9919
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-04-04 15:31:47 +0200

    log stack trace

--- backend/src/server/LogError.ts
+++ backend/src/server/LogError.ts
@@ -1,9 +1,11 @@
+import { LockNotSupportedOnGivenDriverError } from '@dbTools/typeorm'
 import { backendLogger as logger } from './logger'
 
 export default class LogError extends Error {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   constructor(msg: string, ...details: any[]) {
     super(msg)
-    logger.error(this.stack, ...details)
+    // logger.setStack(this.stack)
+    logger.error(msg, ...details)
   }
 }
