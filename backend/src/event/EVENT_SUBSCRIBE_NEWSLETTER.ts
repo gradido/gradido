1505d9abf06163e3d3535c481148d9910e765bef
elweyn <heine.hannes@gmail.com>
2023-04-05 10:43:42 +0200

    Change ACTION_NEWSLETTER to NEWSLETTER_ACTION

--- backend/src/event/EVENT_SUBSCRIBE_NEWSLETTER.ts
+++
@@ -1,7 +0,0 @@
-import { Event as DbEvent } from '@entity/Event'
-import { User as DbUser } from '@entity/User'
-
-import { Event, EventType } from './Event'
-
-export const EVENT_SUBSCRIBE_NEWSLETTER = async (user: DbUser): Promise<DbEvent> =>
-  Event(EventType.SUBSCRIBE_NEWSLETTER, user, user).save()
