b5dba0789ca504863bc107b10c8976f40502f9c5
Moriz Wahl <moriz.wahl@gmx.de>
2023-04-21 20:04:09 +0200

    feat(frontend): calculator on overview page

--- frontend/src/components/Overview/CommunityNews.vue
+++ frontend/src/components/Overview/CommunityNews.vue
@@ -1,5 +1,29 @@
+<!-- eslint-disable @intlify/vue-i18n/no-raw-text -->
 <template>
   <div class="community-news">
+    <b-card class="bg-white appBoxShadow gradido-border-radius">
+      <h1>Gradido zu DankBar Umrechner</h1>
+      <p>
+        Tag des Jahres:
+        <span id="tag">{{ tag }}</span>
+      </p>
+      <p>
+        Faktor:
+        <span id="faktor">{{ faktor }}</span>
+      </p>
+      <form>
+        <label for="gradido">Gradido:</label>
+        <input type="number" id="gradido" name="gradido" v-model="gradido" @input="inputGradido" />
+        <br />
+        <br />
+        <label for="dankbar">DankBar:</label>
+        <input type="number" id="dankbar" name="dankbar" v-model="dankbar" @input="inputDankbar" />
+        <br />
+        <br />
+        <input type="button" value="Berechnen" @click="umrechnen()" />
+      </form>
+      <p id="ergebnis">{{ ergebnis }}</p>
+      <!--
     <div v-for="item in News" :key="item.locale">
       <b-card
         v-if="item.locale === $i18n.locale"
@@ -28,16 +52,52 @@
         </b-card-body>
       </b-card>
     </div>
+    --></b-card>
   </div>
 </template>
 <script>
 import News from '@/assets/News/news.json'
+
+// Berechne den heutigen Tag des Jahres
+const jetzt = new Date()
+const start = new Date(jetzt.getFullYear(), 0, 0)
+const diff = jetzt - start + (start.getTimezoneOffset() - jetzt.getTimezoneOffset()) * 60 * 1000
+const tag = Math.floor(diff / (1000 * 60 * 60 * 24))
+
+// Berechne Faktor1 und Faktor2
+const faktor1 = Math.pow(0.998098, tag)
+const faktor2 = 1 / faktor1
+
 export default {
   name: 'CommunityNews',
   data() {
     return {
       News,
+      tag,
+      faktor: faktor2.toFixed(2),
+      gradido: '',
+      dankbar: '',
+      ergebnis: '',
     }
+  },
+  methods: {
+    umrechnen() {
+      if (this.gradido) {
+        const ergebnis = this.gradido * faktor2
+        this.dankbar = ergebnis.toFixed(2)
+      } else if (this.dankbar) {
+        const ergebnis = this.dankbar * faktor1
+        this.gradido = ergebnis.toFixed(2)
+      } else {
+        this.ergebnis = 'Bitte geben Sie entweder Gradido oder DankBar ein'
+      }
+    },
+    inputGradido(value) {
+      if (this.dankbar) this.dankbar = ''
+    },
+    inputDankbar(value) {
+      if (this.gradido) this.gradido = ''
+    },
   },
 }
 </script>
