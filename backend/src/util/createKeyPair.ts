234ff3b0a5f9cea01f47319c0afad4f957c8941f
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-05-05 09:50:58 +0200

    createKeyPair from seed util

---
+++ backend/src/util/createKeyPair.ts
@@ -0,0 +1,15 @@
+/* eslint-disable camelcase */
+import {
+  crypto_sign_seed_keypair,
+  crypto_sign_PUBLICKEYBYTES,
+  crypto_sign_SECRETKEYBYTES,
+  crypto_sign_SEEDBYTES,
+} from 'sodium-native'
+
+export const createKeyPair = (seed: string) => {
+  const publicKey = Buffer.allocUnsafe(crypto_sign_PUBLICKEYBYTES)
+  const secretKey = Buffer.allocUnsafe(crypto_sign_SECRETKEYBYTES)
+  const seedBuffer = Buffer.alloc(crypto_sign_SEEDBYTES, seed)
+  crypto_sign_seed_keypair(publicKey, secretKey, seedBuffer)
+  return { publicKey, secretKey }
+}
