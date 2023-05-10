c78691c87c3e9ea2df0ee145993c70485b5948b1
Moriz Wahl <moriz.wahl@gmx.de>
2023-05-10 18:00:29 +0200

    ignore unsage regex lint

--- backend/src/graphql/resolver/util/validateAlias.ts
+++ backend/src/graphql/resolver/util/validateAlias.ts
@@ -24,6 +24,7 @@ const reservedAlias = [
 export const validateAlias = async (alias: string): Promise<void> => {
   if (alias.length < 3) throw new LogError('Given alias is too short', alias)
   if (alias.length > 20) throw new LogError('Given alias is too long', alias)
+  /* eslint-disable-next-line security/detect-unsafe-regex */
   if (!alias.match(/^[0-9A-Za-z]([_-]?[A-Za-z0-9])+$/))
     throw new LogError('Invalid characters in alias', alias)
   if (reservedAlias.includes(alias.toLowerCase())) throw new LogError('Alias is not allowed', alias)
