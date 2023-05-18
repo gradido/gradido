cf941518ed88283a16e012bc34599bdc4e7f8e4b
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-05-18 11:56:50 +0200

    lint fixes

--- federation/src/graphql/api/1_1/resolver/PublicKeyResolver.ts
+++ federation/src/graphql/api/1_1/resolver/PublicKeyResolver.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Resolver } from 'type-graphql'
 // eslint-disable-next-line camelcase
 import { PublicKeyResolver as PublicKeyResolver_1_0 } from '../../1_0/resolver/PublicKeyResolver'
