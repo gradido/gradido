15ca48a3321f53b9a5fecab93ac7e5da05af7221
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-05-18 11:45:51 +0200

    implement PublicKeyResolver for 1.1 using inheritance

---
+++ federation/src/graphql/api/1_1/resolver/PublicKeyResolver.ts
@@ -0,0 +1,7 @@
+import { Resolver } from 'type-graphql'
+// eslint-disable-next-line camelcase
+import { PublicKeyResolver as PublicKeyResolver_1_0 } from '../../1_0/resolver/PublicKeyResolver'
+
+@Resolver()
+// eslint-disable-next-line camelcase, @typescript-eslint/no-unused-vars
+export class PublicKeyResolver extends PublicKeyResolver_1_0 {}
