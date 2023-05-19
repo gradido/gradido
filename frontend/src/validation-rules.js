a9c35a4c125b111032fb02ed8e83f2c1faefcbaa
Moriz Wahl <moriz.wahl@gmx.de>
2023-05-19 09:00:46 +0200

    Update frontend/src/validation-rules.js
    Co-authored-by: Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>

--- frontend/src/validation-rules.js
+++ frontend/src/validation-rules.js
@@ -8,7 +8,7 @@ import { validate as validateUuid, version as versionUuid } from 'uuid'
 // eslint-disable-next-line no-useless-escape
 const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-const usernameRegex = /^(?=.{3,20}$)[a-zA-Z0-9]+(?:[_-][a-zA-Z0-9]+?)*$/
+const USERNAME_REGEX = /^(?=.{3,20}$)[a-zA-Z0-9]+(?:[_-][a-zA-Z0-9]+?)*$/
 
 export const loadAllRules = (i18nCallback, apollo) => {
   configure({
