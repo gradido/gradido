feedab3cebde52cdc6fc18ba744405fa58cdb13f
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-05-30 10:47:20 +0200

    missing new line

--- database/migrations/0067-private_key_in_community_table.ts
+++ database/migrations/0067-private_key_in_community_table.ts
@@ -5,6 +5,7 @@
 
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+
 export async function upgrade(queryFn: (query: string, values?: any[]) => Promise<Array<any>>) {
   await queryFn(
     'ALTER TABLE `communities` ADD COLUMN `private_key` binary(64) DEFAULT NULL AFTER `public_key`;',
