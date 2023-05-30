ad1225998fc712f33a5c50afdb0466337790f39d
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-05-30 10:59:47 +0200

    created migration to correct public key length

---
+++ database/migrations/0067-community_table_public_key_length.ts
@@ -0,0 +1,23 @@
+/* MIGRATION TO CORRECT THE PUBLIC KEY LENGTHS
+ *
+ * This migration corrects the length of the saved public keys to 32 as this is the length it is generated for.
+ */
+
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export async function upgrade(queryFn: (query: string, values?: any[]) => Promise<Array<any>>) {
+  await queryFn('ALTER TABLE `communities` MODIFY COLUMN `public_key` binary(32) NOT NULL;')
+  // TODO: it is unclear if this is actually nullable - the model defines "default: null, nullable: true", but the table seems to be created without nullability(?)
+  await queryFn(
+    'ALTER TABLE `federated_communities` MODIFY COLUMN `public_key` binary(32) NULL DEFAULT NULL;',
+  )
+}
+
+export async function downgrade(queryFn: (query: string, values?: any[]) => Promise<Array<any>>) {
+  await queryFn('ALTER TABLE `communities` MODIFY COLUMN `public_key` binary(64) NOT NULL;')
+  // TODO: see above
+  await queryFn(
+    'ALTER TABLE `federated_communities` MODIFY COLUMN `public_key` binary(64) NULL DEFAULT NULL;',
+  )
+}
