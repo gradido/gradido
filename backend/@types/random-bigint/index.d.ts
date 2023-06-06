18a4829ea67419b6e5eb1801698fc5f53045ae72
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-06-06 13:02:21 +0200

    try with default export

--- backend/@types/random-bigint/index.d.ts
+++ backend/@types/random-bigint/index.d.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-types */
 declare module 'random-bigint' {
   export function random(bits: number, cb?: (err: Error, num: BigInt) => void): BigInt
-  export = random
+  // eslint-disable-next-line import/no-default-export
+  export default random
 }
