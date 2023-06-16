1fa38fe5d5ef1c20e006e904cba34124eb985c38
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-06-16 12:30:09 +0200

    generate token and send it in the header

--- backend/src/federation/client/1_0/FederationClient.ts
+++ backend/src/federation/client/1_0/FederationClient.ts
@@ -1,8 +1,12 @@
+import { Community } from '@entity/Community'
 import { FederatedCommunity as DbFederatedCommunity } from '@entity/FederatedCommunity'
 import { GraphQLClient } from 'graphql-request'
+import jose from 'jose'
 
 import { getPublicKey } from '@/federation/client/1_0/query/getPublicKey'
 import { backendLogger as logger } from '@/server/logger'
+
+import { randombytes_random } from 'sodium-native'
 
 // eslint-disable-next-line camelcase
 export class FederationClient {
@@ -24,11 +28,49 @@ export class FederationClient {
     })
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private async request(query: string, variables?: any) {
+    const nonce = randombytes_random()
+    const ownCommunity = await Community.findOneOrFail({ foreign: false })
+    if (!ownCommunity.privateKey) {
+      throw new Error('Own private key not in database.')
+    }
+    const token = await this.generateToken(
+      nonce,
+      { publicKey: ownCommunity.publicKey, privateKey: ownCommunity.privateKey },
+      this.dbCom.publicKey,
+    )
+    const response = this.client.rawRequest(query, variables, {
+      authorization: `Bearer ${token.toString()}`,
+    })
+    return response
+  }
+
+  private async generateToken(
+    nonce: number,
+    keyPair: { publicKey: Buffer; privateKey: Buffer },
+    recieverPublicKey: Buffer,
+  ) {
+    const jws = await new jose.CompactSign(new TextEncoder().encode(nonce.toString()))
+      .setProtectedHeader({ alg: 'ES256' })
+      .sign(keyPair.privateKey)
+    return await new jose.CompactEncrypt(
+      new TextEncoder().encode(
+        JSON.stringify({
+          jws,
+          publicKey: keyPair.publicKey,
+        }),
+      ),
+    )
+      .setProtectedHeader({ alg: 'RSA-OAEP-256', enc: 'A256GCM' })
+      .encrypt(recieverPublicKey)
+  }
+
   getPublicKey = async (): Promise<string | undefined> => {
     logger.info('Federation: getPublicKey from endpoint', this.endpoint)
     try {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      const { data } = await this.client.rawRequest(getPublicKey, {})
+      const { data } = await this.request(getPublicKey, {})
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       if (!data?.getPublicKey?.publicKey) {
         logger.warn('Federation: getPublicKey without response data from endpoint', this.endpoint)
