6fe0083ca154eb9c7a8b951fdea85561843d439c
ogerly <fridolin@tutanota.com>
2023-06-16 14:43:59 +0200

    add @iota/crypto.js lib, add created iota adress, pub and sec key

--- frontend/src/pages/Overview.vue
+++ frontend/src/pages/Overview.vue
@@ -65,19 +65,60 @@
         </b-col>
       </b-row>
     </div>
+    <div>
+      <hr />
+      <div class="h4">blake2b:</div>
+      <b-row>
+        <b-col>
+          <div class="word-break">{{ blake2b }}</div>
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+    </div>
+    <div>
+      <hr />
+      <div class="h3">Address Generator</div>
+      <button @click="generateAddresses">Generate Addresses</button>
+      <div v-for="address in addresses" :key="address.index">
+        <h2>Wallet Index {{ address.index }}</h2>
+        <p>
+          <strong>Private Key:</strong>
+          {{ address.privateKey }}
+        </p>
+        <p>
+          <strong>Public Key:</strong>
+          {{ address.publicKey }}
+        </p>
+        <p>
+          <strong>Address Ed25519:</strong>
+          {{ address.addressEd25519 }}
+        </p>
+        <p>
+          <strong>Address Bech32:</strong>
+          {{ address.addressBech32 }}
+        </p>
+        <hr />
+      </div>
+    </div>
   </div>
 </template>
+
 <script>
 import CommunityNews from '@/components/Overview/CommunityNews'
-import { SingleNodeClient } from '@iota/iota.js'
 import { Converter } from '@iota/util.js'
 
+import { Bip32Path, Bip39, Blake2b } from '@iota/crypto.js'
+import {
+  Bech32Helper,
+  Ed25519Address,
+  Ed25519Seed,
+  ED25519_ADDRESS_TYPE,
+  generateBip44Address,
+  // IOTA_BIP44_BASE_PATH,
+  SingleNodeClient,
+} from '@iota/iota.js'
 
-const bytes = Converter.utf8ToBytes('Hello World')
-
-const hex = Converter.bytesToHex(bytes)
-
-alert(hex)
+const API_ENDPOINT = 'https://chrysalis-nodes.iota.org/'
 
 export default {
   name: 'Overview',
@@ -89,43 +130,68 @@ export default {
       text: '',
       nodeInfo: null,
       nodeHealth: null,
+      addresses: [],
     }
   },
   computed: {
     bytesToHex() {
-      const bytes = Converter.utf8ToBytes(this.text)
+      return Converter.bytesToHex(Converter.utf8ToBytes(this.text))
+    },
 
-      return Converter.bytesToHex(bytes)
+    blake2b() {
+      const sum = Blake2b.sum512(Converter.utf8ToBytes(this.text))
+
+      return this.text === null || this.text === '' ? '' : Converter.bytesToHex(sum)
     },
   },
   methods: {
     async getNodeInfo() {
-      const client = new SingleNodeClient('https://chrysalis-nodes.iota.org')
+      const client = new SingleNodeClient(API_ENDPOINT)
 
       try {
         const info = await client.info()
         this.nodeInfo = info
-
-        const health = await client.health()
-        console.log('Is the node healthy', health ? 'Yes' : 'No')
-        console.log()
-
-        // const output = await client.output('4752414449444f3a205465737448656c6c6f57656c7431')
-        // console.log('Output')
-        // console.log('\tMessage Id:', output.messageId)
-        // console.log('\tTransaction Id:', output.transactionId)
-        // console.log('\tOutput Index:', output.outputIndex)
-        // console.log('\tIs Spent:', output.isSpent)
-        // logOutput('\t', output.output)
-        // console.log()
       } catch (err) {
-        // console.error(err)
         this.nodeInfo = err
       }
     },
+    async generateAddresses() {
+      const client = new SingleNodeClient(API_ENDPOINT)
+      const info = await client.info()
+      const randomMnemonic = Bip39.randomMnemonic()
+      const baseSeed = Ed25519Seed.fromMnemonic(randomMnemonic)
+      const addressGeneratorAccountState = {
+        accountIndex: 0,
+        addressIndex: 0,
+        isInternal: false,
+      }
+
+      const generatedAddresses = []
+      for (let i = 0; i < 6; i++) {
+        const path = generateBip44Address(addressGeneratorAccountState)
+        const addressSeed = baseSeed.generateSeedFromPath(new Bip32Path(path))
+        const addressKeyPair = addressSeed.keyPair()
+        const indexEd25519Address = new Ed25519Address(addressKeyPair.publicKey)
+        const indexPublicKeyAddress = indexEd25519Address.toAddress()
+
+        generatedAddresses.push({
+          index: path,
+          privateKey: Converter.bytesToHex(addressKeyPair.privateKey),
+          publicKey: Converter.bytesToHex(addressKeyPair.publicKey),
+          addressEd25519: Converter.bytesToHex(indexPublicKeyAddress),
+          addressBech32: Bech32Helper.toBech32(
+            ED25519_ADDRESS_TYPE,
+            indexPublicKeyAddress,
+            info.bech32HRP,
+          ),
+        })
+      }
+
+      this.addresses = generatedAddresses
+    },
   },
   created() {
-    // this.getNodeInfo()
+    this.getNodeInfo()
   },
 }
 </script>
