58d89d3f3a63f9a6f67cee857dc65cbfa3ad49c4
ogerly <fridolin@tutanota.com>
2023-06-16 11:50:05 +0200

    add @iota/util.js lib, add byteToHex example

--- frontend/src/pages/Overview.vue
+++ frontend/src/pages/Overview.vue
@@ -45,11 +45,39 @@
         </p>
       </div>
     </div>
+    <div>
+      <hr />
+      <div class="h4">bytesToHex:</div>
+      <b-row>
+        <b-col>
+          <div>{{ bytesToHex }}</div>
+        </b-col>
+        <b-col>
+          <b-form inline>
+            <label class="sr-only" for="inline-form-input-text">Text</label>
+            <b-form-input
+              v-model="text"
+              id="inline-form-input-text"
+              class="mb-2 mr-sm-2 mb-sm-0"
+              placeholder="Jane Doe"
+            ></b-form-input>
+          </b-form>
+        </b-col>
+      </b-row>
+    </div>
   </div>
 </template>
 <script>
 import CommunityNews from '@/components/Overview/CommunityNews'
 import { SingleNodeClient } from '@iota/iota.js'
+import { Converter } from '@iota/util.js'
+
+
+const bytes = Converter.utf8ToBytes('Hello World')
+
+const hex = Converter.bytesToHex(bytes)
+
+alert(hex)
 
 export default {
   name: 'Overview',
@@ -58,8 +86,17 @@ export default {
   },
   data() {
     return {
+      text: '',
       nodeInfo: null,
+      nodeHealth: null,
     }
+  },
+  computed: {
+    bytesToHex() {
+      const bytes = Converter.utf8ToBytes(this.text)
+
+      return Converter.bytesToHex(bytes)
+    },
   },
   methods: {
     async getNodeInfo() {
@@ -68,8 +105,22 @@ export default {
       try {
         const info = await client.info()
         this.nodeInfo = info
+
+        const health = await client.health()
+        console.log('Is the node healthy', health ? 'Yes' : 'No')
+        console.log()
+
+        // const output = await client.output('4752414449444f3a205465737448656c6c6f57656c7431')
+        // console.log('Output')
+        // console.log('\tMessage Id:', output.messageId)
+        // console.log('\tTransaction Id:', output.transactionId)
+        // console.log('\tOutput Index:', output.outputIndex)
+        // console.log('\tIs Spent:', output.isSpent)
+        // logOutput('\t', output.output)
+        // console.log()
       } catch (err) {
         // console.error(err)
+        this.nodeInfo = err
       }
     },
   },
