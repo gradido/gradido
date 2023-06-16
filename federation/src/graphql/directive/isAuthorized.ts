d263702849939f4d336f41d95f9c299cb1bd5ea1
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-06-16 14:01:55 +0200

    isAuthorized middleware

---
+++ federation/src/graphql/directive/isAuthorized.ts
@@ -0,0 +1,70 @@
+import { Community as dbCommunity } from '@entity/Community'
+import { AuthChecker } from 'type-graphql'
+
+import { INALIENABLE_RIGHTS } from '@/auth/INALIENABLE_RIGHTS'
+import { verifyToken, generateToken } from '@/auth/JWE'
+import { RIGHTS } from '@/auth/RIGHTS'
+import { ROLE_UNAUTHORIZED, ROLE_AUTHORIZED } from '@/auth/ROLES'
+import { Context } from '@/server/context'
+import { LogError } from '@/server/LogError'
+
+export const isAuthorized: AuthChecker<Context> = async ({ context }, rights) => {
+  // Do we have a token?
+  if (!context.token) {
+    throw new LogError('401 Unauthorized')
+  }
+
+  try {
+    const ownCommunity = await dbCommunity.findOneOrFail({ foreign: false })
+    if (!ownCommunity.privateKey) {
+      throw new LogError('Internal Server Error', 'Own private key not in database')
+    }
+    // Decode the token
+    const decoded = await verifyToken(context.token, {
+      publicKey: ownCommunity.publicKey,
+      privateKey: ownCommunity.privateKey,
+    })
+    if (!decoded) {
+      throw new LogError('403.13 - Client certificate revoked')
+    }
+
+    context.role = ROLE_UNAUTHORIZED // unauthorized caller
+    // is rights an inalienable right?
+    if (
+      (rights as RIGHTS[]).reduce((acc, right) => acc && INALIENABLE_RIGHTS.includes(right), true)
+    )
+      return true
+
+    // TODO - load from database dynamically & admin - maybe encode this in the token to prevent many database requests
+    // TODO this implementation is bullshit - two database queries cause our user identifiers are not aligned and vary between email, id and pubKey
+
+    const community = await dbCommunity.findOneOrFail({
+      where: { publicKey: decoded.publicKey },
+    })
+    context.community = community
+    context.role = ROLE_AUTHORIZED // TODO here authorization has to be checked
+
+    // check for correct rights
+    const missingRights = (rights as RIGHTS[]).filter((right) => !context.role?.hasRight(right))
+    if (missingRights.length !== 0) {
+      throw new LogError('401 Unauthorized')
+    }
+
+    // set new header token
+    context.setHeaders.push({
+      key: 'token',
+      value: await generateToken(
+        decoded.nonce,
+        {
+          publicKey: ownCommunity.publicKey,
+          privateKey: ownCommunity.privateKey,
+        },
+        community.publicKey,
+      )(decoded.gradidoID),
+    })
+    return true
+  } catch {
+    // in case the database query fails (user deleted)
+    throw new LogError('401 Unauthorized')
+  }
+}
