9a743716713a6fc6f75ee06c8c50b4b115c3a411
ogerly <fridolin@tutanota.com>
2023-06-19 13:14:21 +0200

    add a HelloIota component, remove testcode

---
+++ frontend/src/components/HelloIota.vue
@@ -0,0 +1,477 @@
+<template>
+  <div class="hello">
+    <div v-if="send">
+      <h1>maskiertes, authentifiziertes Messaging Example</h1>
+      <div>seed: {{ seed }}, ({{ seed.length }})</div>
+      <div>text: {{ text }}</div>
+      <pre id="console">{{ consoleOutput }}</pre>
+    </div>
+    <div v-if="!send">
+      <hr />
+      <h1>
+        Iri status by
+        <a href="https://github.com/iotaledger/iota.js/#getting-started" target="_blank">
+          getNodeInfo()
+        </a>
+      </h1>
+      <pre>{{ nodeInfo }}</pre>
+      <b-container>
+        <div class="card mt-5">
+          <button @click="getNodeInfo">{{ $t('iota.infoButton') }}</button>
+          <div v-if="nodeInfo">
+            <h2>{{ $t('iota.nodeInfo') }}</h2>
+
+            {{ nodeInfo }}
+            <p>
+              <strong>{{ $t('iota.name') }}</strong>
+              {{ nodeInfo.name }}
+            </p>
+            <p>
+              <strong>{{ $t('iota.version') }}</strong>
+              {{ nodeInfo.version }}
+            </p>
+            <p>
+              <strong>{{ $t('iota.isHealthy') }}</strong>
+              {{ nodeInfo.isHealthy }}
+            </p>
+            <p>
+              <strong>{{ $t('iota.networkId') }}</strong>
+              {{ nodeInfo.networkId }}
+            </p>
+            <p>
+              <strong>{{ $t('iota.lastMilestoneIndex') }}</strong>
+              {{ nodeInfo.latestMilestoneIndex }}
+            </p>
+            <p>
+              <strong>{{ $t('iota.confirmedMilestoneIndex') }}</strong>
+              {{ nodeInfo.confirmedMilestoneIndex }}
+            </p>
+            <p>
+              <strong>{{ $t('iota.pruning') }}</strong>
+              {{ nodeInfo.pruningIndex }}
+            </p>
+            <p>
+              <strong>{{ $t('iota.features') }}</strong>
+              {{ nodeInfo.features }}
+            </p>
+            <p>
+              <strong>{{ $t('iota.minPoWScore') }}</strong>
+              {{ nodeInfo.minPoWScore }}
+            </p>
+          </div>
+        </div>
+      </b-container>
+      <div>
+        <hr />
+        <div class="h4">bytesToHex:</div>
+        <b-row>
+          <b-col>
+            <div>{{ bytesToHex }}</div>
+          </b-col>
+          <b-col>
+            <b-form inline>
+              <label class="sr-only" for="inline-form-input-text">Text</label>
+              <b-form-input
+                ref="textInput"
+                v-model="text"
+                id="inline-form-input-text"
+                class="mb-2 mr-sm-2 mb-sm-0"
+                placeholder="Jane Doe"
+              ></b-form-input>
+            </b-form>
+          </b-col>
+        </b-row>
+      </div>
+      <div>
+        <hr />
+        <div class="h4">blake2b:</div>
+        <b-row>
+          <b-col>
+            <div class="word-break">{{ blake2b }}</div>
+          </b-col>
+          <b-col></b-col>
+        </b-row>
+      </div>
+      <div>
+        <hr />
+        <div class="h3">Address Generator</div>
+        <button @click="generateAddresses">Generate Addresses</button>
+        <div v-for="address in addresses" :key="address.index">
+          <h2>Wallet Index {{ address.index }}</h2>
+          <p>
+            <strong>Private Key:</strong>
+            {{ address.privateKey }}
+          </p>
+          <p>
+            <strong>Public Key:</strong>
+            {{ address.publicKey }}
+          </p>
+          <p>
+            <strong>Address Ed25519:</strong>
+            {{ address.addressEd25519 }}
+          </p>
+          <p>
+            <strong>Address Bech32:</strong>
+            {{ address.addressBech32 }}
+          </p>
+          <hr />
+        </div>
+      </div>
+      <hr />
+      <div>
+        <b-form inline>
+          <label class="sr-only" for="inline-form-input-text">Text</label>
+          <b-form-input
+            v-model="textMessage"
+            id="inline-form-input-text"
+            class="mb-2 mr-sm-2 mb-sm-0"
+            placeholder="Jane Doe"
+          ></b-form-input>
+        </b-form>
+        <button
+          @click="sendData"
+          class="
+      "
+        >
+          Send Data to Iota Tangle
+        </button>
+        <div v-if="sendResult.messageId" class="mt-4">
+          <div>Received Message Id: {{ sendResult.messageId }}</div>
+          <div class="mt-4">
+            Explorer Link:
+
+            <b-link
+              :href="`https://explorer.iota.org/mainnet/message/${sendResult.messageId}`"
+              target="_blank"
+            >
+              {{ `https://explorer.iota.org/mainnet/message/${sendResult.messageId}` }}
+            </b-link>
+          </div>
+        </div>
+
+        <button @click="findMessages">Find Messages</button>
+      </div>
+      <hr />
+    </div>
+  </div>
+  <!-- <button @click="sendMessage" class="mt-4">Send masked Data to Iota Tangle</button> -->
+</template>
+<script>
+// import { crypto } from 'crypto'
+import { Converter } from '@iota/util.js'
+
+import { Bip32Path, Bip39, Blake2b } from '@iota/crypto.js'
+
+import {
+  Bech32Helper,
+  Ed25519Address,
+  Ed25519Seed,
+  ED25519_ADDRESS_TYPE,
+  generateBip44Address,
+  // IOTA_BIP44_BASE_PATH,
+  retrieveData,
+  sendData,
+  SingleNodeClient,
+} from '@iota/iota.js'
+
+import {
+  createChannel,
+  createMessage,
+  parseMessage,
+  mamAttach,
+  mamFetch,
+  // mamFetchAll,
+  // channelRoot,
+  // mamFetchCombined,
+  TrytesHelper,
+} from '@iota/mam.js'
+
+// import axios from 'axios'
+
+// import HelloIota from '@/components/HelloIota'
+
+const API_ENDPOINT = 'https://chrysalis-nodes.iota.org/'
+const EXPLORER_ROOT = 'https://explorer.iota.org/mainnet'
+
+localStorage.setItem('channelState', JSON.stringify({}))
+
+export default {
+  name: 'HelloIota',
+  props: {
+    text: {
+      type: String,
+    },
+    send: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      nodeInfo: null,
+      nodeHealth: null,
+      addresses: [],
+      myIndex: 0,
+      sendResult: [],
+      textMessage: '',
+      seed: '',
+      mode: 'restricted',
+      sideKey: 'MYKEY',
+      consoleOutput: '',
+      node: API_ENDPOINT,
+      explorerRoot: EXPLORER_ROOT,
+      channelState: {},
+    }
+  },
+  computed: {
+    bytesToHex() {
+      return Converter.bytesToHex(Converter.utf8ToBytes(this.text))
+    },
+
+    blake2b() {
+      const sum = Blake2b.sum512(Converter.utf8ToBytes(this.text))
+
+      return this.text === null || this.text === '' ? '' : Converter.bytesToHex(sum)
+    },
+  },
+  methods: {
+    async getNodeInfo() {
+      const client = new SingleNodeClient(API_ENDPOINT)
+
+      try {
+        const info = await client.info()
+        this.nodeInfo = info
+      } catch (err) {
+        this.nodeInfo = err
+      }
+    },
+    async generateAddresses() {
+      const client = new SingleNodeClient(API_ENDPOINT)
+      const info = await client.info()
+      const randomMnemonic = Bip39.randomMnemonic()
+      const baseSeed = Ed25519Seed.fromMnemonic(randomMnemonic)
+      const addressGeneratorAccountState = {
+        accountIndex: 0,
+        addressIndex: 0,
+        isInternal: false,
+      }
+
+      const generatedAddresses = []
+      for (let i = 0; i < 6; i++) {
+        const path = generateBip44Address(addressGeneratorAccountState)
+        const addressSeed = baseSeed.generateSeedFromPath(new Bip32Path(path))
+        const addressKeyPair = addressSeed.keyPair()
+        const indexEd25519Address = new Ed25519Address(addressKeyPair.publicKey)
+        const indexPublicKeyAddress = indexEd25519Address.toAddress()
+
+        generatedAddresses.push({
+          index: path,
+          privateKey: Converter.bytesToHex(addressKeyPair.privateKey),
+          publicKey: Converter.bytesToHex(addressKeyPair.publicKey),
+          addressEd25519: Converter.bytesToHex(indexPublicKeyAddress),
+          addressBech32: Bech32Helper.toBech32(
+            ED25519_ADDRESS_TYPE,
+            indexPublicKeyAddress,
+            info.bech32HRP,
+          ),
+        })
+      }
+
+      this.addresses = generatedAddresses
+    },
+    async sendData() {
+      if (!this.textMessage) {
+        alert('Please enter message')
+        return
+      }
+
+      console.log('Sending Data')
+      this.sendResult = await sendData(
+        this.client,
+        this.myIndex,
+        Converter.utf8ToBytes(
+          `This is test data from gradido localhost client 🚀 :: ${this.textMessage}`,
+        ),
+      )
+
+      console.log('Received Message Id', this.sendResult.messageId)
+      console.log(`https://explorer.iota.org/mainnet/message/${this.sendResult.messageId}`)
+    },
+    async findMessages() {
+      this.client = new SingleNodeClient(API_ENDPOINT)
+      console.log('Finding messages with index', this.sendResult.messageId)
+      const found = await this.client.messagesFind(
+        '07ba481e4fd6ce172714015a5f5e400007acf0bc1c1fee3824ffa923cf803473',
+      )
+
+      if (found && found.messageIds.length > 0) {
+        console.log(`Found: ${found.count} of ${found.maxResults}`)
+
+        const firstResult = await retrieveData(this.client, found.messageIds[0])
+        if (firstResult) {
+          console.log('First Result')
+          console.log('\tIndex: ', Converter.bytesToUtf8(firstResult.index))
+          console.log(
+            '\tData: ',
+            firstResult.data ? Converter.bytesToUtf8(firstResult.data) : 'None',
+          )
+        }
+      } else {
+        console.log('Found no results')
+      }
+    },
+    async sendMessage() {
+      const channelState = createChannel(this.seed, 2, this.mode, this.sideKey)
+      const mamMessage = createMessage(channelState, 'MY9MESSAGE')
+      this.sendResult = await mamAttach(this.node, mamMessage, 'MY9MAM')
+
+      console.log('masked Message Id', this.sendResult)
+      console.log(`https://explorer.iota.org/mainnet/message/${this.sendResult.messageId}`)
+    },
+    async fetchMessage() {
+      // const channelState = createChannel(this.seed, 2, this.mode, this.sideKey)
+      // const initialRoot = channelRoot(channelState)
+      // const fetched = await mamFetch(this.node, initialRoot, this.mode, this.sideKey)
+      // console.log(fetched)
+    },
+    consoleLog(message, obj) {
+      this.consoleOutput += message + '\n'
+      if (obj) {
+        this.consoleOutput += obj + '\n'
+      }
+    },
+    consoleError(error) {
+      this.consoleOutput += 'Error: ' + error + '\n'
+    },
+    async run(asciiMessage) {
+      // Setup the details for the channel
+
+      // Try and load the channel state from JSON data
+      try {
+        const currentState = localStorage.getItem('channelState')
+        if (currentState) {
+          this.channelState = currentState.data
+        }
+      } catch (e) {}
+
+      // If we couldn't load the details then create a new channel.
+      if (!this.channelState) {
+        this.channelState = createChannel(this.generateSeed(81), 2, this.mode, this.sideKey)
+      }
+
+      // Create a MAM message using the channel state.
+      console.log('asciiMessage', this.text)
+      const mamMessage = createMessage(this.channelState, TrytesHelper.fromAscii(this.text))
+
+      // Display the details for the MAM message.
+      this.consoleLog('Seed:', this.channelState.seed)
+      this.consoleLog('Address:', mamMessage.address)
+      this.consoleLog('Root:', mamMessage.root)
+      this.consoleLog('NextRoot:', this.channelState.nextRoot)
+
+      // Decode the message using the root and sideKey.
+      // The decode is for demonstration purposes, there is no reason to decode at this point.
+      const decodedMessage = parseMessage(mamMessage.payload, mamMessage.root, this.sideKey)
+
+      // Display the decoded data.
+      this.consoleLog('Decoded NextRoot', decodedMessage.nextRoot)
+      this.consoleLog('Decoded Message', decodedMessage.message)
+
+      // Store the channel state.
+      try {
+        // await axios.post('./channelState.json', this.channelState)
+        localStorage.setItem('channelState', JSON.stringify(this.channelState))
+      } catch (e) {
+        this.consoleError(e)
+      }
+
+      // So far we have shown how to create and parse a message
+      // but now we actually want to attach the message to the tangle
+      const node = 'https://chrysalis-nodes.iota.org'
+      const explorerRoot = 'https://explorer.iota.org/mainnet'
+
+      // Attach the message.
+      this.consoleLog('Attaching to tangle, please wait...')
+      const client = new SingleNodeClient(API_ENDPOINT)
+      const { messageId } = await mamAttach(client, mamMessage, 'MY9MAM')
+      this.consoleLog('Message Id', messageId)
+      this.consoleLog(
+        `You can view the stored message here ${this.explorerRoot}/message/${messageId}`,
+      )
+      this.consoleLog(
+        `You can view the MAM channel here ${explorerRoot}/streams/0/${mamMessage.root}/${this.mode}/${this.sideKey}`,
+      )
+
+      // Try fetching it as well.
+      this.consoleLog('Fetching from tangle, please wait...')
+      const fetched = await mamFetch(node, mamMessage.root, this.mode, this.sideKey)
+      if (fetched) {
+        this.consoleLog('Fetched Root', fetched.root)
+        this.consoleLog('Fetched', TrytesHelper.toAscii(fetched.message))
+        this.consoleLog('Fetched Next Root', fetched.nextRoot)
+      } else {
+        this.consoleLog('Nothing was fetched from the MAM channel')
+      }
+    },
+    generateSeed(length) {
+      console.log('Generating seed lenght', length)
+
+      const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ9'
+      const randomValues = new Uint8Array(length)
+      window.crypto.getRandomValues(randomValues)
+
+      this.seed = Array.from(randomValues)
+        .map((value) => charset[value % 27])
+        .join('')
+      console.log('seed', this.seed)
+      console.log('seed length', this.seed.length)
+      return this.seed
+    },
+  },
+  created() {
+    // this.getNodeInfo()
+    // this.client = new SingleNodeClient(API_ENDPOINT)
+    // this.myIndex = Converter.utf8ToBytes('MY-DATA-INDEX')
+
+    if (this.send) {
+      this.run(this.text)
+        .then(() => {
+          this.consoleLog('done')
+        })
+        .catch((err) => {
+          this.consoleError(err)
+        })
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+pre {
+  width: 100%;
+  overflow: auto;
+  overflow-y: hidden;
+  font-size: 12px;
+  line-height: 20px;
+  background: #efefef;
+}
+pre code {
+  padding: 10px;
+  color: #333;
+}
+</style>
