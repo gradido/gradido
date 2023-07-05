9e9e10d9fc8f93fd34ded8a966991c84fad951a1
mahula <lenzmath@posteo.de>
2023-07-05 10:50:59 +0200

    add report metadata for e2e test html report

--- e2e-tests/create-cucumber-html-report.js
+++ e2e-tests/create-cucumber-html-report.js
@@ -1,5 +1,15 @@
 const report = require("multiple-cucumber-html-reporter");
 report.generate({
   jsonDir: "cypress/reports/json_logs",
-  reportPath: "./cypress/reports/cucumber_html_report"
+  reportPath: "./cypress/reports/cucumber_html_report",
+  metadata: {
+    app: {
+      name: "Gradido webapp"
+    },
+    device: "Github worker",
+    plattform: {
+      name: "ubuntu",
+      version: "latest"
+    }
+  }
 });
