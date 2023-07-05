3b3c3bed735e4120b554f1ac3531df0aa1c39b04
mahula <lenzmath@posteo.de>
2023-07-05 18:04:24 +0200

    fix report generation in e2e test flow

--- e2e-tests/create-cucumber-html-report.js
+++ e2e-tests/create-cucumber-html-report.js
@@ -1,7 +1,9 @@
 const report = require("multiple-cucumber-html-reporter");
 report.generate({
-  jsonDir: "cypress/json_logs",
+  jsonDir: "cypress/reports/json_logs",
   reportPath: "./cypress/reports/cucumber_html_report",
+  pageTitle: "Gradido webapp end-to-end test report",
+
   metadata: {
     app: {
       name: "Gradido webapp"
