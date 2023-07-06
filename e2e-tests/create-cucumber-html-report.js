8087bff29a410bc747a5f74b55c4aeddd7437cec
mahula <lenzmath@posteo.de>
2023-07-06 09:11:13 +0200

    update e2e test report meta data

--- e2e-tests/create-cucumber-html-report.js
+++ e2e-tests/create-cucumber-html-report.js
@@ -1,17 +1,11 @@
 const report = require("multiple-cucumber-html-reporter");
+
+const reportTitle = "Gradido webapp end-to-end test report"
+
 report.generate({
   jsonDir: "cypress/reports/json_logs",
   reportPath: "./cypress/reports/cucumber_html_report",
-  pageTitle: "Gradido webapp end-to-end test report",
-
-  metadata: {
-    app: {
-      name: "Gradido webapp"
-    },
-    device: "Github worker",
-    platform: {
-      name: "ubuntu",
-      version: "latest"
-    }
-  }
+  pageTitle: reportTitle,
+  reportName: reportTitle,
+  hideMetadata: true
 });
