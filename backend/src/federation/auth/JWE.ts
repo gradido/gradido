4fc5d6fc68a5b632e532d0fc3e834fe31bc62e0b
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-07-12 22:57:21 +0200

    cleanup the crypto

--- backend/src/federation/auth/JWE.ts
+++ backend/src/federation/auth/JWE.ts
@@ -10,63 +10,34 @@ import {
   crypto_box_SECRETKEYBYTES,
 } from 'sodium-native'
 
-export const verifyToken = async (
-  token: string,
-  keyPair: { publicKey: Buffer; privateKey: Buffer },
-  nonce: number | null = null,
-) => {
-  const pubKeyX = Buffer.alloc(crypto_box_PUBLICKEYBYTES)
-  const privKeyX = Buffer.alloc(crypto_box_SECRETKEYBYTES)
-  crypto_sign_ed25519_pk_to_curve25519(pubKeyX, keyPair.publicKey)
-  crypto_sign_ed25519_sk_to_curve25519(
-    privKeyX,
-    keyPair.privateKey.subarray(0, crypto_sign_SECRETKEYBYTES),
-  )
-  const key = createPrivateKey({
-    key: {
-      kty: 'OKP',
-      crv: 'X25519',
-      x: pubKeyX.toString('base64url'),
-      d: privKeyX.toString('base64url'),
-    },
-    format: 'jwk',
-  })
+interface JWSPayload {
+  nonce: number
+  time: Date
+}
+interface JWEPayload {
+  jws: string
+  publicKey: string
+}
+interface KeyPair {
+  publicKey: Buffer
+  privateKey: Buffer
+}
 
-  const { plaintext } = await compactDecrypt(token, key)
-  const decryptedJWEPayload = JSON.parse(new TextDecoder().decode(plaintext)) as {
-    jws: string
-    publicKey: string
-  }
-  const foreignPub = createPublicKey({
+// eslint-disable-next-line camelcase
+const jwk_ed25519_pk = (publicKey: Buffer) => {
+  return createPublicKey({
     key: {
       kty: 'OKP',
       crv: 'Ed25519',
-      x: Buffer.from(decryptedJWEPayload.publicKey).toString('base64url'),
+      x: publicKey.toString('base64url'),
     },
     format: 'jwk',
   })
-  // TODO: verify clientPubKey (e.g. check federation table + rights here)
-  const { payload: payloadJWS } = await compactVerify(decryptedJWEPayload.jws, foreignPub)
-  const receivedNonce = JSON.parse(new TextDecoder().decode(payloadJWS)) as {
-    nonce: number
-    time: Date
-  }
-
-  if (nonce && receivedNonce.nonce !== nonce) {
-    throw new Error('Could not verify nonce')
-  }
-
-  return { publicKey: decryptedJWEPayload.publicKey, nonce: receivedNonce.nonce }
 }
 
-export const generateToken = async (
-  nonce: number,
-  keyPair: { publicKey: Buffer; privateKey: Buffer },
-  receiverPublicKey: Buffer,
-) => {
-  const receiverPublicKeyX = Buffer.alloc(crypto_box_PUBLICKEYBYTES)
-  crypto_sign_ed25519_pk_to_curve25519(receiverPublicKeyX, receiverPublicKey)
-  const key = createPrivateKey({
+// eslint-disable-next-line camelcase
+const jwk_ed25519_sk = (keyPair: KeyPair) => {
+  return createPrivateKey({
     key: {
       kty: 'OKP',
       crv: 'Ed25519',
@@ -75,14 +46,62 @@ export const generateToken = async (
     },
     format: 'jwk',
   })
-  const foreignPub = createPublicKey({
+}
+
+// eslint-disable-next-line camelcase
+const jwk_x25519_pk = (publicKey: Buffer) => {
+  const publicKeyX = Buffer.alloc(crypto_box_PUBLICKEYBYTES)
+  crypto_sign_ed25519_pk_to_curve25519(publicKeyX, publicKey)
+  return createPublicKey({
     key: {
       kty: 'OKP',
       crv: 'X25519',
-      x: receiverPublicKeyX.toString('base64url'),
+      x: publicKeyX.toString('base64url'),
     },
     format: 'jwk',
   })
+}
+
+// eslint-disable-next-line camelcase
+const jwk_x25519_sk = (keyPair: KeyPair) => {
+  const pubKeyX = Buffer.alloc(crypto_box_PUBLICKEYBYTES)
+  const privKeyX = Buffer.alloc(crypto_box_SECRETKEYBYTES)
+  crypto_sign_ed25519_pk_to_curve25519(pubKeyX, keyPair.publicKey)
+  crypto_sign_ed25519_sk_to_curve25519(
+    privKeyX,
+    keyPair.privateKey.subarray(0, crypto_sign_SECRETKEYBYTES),
+  )
+  return createPrivateKey({
+    key: {
+      kty: 'OKP',
+      crv: 'X25519',
+      x: pubKeyX.toString('base64url'),
+      d: privKeyX.toString('base64url'),
+    },
+    format: 'jwk',
+  })
+}
+
+export const verifyToken = async (token: string, keyPair: KeyPair, nonce: number | null = null) => {
+  const key = jwk_x25519_sk(keyPair)
+  const { plaintext } = await compactDecrypt(token, key)
+  const decryptedJWEPayload = JSON.parse(new TextDecoder().decode(plaintext)) as JWEPayload
+  // TODO validate `decryptedJWEPayload.publicKey` to be in database/authorized
+  const foreignKey = jwk_ed25519_pk(Buffer.from(decryptedJWEPayload.publicKey))
+  const { payload: payloadJWS } = await compactVerify(decryptedJWEPayload.jws, foreignKey)
+  const decryptedJWSPayload = JSON.parse(new TextDecoder().decode(payloadJWS)) as JWSPayload
+  // TODO validate `decryptedJWSPayload.time`
+
+  if (nonce && decryptedJWSPayload.nonce !== nonce) {
+    throw new Error('Could not verify nonce')
+  }
+
+  return { publicKey: decryptedJWEPayload.publicKey, ...decryptedJWSPayload }
+}
+
+export const generateToken = async (nonce: number, keyPair: KeyPair, receiverPublicKey: Buffer) => {
+  const key = jwk_ed25519_sk(keyPair)
+  const foreignPub = jwk_x25519_pk(receiverPublicKey)
   const jws = await new CompactSign(
     new TextEncoder().encode(
       JSON.stringify({
