1456d4a74abc553b3ef466211ed616606fdcb70f
clauspeterhuebner <86960882+clauspeterhuebner@users.noreply.github.com>
2023-07-13 13:34:33 +0200

    Update backend/src/graphql/resolver/UserResolver.test.ts
    Co-authored-by: Moriz Wahl <moriz.wahl@gmx.de>

--- backend/src/graphql/resolver/UserResolver.test.ts
+++ backend/src/graphql/resolver/UserResolver.test.ts
@@ -165,7 +165,7 @@ describe('UserResolver', () => {
               createdAt: expect.any(Date),
               // emailChecked: false,
               language: 'de',
-              userRoles: [], // expect.any(Array),
+              userRoles: [],
               deletedAt: null,
               publisherId: 1234,
               referrerId: null,
