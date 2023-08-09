51d1c6b64bf95ce7fc388c8870f10fba0437bb39
Einhornimmond <git@einhornimmond.de>
2023-08-09 13:55:46 +0200

    add logging for automatic validation

--- backend/src/graphql/schema.ts
+++ backend/src/graphql/schema.ts
@@ -1,8 +1,11 @@
 import path from 'path'
 
+import { validate } from 'class-validator'
 import { Decimal } from 'decimal.js-light'
 import { GraphQLSchema } from 'graphql'
 import { buildSchema } from 'type-graphql'
+
+import { LogError } from '@/server/LogError'
 
 import { isAuthorized } from './directive/isAuthorized'
 import { DecimalScalar } from './scalar/Decimal'
@@ -12,5 +15,20 @@ export const schema = async (): Promise<GraphQLSchema> => {
     resolvers: [path.join(__dirname, 'resolver', `!(*.test).{js,ts}`)],
     authChecker: isAuthorized,
     scalarsMap: [{ type: Decimal, scalar: DecimalScalar }],
+    validate: (argValue) => {
+      if (argValue) {
+        validate(argValue)
+          .then((errors) => {
+            if (errors.length > 0) {
+              throw new LogError('validation failed. errors: ', errors)
+            } else {
+              return true
+            }
+          })
+          .catch((e) => {
+            throw new LogError('validation throw an exception: ', e)
+          })
+      }
+    },
   })
 }
