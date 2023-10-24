29c104002dc194e5486bca20260b0544c4bc7468
Claus-Peter Huebner <claus-peter.huebner@email.de>
2023-10-24 23:35:48 +0200

    additional validations

--- federation/src/graphql/api/1_0/model/OpenConnectionCallbackArgs.ts
+++ federation/src/graphql/api/1_0/model/OpenConnectionCallbackArgs.ts
@@ -1,10 +1,13 @@
+import { IsString } from 'class-validator'
 import { Field, InputType } from 'type-graphql'
 
 @InputType()
 export class OpenConnectionCallbackArgs {
   @Field(() => String)
+  @IsString()
   oneTimeCode: string
 
   @Field(() => String)
+  @IsString()
   url: string
 }
