c3d72cb8df380a83ef69315c9dba8bd427fb00c8
Einhornimmond <git@einhornimmond.de>
2023-12-06 21:50:30 +0100

    add missing init call for key manager

--- dlt-connector/src/index.ts
+++ dlt-connector/src/index.ts
@@ -1,12 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { CONFIG } from '@/config'
 import createServer from './server/createServer'
+import { KeyManager } from './manager/KeyManager'
 
 async function main() {
   // eslint-disable-next-line no-console
   console.log(`DLT_CONNECTOR_PORT=${CONFIG.DLT_CONNECTOR_PORT}`)
   const { app } = await createServer()
 
+  await KeyManager.getInstance().init()
   app.listen(CONFIG.DLT_CONNECTOR_PORT, () => {
     // eslint-disable-next-line no-console
     console.log(`Server is running at http://localhost:${CONFIG.DLT_CONNECTOR_PORT}`)
