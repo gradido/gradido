3f2387cfa1a8d7ba99e7fdb8f06aa57d03d7f358
Claus-Peter Huebner <claus-peter.huebner@email.de>
2024-02-07 23:35:27 +0100

    linting

--- backend/src/graphql/arg/UpdateUserInfosArgs.ts
+++ backend/src/graphql/arg/UpdateUserInfosArgs.ts
@@ -1,8 +1,10 @@
+
 import { IsBoolean, IsInt, IsString } from 'class-validator'
 import { ArgsType, Field, InputType, Int } from 'type-graphql'
 
 import { Location } from '@model/Location'
-import { isValidLocation } from '@validator/Location'
+
+import { isValidLocation } from '@/graphql/validator/Location'
 
 @InputType()
 @ArgsType()
