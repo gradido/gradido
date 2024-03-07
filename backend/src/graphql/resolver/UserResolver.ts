58e4c072442219c1299b0461cd82880be1e92668
Claus-Peter Huebner <claus-peter.huebner@email.de>
2024-03-07 14:14:24 +0100

    add configswitch GMS_ACTIVE before transfer to gms

--- backend/src/graphql/resolver/UserResolver.ts
+++ backend/src/graphql/resolver/UserResolver.ts
@@ -645,7 +645,7 @@ export class UserResolver {
     await EVENT_USER_INFO_UPDATE(user)
 
     // validate if user settings are changed with relevance to update gms-user
-    if (compareGmsRelevantUserSettings(user, updateUserInfosArgs)) {
+    if (CONFIG.GMS_ACTIVE && compareGmsRelevantUserSettings(user, updateUserInfosArgs)) {
       logger.debug(`changed user-settings relevant for gms-user update...`)
       const homeCom = await getHomeCommunity()
       if (homeCom.gmsApiKey !== null) {
