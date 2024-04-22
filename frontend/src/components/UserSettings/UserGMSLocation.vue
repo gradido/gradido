49c79d3a9209b0996883468bcf1083ce8ed5675e
Claus-Peter Huebner <claus-peter.huebner@email.de>
2024-04-22 19:15:12 +0200

    first try of modal-dialog

--- frontend/src/components/UserSettings/UserGMSLocation.vue
+++ frontend/src/components/UserSettings/UserGMSLocation.vue
@@ -1,8 +1,28 @@
 <template>
-  <b-button>{{ $t('settings.GMS.location.button') }}</b-button>
+  <div class="mt-3 mb-5">
+    <b-button @click="showModal">{{ $t('settings.GMS.location.button') }}</b-button>
+    <modal v-show="isModalVisible" @close="closeModal" />
+  </div>
 </template>
 <script>
+import modal from './UserGMSLocationCapturing.vue'
 export default {
   name: 'UserGMSLocation',
+  components: {
+    modal,
+  },
+  data() {
+    return {
+      isModalVisible: false,
+    }
+  },
+  methods: {
+    showModal() {
+      this.isModalVisible = true
+    },
+    closeModal() {
+      this.isModalVisible = false
+    },
+  },
 }
 </script>
---
+++ frontend/src/components/UserSettings/UserGMSLocationCapturing.vue
@@ -0,0 +1,96 @@
+<script>
+export default {
+  name: 'UserGMSLocationCapturing',
+  methods: {
+    close() {
+      this.$emit('close')
+    },
+  },
+}
+</script>
+
+<template>
+  <div class="modal-backdrop">
+    <div class="modal">
+      <header class="modal-header">
+        <slot name="header">This is the default title!</slot>
+        <button type="button" class="btn-close" @click="close">x</button>
+      </header>
+
+      <section class="modal-body">
+        <slot name="body">This is the default body!</slot>
+      </section>
+
+      <footer class="modal-footer">
+        <slot name="footer">This is the default footer!</slot>
+        <button type="button" class="btn-green" @click="close">Close Modal</button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<style>
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal {
+  background: #ffffff;
+  box-shadow: 2px 2px 20px 1px;
+  overflow-x: auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.modal-header,
+.modal-footer {
+  padding: 15px;
+  display: flex;
+}
+
+.modal-header {
+  position: relative;
+  border-bottom: 1px solid #eeeeee;
+  color: #088000;
+  justify-content: space-between;
+}
+
+.modal-footer {
+  border-top: 1px solid #eeeeee;
+  flex-direction: column;
+  justify-content: flex-end;
+}
+
+.modal-body {
+  position: relative;
+  padding: 20px 10px;
+}
+
+.btn-close {
+  position: absolute;
+  top: 0;
+  right: 0;
+  border: none;
+  font-size: 20px;
+  padding: 10px;
+  cursor: pointer;
+  font-weight: bold;
+  color: #088000;
+  background: transparent;
+}
+
+.btn-green {
+  color: white;
+  background: #088000;
+  border: 1px solid #4aae9b;
+  border-radius: 2px;
+}
+</style>
