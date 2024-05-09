0a82bdc261f031009a786cc08cbc70782bdf83e9
einhornimmond <info@einhornimmond.de>
2024-05-09 14:17:23 +0200

    remove gdt menu entry, make sure transactions is also active if gdt was clicked

--- frontend/src/components/Menu/Sidebar.vue
+++ frontend/src/components/Menu/Sidebar.vue
@@ -16,17 +16,13 @@
             <b-icon icon="cash" aria-hidden="true"></b-icon>
             <span class="ml-2">{{ $t('navigation.send') }}</span>
           </b-nav-item>
-          <b-nav-item to="/transactions" class="mb-3" active-class="activeRoute">
+          <b-nav-item to="/transactions" :class="transactionClass" active-class="activeRoute">
             <b-img src="/img/svg/transaction.svg" height="20" class="svg-icon" />
             <span class="ml-2">{{ $t('navigation.transactions') }}</span>
           </b-nav-item>
           <b-nav-item to="/community" class="mb-3" active-class="activeRoute">
             <b-img src="/img/svg/community.svg" height="20" class="svg-icon" />
             <span class="ml-2">{{ $t('creation') }}</span>
-          </b-nav-item>
-          <b-nav-item to="/gdt" class="mb-3" active-class="activeRoute">
-            <b-icon icon="layers" aria-hidden="true"></b-icon>
-            <span class="ml-2">{{ $t('GDT') }}</span>
           </b-nav-item>
           <b-nav-item to="/information" class="mb-3" active-class="activeRoute">
             <b-img src="/img/svg/info.svg" height="20" class="svg-icon" />
@@ -81,6 +77,14 @@ export default {
   name: 'Sidebar',
   props: {
     shadow: { type: Boolean, required: false, default: true },
+  },
+  computed: {
+    transactionClass() {
+      if (this.$route.path === '/gdt') {
+        return 'mb-3 activeRoute'
+      }
+      return 'mb-3'
+    },
   },
 }
 </script>
