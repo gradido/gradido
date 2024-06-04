e5216fd685a1e8b3239465d53934b01645134585
Claus-Peter Huebner <claus-peter.huebner@email.de>
2024-06-05 01:44:52 +0200

    add node-options for openssl topic at frontend

--- deployment/bare_metal/start.sh
+++ deployment/bare_metal/start.sh
@@ -213,6 +213,8 @@ echo "==========================================================================
 cd $PROJECT_ROOT/frontend
 # TODO maybe handle this differently?
 unset NODE_ENV
+# TODO this is the quick&dirty solution for the openssl security topic, please see https://stackoverflow.com/questions/69692842/error-message-error0308010cdigital-envelope-routinesunsupported
+export NODE_OPTIONS=--openssl-legacy-provider
 
 # upgrade yarn and node versions
 nvm use v20.0.0
