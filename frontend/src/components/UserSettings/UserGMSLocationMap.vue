cbcbd5a2381a41e4850a88d33ec1e7d156cfd824
Claus-Peter Huebner <claus-peter.huebner@email.de>
2024-06-12 21:42:32 +0200

    correct LMarker usage

--- frontend/src/components/UserSettings/UserGMSLocationMap.vue
+++ frontend/src/components/UserSettings/UserGMSLocationMap.vue
@@ -42,8 +42,8 @@
     />
     <l-map
       v-if="showMap"
-      :zoom="this.zoom"
-      :center="this.center"
+      :zoom="zoom"
+      :center="center"
       :options="mapOptions"
       class="map_div3"
       @update:center="centerUpdate"
@@ -51,14 +51,14 @@
     >
       <l-tile-layer :url="url" :attribution="attribution" />
       <l-geosearch :options="geosearchOptions" />
-      <l-marker :lat-lng="this.userLocation">
+      <l-marker :lat-lng="userLocation">
         <l-tooltip :options="{ permanent: true, interactive: true }">
           <div @click="fixLocation">
             {{ $t('userlocationcapturing.userlocationlabel') }}
           </div>
         </l-tooltip>
       </l-marker>
-      <l-marker :lat-lng="this.comLocation">
+      <l-marker :lat-lng="comLocation">
         <l-tooltip :options="{ permanent: true }">
           <div>
             {{ $t('userlocationcapturing.communitylocationlabel') }}
