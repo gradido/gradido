9ea791e2521c4e90d07e2c888f0c8ff82ba18297
Mateusz Michałowski <mateusz.michalowski@monterail.com>
2024-07-11 10:23:11 +0200

    Revert "initial dependency update with initial setup"
    This reverts commit aa71afc3eca20042a1e13066bee1730a15606dd2.

--- admin/src/components/Tables/StatisticTable.vue
+++ admin/src/components/Tables/StatisticTable.vue
@@ -1,84 +1,100 @@
 <!-- eslint-disable vue/no-static-inline-styles -->
 <template>
   <div class="statistic-table">
-    <b-table-simple style="width: auto" class="mt-5" striped stacked="md">
-      <b-thead>
-        <b-tr>
-          <b-th></b-th>
-          <b-th class="text-right">{{ $t('statistic.count') }}</b-th>
-          <b-th class="text-right">{{ $t('statistic.details') }}</b-th>
-        </b-tr>
-      </b-thead>
-      <b-tbody>
-        <b-tr>
-          <b-td>
+    <BTableSimple style="width: auto" class="mt-5" striped stacked="md">
+      <BThead>
+        <BTr>
+          <BTh />
+          <BTh class="text-right">{{ $t('statistic.count') }}</BTh>
+          <BTh class="text-right">{{ $t('statistic.details') }}</BTh>
+        </BTr>
+      </BThead>
+      <BTbody>
+        <BTr>
+          <BTd>
             <b>{{ $t('statistic.totalUsers') }}</b>
-          </b-td>
-          <b-td class="text-right">{{ value.totalUsers }}</b-td>
-          <b-td></b-td>
-        </b-tr>
-        <b-tr>
-          <b-td>
+          </BTd>
+          <BTd class="text-right">
+            {{ value.totalUsers }}
+          </BTd>
+          <BTd></BTd>
+        </BTr>
+        <BTr>
+          <BTd>
             <b>{{ $t('statistic.activeUsers') }}</b>
-          </b-td>
-          <b-td class="text-right">{{ value.activeUsers }}</b-td>
-          <b-td></b-td>
-        </b-tr>
-        <b-tr>
-          <b-td>
+          </BTd>
+          <BTd class="text-right">
+            {{ value.activeUsers }}
+          </BTd>
+          <BTd></BTd>
+        </BTr>
+        <BTr>
+          <BTd>
             <b>{{ $t('statistic.deletedUsers') }}</b>
-          </b-td>
-          <b-td class="text-right">{{ value.deletedUsers }}</b-td>
-          <b-td></b-td>
-        </b-tr>
-        <b-tr>
-          <b-td>
+          </BTd>
+          <BTd class="text-right">
+            {{ value.deletedUsers }}
+          </BTd>
+          <BTd></BTd>
+        </BTr>
+        <BTr>
+          <BTd>
             <b>{{ $t('statistic.totalGradidoCreated') }}</b>
-          </b-td>
-          <b-td class="text-right">
-            {{ $n(value.totalGradidoCreated, 'decimal') }} {{ $t('GDD') }}
-          </b-td>
-          <b-td class="text-right">{{ value.totalGradidoCreated }}</b-td>
-        </b-tr>
-        <b-tr>
-          <b-td>
+          </BTd>
+          <BTd class="text-right">
+            <!--            {{ $n(value.totalGradidoCreated, 'decimal') }} {{ $t('GDD') }}-->
+            4500
+          </BTd>
+          <BTd class="text-right">
+            {{ value.totalGradidoCreated }}
+          </BTd>
+        </BTr>
+        <BTr>
+          <BTd>
             <b>{{ $t('statistic.totalGradidoDecayed') }}</b>
-          </b-td>
-          <b-td class="text-right">
-            {{ $n(value.totalGradidoDecayed, 'decimal') }} {{ $t('GDD') }}
-          </b-td>
-          <b-td class="text-right">{{ value.totalGradidoDecayed }}</b-td>
-        </b-tr>
-        <b-tr>
-          <b-td>
+          </BTd>
+          <BTd class="text-right">
+            <!--            {{ $n(value.totalGradidoDecayed, 'decimal') }} {{ $t('GDD') }}-->
+            100
+          </BTd>
+          <BTd class="text-right">{{ value.totalGradidoDecayed }}</BTd>
+        </BTr>
+        <BTr>
+          <BTd>
             <b>{{ $t('statistic.totalGradidoAvailable') }}</b>
-          </b-td>
-          <b-td class="text-right">
-            {{ $n(value.totalGradidoAvailable, 'decimal') }} {{ $t('GDD') }}
-          </b-td>
-          <b-td class="text-right">{{ value.totalGradidoAvailable }}</b-td>
-        </b-tr>
-        <b-tr>
-          <b-td>
+          </BTd>
+          <BTd class="text-right">
+            <!--            {{ $n(value.totalGradidoAvailable, 'decimal') }} {{ $t('GDD') }}-->
+            500
+          </BTd>
+          <BTd class="text-right">
+            {{ value.totalGradidoAvailable }}
+          </BTd>
+        </BTr>
+        <BTr>
+          <BTd>
             <b>{{ $t('statistic.totalGradidoUnbookedDecayed') }}</b>
-          </b-td>
-          <b-td class="text-right">
-            {{ $n(value.totalGradidoUnbookedDecayed, 'decimal') }} {{ $t('GDD') }}
-          </b-td>
-          <b-td class="text-right">{{ value.totalGradidoUnbookedDecayed }}</b-td>
-        </b-tr>
-      </b-tbody>
-    </b-table-simple>
+          </BTd>
+          <BTd class="text-right">
+            <!--            {{ $n(value.totalGradidoUnbookedDecayed, 'decimal') }} {{ $t('GDD') }}-->
+            600
+          </BTd>
+          <BTd class="text-right">
+            {{ value.totalGradidoUnbookedDecayed }}
+          </BTd>
+        </BTr>
+      </BTbody>
+    </BTableSimple>
   </div>
 </template>
-<script>
-export default {
-  name: 'StatisticTable',
-  props: {
-    value: {
-      type: Object,
-      required: true,
-    },
+<script setup>
+import { defineProps } from 'vue'
+import { BTableSimple, BThead, BTr, BTh, BTbody, BTd } from 'bootstrap-vue-next'
+
+const props = defineProps({
+  value: {
+    type: Object,
+    required: true,
   },
-}
+})
 </script>
