18cbd772913af326aa52c9cbf9aad05061aae36e
Kamila <kamila.lach1@gmail.com>
2024-07-14 14:10:13 +0200

    bump apollo package

--- admin/src/plugins/apolloProvider.js
+++ admin/src/plugins/apolloProvider.js
@@ -1,9 +1,10 @@
-import { ApolloClient, ApolloLink, InMemoryCache, createHttpLink } from '@apollo/client/core'
-import { createApolloProvider } from '@vue/apollo-option'
+import { ApolloClient, ApolloLink, InMemoryCache, HttpLink } from 'apollo-boost'
+import VueApollo from 'vue-apollo'
 import CONFIG from '../config'
 import store from '../store/store'
+import { provideApolloClient } from '@vue/apollo-composable'
 
-const httpLink = createHttpLink({ uri: CONFIG.GRAPHQL_URI })
+const httpLink = new HttpLink({ uri: CONFIG.GRAPHQL_URI })
 
 const authLink = new ApolloLink((operation, forward) => {
   const token = store.state.token
@@ -30,6 +31,8 @@ const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
 })
 
-export const apolloProvider = createApolloProvider({
+provideApolloClient(apolloClient)
+
+export const apolloProvider = new VueApollo({
   defaultClient: apolloClient,
 })
