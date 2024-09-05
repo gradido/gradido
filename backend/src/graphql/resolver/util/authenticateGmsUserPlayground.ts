8e55817f53461d929d1fdf8c5b6d6f065539dcac
Claus-Peter Huebner <claus-peter.huebner@email.de>
2024-09-06 00:11:19 +0200

    userSearchPlayground

--- backend/src/graphql/resolver/util/authenticateGmsUserPlayground.ts
+++ backend/src/graphql/resolver/util/authenticateGmsUserPlayground.ts
@@ -14,7 +14,7 @@ export async function authenticateGmsUserPlayground(
   const result = new GmsUserAuthenticationResult()
   const dashboardUrl = ensureUrlEndsWithSlash(CONFIG.GMS_DASHBOARD_URL)
 
-  result.url = dashboardUrl.concat('playground')
+  result.url = dashboardUrl.concat('usersearch-playground')
   result.token = await verifyAuthToken(apiKey, dbUser.communityUuid, token)
   logger.info('GmsUserAuthenticationResult:', result)
   return result
