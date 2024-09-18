2e5a66ae68c7558d694bf59e8ac42c1bf9d525e7
Claus-Peter Huebner <claus-peter.huebner@email.de>
2024-09-18 21:47:07 +0200

    delete save_node_modules directories

--- frontend/save2_node_modules_brokenbuild/maplibre-gl/src/shaders/terrain.fragment.glsl
+++
@@ -1,7 +0,0 @@
-uniform sampler2D u_texture;
-
-in vec2 v_texture_pos;
-
-void main() {
-    fragColor = texture(u_texture, v_texture_pos);
-}
--- frontend/save2_node_modules_brokenbuild/vue-apollo/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import './vue'
-import './gql'
-import { VueApollo } from './vue-apollo'
-import { ApolloProvider } from './apollo-provider'
-
-export default VueApollo
-export { ApolloProvider }
