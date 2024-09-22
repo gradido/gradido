c4697a010a7839e4d0e8170ff4e4c6e999e98bf6
einhornimmond <info@einhornimmond.de>
2024-09-22 17:06:17 +0200

    remove dlt-database

--- dlt-database/entity/Community.ts
+++
@@ -1,1 +0,0 @@
-export { Community } from './0005-refactor_with_gradido_blockchain_lib/Community'
--- dlt-database/entity/Transaction.ts
+++
@@ -1,1 +0,0 @@
-export { Transaction } from './0005-refactor_with_gradido_blockchain_lib/Transaction'
