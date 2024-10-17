23acb1e5ca9dbe41d4cf99cd8a70a17259ff26f7
Mateusz Michałowski <mateusz.michalowski@monterail.com>
2024-10-17 16:57:21 +0200

    feat(frontend): Map feature update

---
+++ frontend/src/components/UserSettings/CoordinatesDisplay.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="coordinates-display">
+    <div class="p-2">
+      <BButton class="me-1" size="sm" @click="emit('centerMap', 'USER')"><IBiPinMapFill /></BButton>
+      <span>
+        {{
+          $t('settings.GMS.map.userCoords', {
+            lat: userPosition.lat.toFixed(6),
+            lng: userPosition.lng.toFixed(6),
+          })
+        }}
+      </span>
+    </div>
+    <div class="p-2">
+      <BButton class="me-1" @click="emit('centerMap', 'COMMUNITY')" size="sm">
+        <IBiPinMap />
+      </BButton>
+      <span>
+        {{
+          $t('settings.GMS.map.communityCoords', {
+            lat: communityPosition.lat.toFixed(6),
+            lng: communityPosition.lng.toFixed(6),
+          })
+        }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { BButton } from 'bootstrap-vue-next'
+
+const emit = defineEmits(['centerMap'])
+
+const props = defineProps({
+  userPosition: Object,
+  communityPosition: Object,
+})
+</script>
+
+<style scoped>
+.coordinates-display {
+  margin-top: 10px;
+  font-weight: bold;
+}
+</style>
--- frontend/src/components/UserSettings/UserGMSLocation.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <BButton>{{ $t('settings.GMS.location.button') }}</BButton>
-</template>
-<script setup>
-import { BButton } from 'bootstrap-vue-next'
-</script>
