18d7bd02c79043480fc4ee237f445afd13aeea54
einhornimmond <info@einhornimmond.de>
2024-10-22 13:05:36 +0200

    check for empty emails, add receving from humhub progress

--- backend/src/apis/humhub/ExportUsers.ts
+++ backend/src/apis/humhub/ExportUsers.ts
@@ -12,6 +12,7 @@ import { GetUser } from './model/GetUser'
 import { ExecutedHumhubAction, syncUser } from './syncUser'
 
 const USER_BULK_SIZE = 20
+const HUMHUB_BULK_SIZE = 50
 
 function getUsersPage(page: number, limit: number): Promise<[User[], number]> {
   return User.findAndCount({
@@ -29,16 +30,22 @@ function getUsersPage(page: number, limit: number): Promise<[User[], number]> {
 async function loadUsersFromHumHub(client: HumHubClient): Promise<Map<string, GetUser>> {
   const start = new Date().getTime()
   const humhubUsers = new Map<string, GetUser>()
-  const firstPage = await client.users(0, 50)
+  const firstPage = await client.users(0, HUMHUB_BULK_SIZE)
+  let skippedUsersCount = 0
   if (!firstPage) {
     throw new LogError('not a single user found on humhub, please check config and setup')
   }
   firstPage.results.forEach((user) => {
-    humhubUsers.set(user.account.email.trim(), user)
+    // deleted users have empty emails
+    if (user.account.email) {
+      humhubUsers.set(user.account.email.trim(), user)
+    } else {
+      skippedUsersCount++
+    }
   })
   let page = 1
   while (humhubUsers.size < firstPage.total) {
-    const usersPage = await client.users(page, 50)
+    const usersPage = await client.users(page, HUMHUB_BULK_SIZE)
     if (!usersPage) {
       throw new LogError('error requesting next users page from humhub')
     }
@@ -46,6 +53,9 @@ async function loadUsersFromHumHub(client: HumHubClient): Promise<Map<string, Ge
       humhubUsers.set(user.account.email.trim(), user)
     })
     page++
+    process.stdout.write(
+      `load users from humhub: ${humhubUsers.size}/${usersPage.total}, skipped: ${skippedUsersCount}\r`,
+    )
   }
   const elapsed = new Date().getTime() - start
   logger.info('load users from humhub', {
