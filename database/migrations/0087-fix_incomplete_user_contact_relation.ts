9ee7f7be09c7f680d5e3217110969aa4d3b65f50
einhornimmond <info@einhornimmond.de>
2024-12-18 12:07:40 +0100

    fix migration

--- database/migrations/0087-fix_incomplete_user_contact_relation.ts
+++ database/migrations/0087-fix_incomplete_user_contact_relation.ts
@@ -10,11 +10,11 @@ export async function upgrade(queryFn: (query: string, values?: any[]) => Promis
 }
 
 export async function downgrade(queryFn: (query: string, values?: any[]) => Promise<Array<any>>) {
-  await queryFn('ALTER TABLE `user_contacts` DROP `is_primary`;')
   await queryFn('ALTER TABLE `users` ADD COLUMN `email_id` int(10) NULL AFTER `alias`;')
   // set in users table the email_id of the primary contact for this user
   await queryFn(
     'UPDATE users AS u JOIN user_contacts AS c ON u.id = c.user_id SET u.email_id = c.id WHERE c.is_primary = true;',
   )
+  await queryFn('ALTER TABLE `user_contacts` DROP `is_primary`;')
   await queryFn('ALTER TABLE `user_roles` DROP INDEX `user_id`;')
 }
