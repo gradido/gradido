507f6d251259a029de9816e34084dfa8961a2a8f
clauspeterhuebner <hr.softwarelab@t-online.de>
2025-03-27 23:55:56 +0100

    add dependency of overview cards

--- frontend/src/pages/Overview.vue
+++ frontend/src/pages/Overview.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="overview">
     <BCol>
-      <BRow>
-        <card-circles />
-      </BRow>
-      <BRow>
-        <card-user-search />
-      </BRow>
+      <div v-if="isHumhubActive">
+        <BRow>
+          <card-circles />
+        </BRow>
+      </div>
+      <div v-if="isGmsActive">
+        <BRow>
+          <card-user-search />
+        </BRow>
+      </div>
     </BCol>
   </div>
 </template>
@@ -14,11 +18,23 @@
 // import CommunityNews from '@/components/Overview/CommunityNews'
 import CardCircles from '@/components/Overview/CardCircles'
 import CardUserSearch from '@/components/Overview/CardUserSearch'
+import CONFIG from '@/config'
+
 export default {
   name: 'Overview',
   components: {
     CardCircles,
     CardUserSearch,
   },
+  computed: {
+    isGmsActive() {
+      console.log('isGmsActive=', CONFIG.GMS_ACTIVE)
+      return CONFIG.GMS_ACTIVE === true
+    },
+    isHumhubActive() {
+      console.log('isHumhubActive=', CONFIG.HUMHUB_ACTIVE)
+      return CONFIG.HUMHUB_ACTIVE === true
+    },
+  }
 }
 </script>
