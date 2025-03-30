03c018b71017e28ed09041cd180dc44a60cad99e
einhornimmond <info@einhornimmond.de>
2025-03-30 11:03:07 +0200

    enhance database migrations logging

--- database/src/index.ts
+++ database/src/index.ts
@@ -1,24 +1,34 @@
+/* eslint-disable no-console */
 import { DatabaseState, getDatabaseState } from './prepare'
 import { CONFIG } from './config'
 
 import { createPool } from 'mysql2'
 import { Migration, MigrationConnection } from 'ts-mysql-migrate'
 import path from 'path'
+import { latestDbVersion } from './config/detectLastDBVersion'
 
 const run = async (command: string) => {
   // Database actions not supported by our migration library
   // await createDatabase()
   const state = await getDatabaseState()
   if (state === DatabaseState.NOT_CONNECTED) {
-    throw new Error('Database not connected')
+    throw new Error(
+      `Database not connected, is database server running?
+      host: ${CONFIG.DB_HOST}
+      port: ${CONFIG.DB_PORT}
+      user: ${CONFIG.DB_USER}
+      password: ${CONFIG.DB_PASSWORD.slice(-2)}
+      database: ${CONFIG.DB_DATABASE}`,
+    )
   }
   if (state === DatabaseState.HIGHER_VERSION) {
     throw new Error('Database version is higher than required, please switch to the correct branch')
   }
   if (state === DatabaseState.SAME_VERSION) {
-    // eslint-disable-next-line no-console
-    console.log('Database is up to date')
-    return
+    if (command === 'up') {
+      console.log('Database is up to date')
+      return
+    }
   }
   // Initialize Migrations
   const pool = createPool({
@@ -51,14 +61,23 @@ const run = async (command: string) => {
     default:
       throw new Error(`Unsupported command ${command}`)
   }
-
+  if (command === 'reset') {
+    console.log('Database was reset')
+  } else {
+    const currentDbVersion = await migration.getLastVersion()
+    console.log(`Database was ${command} migrated to version: ${currentDbVersion.fileName}`)
+    if (latestDbVersion === currentDbVersion.fileName.split('.')[0]) {
+      console.log('Database is now up to date')
+    } else {
+      console.log('The latest database version is: ', latestDbVersion)
+    }
+  }
   // Terminate connections gracefully
   pool.end()
 }
 
 run(process.argv[2])
   .catch((err) => {
-    // eslint-disable-next-line no-console
     console.log(err)
     process.exit(1)
   })
