3ec68ce081f7a3925b07da61108be7f69c81b78d
einhornimmond <info@einhornimmond.de>
2025-04-26 10:22:42 +0200

    add biome to config, fix lint, use biome config file from root

--- config/src/commonSchema.ts
+++ config/src/commonSchema.ts
@@ -1,22 +1,22 @@
 import Joi from 'joi'
 
 export const browserUrls = Joi.array()
-  .items(Joi.string().uri()) 
+  .items(Joi.string().uri())
   .sparse(true)
   .custom((value: string[], helpers: Joi.CustomHelpers<string[]>) => {
     let protocol: string | undefined
     for (const url of value) {
-      if (url === undefined) { 
-        continue 
+      if (url === undefined) {
+        continue
       }
       const urlObject = new URL(url)
-      if(!protocol) {
+      if (!protocol) {
         protocol = urlObject.protocol
-      } else if(urlObject.protocol !== protocol) {
+      } else if (urlObject.protocol !== protocol) {
         return helpers.error('any.invalid')
       }
     }
-    return value;
+    return value
   })
   .required()
   .description('All URLs need to have same protocol to prevent mixed block errors')
@@ -29,8 +29,12 @@ export const DECAY_START_TIME = Joi.date()
 
 export const DB_VERSION = Joi.string()
   .pattern(/^\d{4}-[a-z0-9-_]+$/)
-  .message('DB_VERSION must be in the format: YYYY-description, e.g. "0087-add_index_on_user_roles".')
-  .description('db version string, last migration file name without ending or last folder in entity')
+  .message(
+    'DB_VERSION must be in the format: YYYY-description, e.g. "0087-add_index_on_user_roles".',
+  )
+  .description(
+    'db version string, last migration file name without ending or last folder in entity',
+  )
   .required()
 
 export const COMMUNITY_URL = Joi.string()
@@ -39,9 +43,11 @@ export const COMMUNITY_URL = Joi.string()
     if (value.endsWith('/')) {
       return helpers.error('any.invalid', { message: 'URL should not end with a slash (/)' })
     }
-    return value;
+    return value
   })
-  .description('The base URL of the community, should have the same protocol as frontend, admin and backend api to prevent mixed contend issues.')
+  .description(
+    'The base URL of the community, should have the same protocol as frontend, admin and backend api to prevent mixed contend issues.',
+  )
   .default('http://0.0.0.0')
   .required()
 
@@ -62,7 +68,7 @@ export const COMMUNITY_NAME = Joi.string()
   .max(40)
   .description('The name of the community')
   .default('Gradido Entwicklung')
-  .required()  
+  .required()
 
 export const COMMUNITY_DESCRIPTION = Joi.string()
   .min(10)
@@ -82,19 +88,19 @@ export const COMMUNITY_LOCATION = Joi.string()
   .when('GMS_ACTIVE', {
     is: true,
     then: Joi.string().required(),
-    otherwise: Joi.string().optional()
+    otherwise: Joi.string().optional(),
   })
   .description('Geographical location of the community in "latitude, longitude" format')
-  .default('49.280377, 9.690151')    
-  
+  .default('49.280377, 9.690151')
+
 export const GRAPHIQL = Joi.boolean()
-    .description('Flag for enabling GraphQL playground for debugging.')
-    .default(false)
-    .when('NODE_ENV', {
-      is: 'development',
-      then: Joi.boolean().valid(true, false).required(), // only allow true in development mode
-      otherwise: Joi.boolean().valid(false).required() // false in any other mode
-    })
+  .description('Flag for enabling GraphQL playground for debugging.')
+  .default(false)
+  .when('NODE_ENV', {
+    is: 'development',
+    then: Joi.boolean().valid(true, false).required(), // only allow true in development mode
+    otherwise: Joi.boolean().valid(false).required(), // false in any other mode
+  })
 
 export const GMS_ACTIVE = Joi.boolean()
   .description('Flag to indicate if the GMS (Geographic Member Search) service is used.')
@@ -123,20 +129,20 @@ export const HUMHUB_API_URL = Joi.string()
   .description('The API URL for HumHub integration')
 
 export const LOG_LEVEL = Joi.string()
-  .valid('all', 'mark', 'trace', 'debug', 'info', 'warn', 'error', 'fatal', 'off') 
+  .valid('all', 'mark', 'trace', 'debug', 'info', 'warn', 'error', 'fatal', 'off')
   .description('set log level')
   .default('info')
   .required()
 
 export const LOG4JS_CONFIG = Joi.string()
-  .pattern(/^[a-zA-Z0-9-_]+\.json$/) 
+  .pattern(/^[a-zA-Z0-9-_]+\.json$/)
   .message('LOG4JS_CONFIG must be a valid filename ending with .json')
   .description('config file name for log4js config file')
   .default('log4js-config.json')
   .required()
 
 export const LOGIN_APP_SECRET = Joi.string()
-  .pattern(/^[a-fA-F0-9]+$/) 
+  .pattern(/^[a-fA-F0-9]+$/)
   .message('need to be valid hex')
   .default('21ffbbc616fe')
   .description('App secret for salt component for libsodium crypto_pwhash')
@@ -147,7 +153,9 @@ export const LOGIN_SERVER_KEY = Joi.string()
   .length(32)
   .message('need to be valid hex and 32 character')
   .default('a51ef8ac7ef1abf162fb7a65261acd7a')
-  .description('Server key for password hashing as additional salt for libsodium crypto_shorthash_keygen')
+  .description(
+    'Server key for password hashing as additional salt for libsodium crypto_shorthash_keygen',
+  )
   .required()
 
 export const OPENAI_ACTIVE = Joi.boolean()
@@ -156,7 +164,7 @@ export const OPENAI_ACTIVE = Joi.boolean()
   .required()
 
 export const TYPEORM_LOGGING_RELATIVE_PATH = Joi.string()
-  .pattern(new RegExp('^[a-zA-Z0-9-_\./]+\.log$')) 
+  .pattern(/^[a-zA-Z0-9-_\.\/]+\.log$/)
   .message('TYPEORM_LOGGING_RELATIVE_PATH must be a valid filename ending with .log')
   .description('log file name for logging typeorm activities')
   .default('typeorm.log')
@@ -168,7 +176,7 @@ export const DB_HOST = Joi.string()
   .description("database host like 'localhost' or 'mariadb' in docker setup")
   .default('localhost')
   .required()
-  
+
 export const DB_PORT = Joi.number()
   .integer()
   .min(1024)
@@ -179,10 +187,10 @@ export const DB_PORT = Joi.number()
 
 export const DB_USER = Joi.string()
   .pattern(/^[A-Za-z0-9]([A-Za-z0-9-_\.]*[A-Za-z0-9])?$/) // Validates MariaDB username rules
-  .min(1)  // Minimum length 1
+  .min(1) // Minimum length 1
   .max(16) // Maximum length 16
   .message(
-    'Valid database username (letters, numbers, hyphens, underscores, dots allowed; no spaces, must not start or end with hyphen, dot, or underscore)'
+    'Valid database username (letters, numbers, hyphens, underscores, dots allowed; no spaces, must not start or end with hyphen, dot, or underscore)',
   )
   .description('database username for mariadb')
   .default('root')
@@ -191,24 +199,26 @@ export const DB_USER = Joi.string()
 export const DB_PASSWORD = Joi.string()
   .when(Joi.ref('NODE_ENV'), {
     is: 'development',
-    then: Joi.string().allow(''), 
+    then: Joi.string().allow(''),
     otherwise: Joi.string()
-      .min(8) 
+      .min(8)
       .max(32)
       .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).+$/)
       .message(
-        'Password must be between 8 and 32 characters long, and contain at least one uppercase letter, one lowercase letter, one number, and one special character (e.g., !@#$%^&*).'
-      )
+        'Password must be between 8 and 32 characters long, and contain at least one uppercase letter, one lowercase letter, one number, and one special character (e.g., !@#$%^&*).',
+      ),
   })
   .description(
-    'Password for the database user. In development mode, an empty password is allowed. In other environments, a complex password is required.'
+    'Password for the database user. In development mode, an empty password is allowed. In other environments, a complex password is required.',
   )
-  .default('') 
+  .default('')
   .required()
 
 export const DB_DATABASE = Joi.string()
   .pattern(/^[a-zA-Z][a-zA-Z0-9_-]{1,63}$/)
-  .description('Database name like gradido_community (must start with a letter, and can only contain letters, numbers, underscores, or dashes)')
+  .description(
+    'Database name like gradido_community (must start with a letter, and can only contain letters, numbers, underscores, or dashes)',
+  )
   .default('gradido_community')
   .required()
 
@@ -221,13 +231,13 @@ export const APP_VERSION = Joi.string()
 export const BUILD_COMMIT = Joi.string()
   .pattern(/^[0-9a-f]{40}$/)
   .message('The commit hash must be a 40-character hexadecimal string.')
-  .description('The full git commit hash.')    
+  .description('The full git commit hash.')
   .optional()
 
 export const BUILD_COMMIT_SHORT = Joi.string()
   .pattern(/^[0-9a-f]{7}$/)
   .message('The first 7 hexadecimal character from git commit hash.')
-  .description('A short version from the git commit hash.')    
+  .description('A short version from the git commit hash.')
   .required()
 
 export const NODE_ENV = Joi.string()
@@ -236,11 +246,15 @@ export const NODE_ENV = Joi.string()
   .description('Specifies the environment in which the application is running.')
 
 export const DEBUG = Joi.boolean()
-  .description('Indicates whether the application is in debugging mode. Set to true when NODE_ENV is not "production".')
+  .description(
+    'Indicates whether the application is in debugging mode. Set to true when NODE_ENV is not "production".',
+  )
   .default(false)
   .required()
-  
+
 export const PRODUCTION = Joi.boolean()
   .default(false)
-  .description('Indicates whether the application is running in production mode. Set to true when NODE_ENV is "production".')
+  .description(
+    'Indicates whether the application is running in production mode. Set to true when NODE_ENV is "production".',
+  )
   .required()
