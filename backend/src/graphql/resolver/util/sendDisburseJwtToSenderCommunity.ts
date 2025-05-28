9eee093519f00a31ce449a091d3f538ac3481976
clauspeterhuebner <hr.softwarelab@t-online.de>
2025-05-28 02:24:45 +0200

    more loggers around the invokation of disburseJwt

--- backend/src/graphql/resolver/util/sendDisburseJwtToSenderCommunity.ts
+++ backend/src/graphql/resolver/util/sendDisburseJwtToSenderCommunity.ts
@@ -3,11 +3,16 @@ import { DisbursementJwtResult } from '@/federation/client/1_0/model/Disbursemen
 import { DisbursementClientFactory } from '@/federation/client/DisbursementClientFactory'
 import { LogError } from '@/server/LogError'
 import { Community as DbCommunity, FederatedCommunity as DbFederatedCommunity } from 'database'
+import { backendLogger as logger } from '@/server/logger'
 
 export async function sendDisburseJwtToSenderCommunity(
   senderCommunity: DbCommunity,
   disburseJwt: string,
 ): Promise<DisbursementJwtResult> {
+  logger.debug('sendDisburseJwtToSenderCommunity... args=', {
+    senderCommunity,
+    disburseJwt,
+  })
   const senderFCom = await DbFederatedCommunity.findOneOrFail({
     where: {
       publicKey: senderCommunity.publicKey,
@@ -16,9 +21,11 @@ export async function sendDisburseJwtToSenderCommunity(
   })
   const disbursementClient = DisbursementClientFactory.getInstance(senderFCom)
   if (!disbursementClient) {
-    throw new LogError('Disbursement client not found', senderCommunity.publicKey)
+    throw new LogError('Disbursement client not found', senderCommunity.publicKey.toString('hex'))
   }
+  logger.debug('sendDisburseJwtToSenderCommunity... vor disburseJwt()')
   const result = await disbursementClient.disburseJwt(disburseJwt)
+  logger.debug('sendDisburseJwtToSenderCommunity... nach disburseJwt() result=', result)
   if (!result.accepted) {
     throw new LogError('Disbursement failed', result.message)
   }
