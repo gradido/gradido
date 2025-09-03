be51e3709292e8ea70409c509d0a482cf4016890
clauspeterhuebner <hr.softwarelab@t-online.de>
2025-09-03 17:01:50 +0200

    remove loggers and export DisbursementResolver

--- federation/src/graphql/api/1_0/schema.ts
+++ federation/src/graphql/api/1_0/schema.ts
@@ -1,9 +1,10 @@
 import { NonEmptyArray } from 'type-graphql'
 import { AuthenticationResolver } from './resolver/AuthenticationResolver'
+import { DisbursementResolver } from './resolver/DisbursementResolver'
 import { PublicCommunityInfoResolver } from './resolver/PublicCommunityInfoResolver'
 import { PublicKeyResolver } from './resolver/PublicKeyResolver'
 import { SendCoinsResolver } from './resolver/SendCoinsResolver'
 
 export const getApiResolvers = (): NonEmptyArray<Function> => {
-  return [AuthenticationResolver, PublicCommunityInfoResolver, PublicKeyResolver, SendCoinsResolver]
+  return [AuthenticationResolver, DisbursementResolver, PublicCommunityInfoResolver, PublicKeyResolver, SendCoinsResolver]
 }
