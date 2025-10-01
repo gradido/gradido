579ec59daa10e16a1a86f4ea72be1651c9fd48df
clauspeterhuebner <hr.softwarelab@t-online.de>
2025-10-02 01:09:53 +0200

    remove check with parsing OneTimeCode

--- federation/src/graphql/api/1_0/resolver/AuthenticationResolver.ts
+++ federation/src/graphql/api/1_0/resolver/AuthenticationResolver.ts
@@ -125,12 +125,15 @@ export class AuthenticationResolver {
         // no infos to the caller
         return null
       }
+/*
       if (!uint32Schema.safeParse(authArgs.oneTimeCode).success) {
         const errmsg = `invalid oneTimeCode: ${authArgs.oneTimeCode} for community with publicKey ${authArgs.publicKey}, expect uint32`
         methodLogger.error(errmsg)
         // no infos to the caller
         return null
       }
+*/
+      methodLogger.debug(`search community per oneTimeCode:`, authArgs.oneTimeCode)
       const authCom = await DbCommunity.findOneByOrFail({ communityUuid: authArgs.oneTimeCode })
       if (authCom) {
         methodLogger.debug('found authCom:', new CommunityLoggingView(authCom))
