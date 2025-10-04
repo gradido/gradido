e0e7c312d40852fa904836b5fbebf9c8308f3942
einhornimmond <info@einhornimmond.de>
2025-10-04 14:16:13 +0200

    fix stylelint

--- frontend/src/components/Breadcrumb/breadcrumb.vue
+++ frontend/src/components/Breadcrumb/breadcrumb.vue
@@ -23,7 +23,8 @@ export default {
   margin-bottom: 1rem;
   padding: 0.75rem 1rem;
 }
-@media screen and (max-width: 450px) {
+
+@media screen and (width <= 450px) {
   .page-breadcrumb {
     margin-top: 3rem;
   }
