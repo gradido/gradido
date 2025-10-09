671c7b238df941b641b80bae841ef95cf63a7b6a
einhornimmond <silas@einhornimmond.de>
2025-10-09 06:32:43 +0200

    actually we don't need to define default on CreateDateColumn and default and onUpdate on UpdateDateColumn, typeorm do this by his self

--- database/src/entity/OpenaiThreads.ts
+++ database/src/entity/OpenaiThreads.ts
@@ -5,10 +5,10 @@ export class OpenaiThreads extends BaseEntity {
   @PrimaryColumn({ type: 'char', length: 30 })
   id: string
 
-  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  @CreateDateColumn({ type: 'timestamp' })
   createdAt: Date
 
-  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+  @UpdateDateColumn({ type: 'timestamp' })
   updatedAt: Date
 
   @Column({ name: 'user_id', type: 'int', unsigned: true })
