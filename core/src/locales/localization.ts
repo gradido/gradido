a5bd54dd58c06560205ae4b00405a585c93b50d3
einhornimmond <info@einhornimmond.de>
2025-11-27 19:03:43 +0100

    move localization completly into core

--- core/src/locales/localization.ts
+++ core/src/locales/localization.ts
@@ -1,6 +1,10 @@
-import en from './locales/en.json'
-import de from './locales/de.json'
+import en from './en.json'
+import de from './de.json'
 import { I18n } from 'i18n'
+import { getLogger } from 'log4js'
+import { LOG4JS_BASE_CATEGORY_NAME } from '../config/const'
+
+const logger = getLogger(`${LOG4JS_BASE_CATEGORY_NAME}.emails.localization`)
 
 function flatten(obj: any, prefix: string = ''): any {
   const result: any = {}
@@ -18,6 +22,9 @@ export const i18n = new I18n({
   locales: ['en', 'de'],
   defaultLocale: 'en',
   staticCatalog: { en: flatten(en), de: flatten(de) },
+  logDebugFn: (msg) => logger.debug(msg),
+  logWarnFn: (msg) => logger.info(msg),
+  logErrorFn: (msg) => logger.error(msg),
   api: {
     __: 't', // now req.__ becomes req.t
     __n: 'tn', // and req.__n can be called as req.tn
