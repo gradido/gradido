07ecf24758638009aa0a485516734a9abc61bbb7
einhornimmond <info@einhornimmond.de>
2026-04-25 15:28:12 +0200

    fix lint

--- shared/src/schema/base.schema.ts
+++ shared/src/schema/base.schema.ts
@@ -1,12 +1,11 @@
 import { validate, version } from 'uuid'
 import { z } from 'zod'
-import { GradidoUnit } from '../data/GradidoUnit'
 import { Duration } from '../data/Duration'
+import { GradidoUnit } from '../data/GradidoUnit'
 
-export const uuidv4Schema = z.string().refine(
-  (val: string) => validate(val) && version(val) === 4,
-  'Invalid uuid',
-)
+export const uuidv4Schema = z
+  .string()
+  .refine((val: string) => validate(val) && version(val) === 4, 'Invalid uuid')
 export const emailSchema = z.string().email()
 export const urlSchema = z.string().url()
 export const uint32Schema = z.number().positive().lte(4294967295)
