bae15fcfc37a964e26a4a59ff27e7cb77dedec7d
einhornimmond <info@einhornimmond.de>
2026-04-25 16:46:10 +0200

    revert change

--- shared/src/schema/base.schema.ts
+++ shared/src/schema/base.schema.ts
@@ -1,9 +1,10 @@
 import { validate, version } from 'uuid'
-import { z } from 'zod'
+import { number, string } from 'zod'
 
-export const uuidv4Schema = z
-  .string()
-  .refine((val: string) => validate(val) && version(val) === 4, 'Invalid uuid')
-export const emailSchema = z.string().email()
-export const urlSchema = z.string().url()
-export const uint32Schema = z.number().positive().lte(4294967295)
+export const uuidv4Schema = string().refine(
+  (val: string) => validate(val) && version(val) === 4,
+  'Invalid uuid',
+)
+export const emailSchema = string().email()
+export const urlSchema = string().url()
+export const uint32Schema = number().positive().lte(4294967295)
