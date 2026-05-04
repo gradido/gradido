66b5cb97f1aa89f89483e04d442fdf833de8d3e6
clauspeterhuebner <hr.softwarelab@t-online.de>
2026-05-04 23:35:06 +0200

    linting

--- core/src/federation/client/1_0/CommandClient.ts
+++ core/src/federation/client/1_0/CommandClient.ts
@@ -28,9 +28,12 @@ export class CommandClient {
   async sendCommand(args: EncryptedTransferArgs): Promise<boolean> {
     logger.debug(`sendCommand at ${this.endpoint} for args:`, args)
     try {
-      const result = await this.client.rawRequest<{ sendCommand: { success: boolean } }>(sendCommandQuery, {
-        args,
-      })
+      const result = await this.client.rawRequest<{ sendCommand: { success: boolean } }>(
+        sendCommandQuery,
+        {
+          args,
+        },
+      )
       logger.debug('nach rawRequest: result', result)
       if (!result?.data?.sendCommand?.success) {
         logger.warn('sendCommand without response data from endpoint', this.endpoint)
