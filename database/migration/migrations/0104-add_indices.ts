127c6bf37a881ba0c45d62e9f4408e6ec315f5a3
einhornimmond <info@einhornimmond.de>
2026-06-30 13:03:40 +0200

    lint

--- database/migration/migrations/0104-add_indices.ts
+++ database/migration/migrations/0104-add_indices.ts
@@ -1,6 +1,8 @@
 export async function upgrade(queryFn: (query: string, values?: any[]) => Promise<Array<any>>) {
   await queryFn(`CREATE INDEX idx_contributions_transaction_id ON contributions(transaction_id);`)
-  await queryFn(`CREATE INDEX idx_user_id_valid_until ON transaction_links(userId, validUntil, id); `)
+  await queryFn(
+    `CREATE INDEX idx_user_id_valid_until ON transaction_links(userId, validUntil, id); `,
+  )
 }
 
 export async function downgrade(queryFn: (query: string, values?: any[]) => Promise<Array<any>>) {
