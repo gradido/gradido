4b14fb805119f9d524aebde0bf1add0fc408037e
einhornimmond <info@einhornimmond.de>
2026-07-08 12:30:25 +0200

    update api for new gms server

--- backend/src/webhook/gms.ts
+++ backend/src/webhook/gms.ts
@@ -1,6 +1,6 @@
 import { User as DbUser } from 'database'
 import { getLogger } from 'log4js'
-import { decode } from '@/auth/JWT'
+import { verifyGmsHandshakeJWTToken } from '@/apis/gms/GmsClient'
 import { LOG4JS_BASE_CATEGORY_NAME } from '@/config/const'
 
 const logger = getLogger(`${LOG4JS_BASE_CATEGORY_NAME}.webhook.gms`)
@@ -14,16 +14,16 @@ export const gmsWebhook = async (req: any, res: any): Promise<void> => {
     res.status(400).json({ message: 'false' })
     return
   }
-  const payload = await decode(token)
-  logger.debug('gmsWebhook: decoded token=', payload)
-  if (!payload) {
+  const gradidoID = await verifyGmsHandshakeJWTToken(token)
+  logger.debug('gmsWebhook: decoded token, gradidoID=', gradidoID)
+  if (!gradidoID) {
     logger.debug('gmsWebhook: invalid token', token)
     res.status(400).json({ message: 'false' })
     return
   }
-  const user = await DbUser.findOne({ where: { gradidoID: payload.gradidoID } })
+  const user = await DbUser.findOne({ where: { gradidoID } })
   if (!user) {
-    logger.error('gmsWebhook: missing user', payload.gradidoID)
+    logger.error('gmsWebhook: missing user', gradidoID)
     res.status(400).json({ message: 'false' })
     return
   }
@@ -34,5 +34,5 @@ export const gmsWebhook = async (req: any, res: any): Promise<void> => {
     user.lastName.slice(0, 3),
   )
   logger.info('gmsWebhook: authentication successful')
-  res.status(200).json({ userUuid: user.gradidoID })
+  res.status(200).send(user.gradidoID)
 }
